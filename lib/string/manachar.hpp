dd080fd01fffa2ddf6944dafc55496296291277f
kuhaku-space <te4958yu6924@gmail.com>
2021-01-13 00:04:43 +0900

    Update manachar.hpp

--- lib/string/manachar.hpp
+++ lib/string/manachar.hpp
@@ -4,18 +4,17 @@
 // R[i] := S[i-(R[i]-1):i] == S[i:i+(R[i]-1)]
 template <typename Type>
 struct Manacher {
-    Type S;
-    vector<int64_t> R;
+    Type s;
+    vector<int64_t> data;
 
-    Manacher(Type S) : S(S) {
-        R.resize(S.size());
+    Manacher(Type _s) : s(_s), data(_s.size()) {
         int64_t i = 0, j = 0;
-        while (i < S.size()) {
-            while (i - j >= 0 && i + j < S.size() && S[i - j] == S[i + j]) ++j;
-            R[i] = j;
+        while (i < s.size()) {
+            while (i - j >= 0 && i + j < s.size() && s[i - j] == s[i + j]) ++j;
+            data[i] = j;
             int64_t k = 1;
-            while (i - k >= 0 && i + k < S.size() && k + R[i - k] < j)
-                R[i + k] = R[i - k], ++k;
+            while (i - k >= 0 && i + k < s.size() && k + data[i - k] < j)
+                data[i + k] = data[i - k], ++k;
             i += k, j -= k;
         }
     }
