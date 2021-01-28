ad997324b080d75df60a7a37b9f78a478336c8ae
kuhaku-space <te4958yu6924@gmail.com>
2021-01-28 12:19:13 +0900

    Create UMap.hpp

---
+++ lib/data_struct/UMap.hpp
@@ -0,0 +1,21 @@
+#include "_base.hpp"
+
+template <class Key, class Val>
+struct UMap {
+    unordered_map<Key, Val> data;
+    Val e;
+
+    UMap(Val _e = Val()) : e(_e), data() {}
+
+    Val &operator[](Key k) {
+        if (data.find(k) == data.end()) data[k] = e;
+        return data[k];
+    }
+
+    auto begin() { return data.begin(); }
+    auto end() { return data.end(); }
+
+    auto find() { return data.find(); }
+
+    auto erase(Val v) { return data.erase(v); }
+};
