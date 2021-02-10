76727573876315dab775279e6778d7278fcbe97a
kuhaku-space <te4958yu6924@gmail.com>
2021-02-10 14:18:52 +0900

    Update graph_topological_sort.hpp

--- graph/graph_topological_sort.hpp
+++ graph/graph_topological_sort.hpp
@@ -7,9 +7,7 @@ struct Graph {
         T dist;
 
         bool operator<(const edge &rhs) const { return dist < rhs.dist; }
-
         bool operator>(const edge &rhs) const { return dist > rhs.dist; }
-
         edge &operator+=(const edge &rhs) {
             to = rhs.to;
             dist += rhs.dist;
