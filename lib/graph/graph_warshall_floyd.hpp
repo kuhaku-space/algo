6864797f8f042dd6da26f5189efda9d201c2fb57
kuhaku-space <te4958yu6924@gmail.com>
2021-02-10 12:47:53 +0900

    Update graph_warshall_floyd.hpp

--- lib/graph/graph_warshall_floyd.hpp
+++ lib/graph/graph_warshall_floyd.hpp
@@ -9,15 +9,16 @@ struct Graph {
         dist = vector<vector<T>>(V, vector<int64_t>(V));
     }
 
+    const vector<T> &operator[](int64_t i) const { return dist[i]; }
+    vector<T> &operator[](int64_t i) { return dist[i]; }
+
     void add_edge(int64_t a, int64_t b, T d) {
         dist[a][b] = d;
         dist[b][a] = d;
     }
 
     void warshall_floyd() {
-        for (int64_t i = 0; i < V; ++i) {
-            dist[i][i] = 0;
-        }
+        for (int64_t i = 0; i < V; ++i) dist[i][i] = 0;
         for (int64_t k = 0; k < V; ++k) {
             for (int64_t i = 0; i < V; ++i) {
                 for (int64_t j = 0; j < V; ++j) {
@@ -26,6 +27,4 @@ struct Graph {
             }
         }
     }
-
-    const T get_dist(int64_t a, int64_t b) const { return dist[a][b]; }
 };
