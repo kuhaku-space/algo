7bce468e9373b6ab9eaa1de26fb5686414a59f23
kuhaku-space <te4958yu6924@gmail.com>
2021-02-18 20:38:40 +0900

    Create convex_hull.hpp

---
+++ data_struct/convex_hull.hpp
@@ -0,0 +1,30 @@
+#include "_base.hpp"
+
+template <class T>
+pair<T, T> &operator-=(pair<T, T> &a, const pair<T, T> &b) {
+    a.first -= b.first, a.second -= b.second;
+    return a;
+}
+template <class T>
+vector<pair<T, T>> convex_hull(vector<pair<T, T>> ps) {
+    int64_t n = ps.size(), k = 0;
+    auto asc = [](pair<T, T> a, pair<T, T> b) {
+        return a.first != b.first ? a.first < b.first : a.second < b.second;
+    };
+    sort(ps.begin(), ps.end(), asc);
+    vector<pair<T, T>> ch(n * 2);
+    auto cross = [](pair<T, T> a, pair<T, T> b, const pair<T, T> &c) {
+        a -= c, b -= c;
+        return a.first * b.second - a.second * b.first;
+    };
+    for (int64_t i = 0; i < n; ++i) {
+        while (k > 1 && cross(ps[i], ch[k - 2], ch[k - 1]) < 0) --k;
+        ch[k++] = ps[i];
+    }
+    for (int64_t i = n - 2, t = k; i >= 0; --i) {
+        while (k > t && cross(ps[i], ch[k - 2], ch[k - 1]) < 0) --k;
+        ch[k++] = ps[i];
+    }
+    ch.resize(k - 1);
+    return ch;
+}
