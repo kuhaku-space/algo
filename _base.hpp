b378c7e5db013517ef52c690054282fe90367c51
kuhaku-space <te4958yu6924@gmail.com>
2021-02-18 22:07:57 +0900

    Update _base.hpp

--- _base.hpp
+++ _base.hpp
@@ -8,7 +8,7 @@ template <class T1, class T2>
 bool chmin(T1 &a, const T2 b) {
     return a > b ? a = b, true : false;
 }
-constexpr int64_t INF = 1000000000000000001;
+constexpr int64_t INF = 1000000000000000003;
 constexpr int64_t MOD = 1000000007;
 constexpr int64_t MOD_N = 998244353;
 constexpr double EPS = 1e-11;
