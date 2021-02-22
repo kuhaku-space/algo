5e1de26aab8920a8f54748ef592689439fb82585
kuhaku-space <te4958yu6924@gmail.com>
2021-02-22 23:45:39 +0900

    Update _base.hpp

--- _base.hpp
+++ _base.hpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-template <class T1, class T2>
-bool chmax(T1 &a, const T2 b) {
+template <class T, class U>
+bool chmax(T &a, const U &b) {
     return a < b ? a = b, true : false;
 }
-template <class T1, class T2>
-bool chmin(T1 &a, const T2 b) {
-    return a > b ? a = b, true : false;
+template <class T, class U>
+bool chmin(T &a, const U &b) {
+    return b < a ? a = b, true : false;
 }
 constexpr int64_t INF = 1000000000000000003;
 constexpr int64_t MOD = 1000000007;
