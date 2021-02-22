e4454f902a66e2f25377ee11aa71918c0d7ea24f
kuhaku-space <te4958yu6924@gmail.com>
2021-02-22 23:48:42 +0900

    Rename topological_sort.hpp

---
+++ graph/topological_sort.hpp
@@ -0,0 +1,19 @@
+#include "_base.hpp"
+#include "graph/graph.hpp"
+
+template <class T>
+vector<int> topological_sort(const Graph<T> &g) {
+    int n = g.size();
+    vector<int> res;
+    bitset<1 << 20> is_seen;
+    auto dfs = [&](auto self, int v) {
+        if (is_seen[v]) continue;
+        is_seen.set(v);
+        for (auto &e : g.edges[v])
+            if (!is_seen[e.to]) self(self, e.to);
+        res.emplace_back(v);
+    };
+    for (int64_t i = 0; i < n; ++i) dfs(dfs, i);
+    reverse(res.begin(), res.end());
+    return res;
+}
