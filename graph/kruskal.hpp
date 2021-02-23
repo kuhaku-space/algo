a5e4a901162444e9d65c417a37cf1668fe69832b
kuhaku-space <te4958yu6924@gmail.com>
2021-02-24 03:06:36 +0900

    Update kruskal.hpp

--- graph/kruskal.hpp
+++ graph/kruskal.hpp
@@ -2,17 +2,17 @@
 #include "graph/graph.hpp"
 #include "tree/union_find.hpp"
 
-// verify : https://onlinejudge.u-aizu.ac.jp/problems/GRL_2_A 21/02/23
+// verify : https://onlinejudge.u-aizu.ac.jp/problems/GRL_2_A 21/02/24
 
 // 最小全域木
 template <class T>
-vector<Graph<T>::edge> kruskal(const Graph<T> &g) {
-    using _edge = Graph<T>::edge;
-    union_find uf();
+vector<typename Graph<T>::edge> kruskal(const Graph<T> &g) {
+    using _edge = typename Graph<T>::edge;
+    union_find uf(g.size());
     vector<_edge> res;
     vector<_edge> edge_list;
-    for (auto &v : g.edges) {
-        edge_list.emplace(edge_list.end(), v.begin(), v.end());
+    for (auto &v : g) {
+        for (auto &e : v) edge_list.emplace_back(e);
     }
     sort(edge_list.begin(), edge_list.end());
     for (auto &e : edge_list) {
