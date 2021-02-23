6e8e1510b3911ff501bda2f25f53151f352a25dd
kuhaku-space <te4958yu6924@gmail.com>
2021-02-24 03:06:10 +0900

    Update bellman_ford.hpp

--- graph/bellman_ford.hpp
+++ graph/bellman_ford.hpp
@@ -1,6 +1,9 @@
 #include "_base.hpp"
 #include "graph/graph.hpp"
 
+// verify : https://atcoder.jp/contests/abc137/tasks/abc137_e 21/02/24
+// verify : https://atcoder.jp/contests/abc061/tasks/abc061_d 21/02/24
+
 template <class T>
 vector<T> bellman_ford(const Graph<T> &g, int s = 0,
                        T inf = numeric_limits<T>::max()) {
@@ -13,8 +16,9 @@ vector<T> bellman_ford(const Graph<T> &g, int s = 0,
         flg = false;
         for (int i = 0; i < n; ++i) {
             if (dist[i] == inf) continue;
-            for (auto j : g.edges[i]) {
-                if (chmin(dist[j.to], dist[i] + j.dist)) {
+            for (auto &j : g[i]) {
+                if (chmin(dist[j.to], dist[i] + j.dist) ||
+                    negative_cycle[i] && !negative_cycle[j.to]) {
                     flg = true;
                     if (cnt >= n) negative_cycle.set(j.to);
                 }
