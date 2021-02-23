73acc073cb6ea00dd182b6b1eaf7876374b8602a
kuhaku-space <te4958yu6924@gmail.com>
2021-02-24 03:21:13 +0900

    Update graph_warshall_floyd.hpp

--- graph/graph_warshall_floyd.hpp
+++ graph/graph_warshall_floyd.hpp
@@ -2,26 +2,24 @@
 
 template <class T>
 struct Graph {
-    int64_t V;
+    int V;
     vector<vector<T>> dist;
 
-    Graph(int64_t _V) : V(_V) {
-        dist = vector<vector<T>>(V, vector<int64_t>(V));
-    }
+    Graph(int _V) : V(_V) { dist = vector<vector<T>>(V, vector<T>(V)); }
 
-    const vector<T> &operator[](int64_t i) const { return dist[i]; }
-    vector<T> &operator[](int64_t i) { return dist[i]; }
+    const vector<T> &operator[](int i) const { return dist[i]; }
+    vector<T> &operator[](int i) { return dist[i]; }
 
-    void add_edge(int64_t a, int64_t b, T d) {
+    void add_edge(int a, int b, T d) {
         dist[a][b] = d;
         dist[b][a] = d;
     }
 
     void warshall_floyd() {
-        for (int64_t i = 0; i < V; ++i) dist[i][i] = 0;
-        for (int64_t k = 0; k < V; ++k) {
-            for (int64_t i = 0; i < V; ++i) {
-                for (int64_t j = 0; j < V; ++j) {
+        for (int i = 0; i < V; ++i) dist[i][i] = 0;
+        for (int k = 0; k < V; ++k) {
+            for (int i = 0; i < V; ++i) {
+                for (int j = 0; j < V; ++j) {
                     chmin(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
