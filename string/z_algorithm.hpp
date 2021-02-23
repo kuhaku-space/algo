570be3feae35c75b06d12d5a7426b15047764d30
kuhaku-space <te4958yu6924@gmail.com>
2021-02-24 07:49:55 +0900

    Update z_algorithm.hpp

--- string/z_algorithm.hpp
+++ string/z_algorithm.hpp
@@ -7,11 +7,11 @@
 template <typename Type>
 struct Z_algorithm {
     Type s;
-    vector<int64_t> data;
+    vector<int> data;
 
     Z_algorithm(const Type &_s) : s(_s), data(_s.size()) {
         data[0] = s.size();
-        int64_t i = 1, j = 0;
+        int i = 1, j = 0;
         while (i < s.size()) {
             while (i + j < s.size() && s[j] == s[i + j]) ++j;
             data[i] = j;
@@ -19,7 +19,7 @@ struct Z_algorithm {
                 ++i;
                 continue;
             }
-            int64_t k = 1;
+            int k = 1;
             while (i + k < s.size() && k + data[k] < j)
                 data[i + k] = data[k], ++k;
             i += k, j -= k;
