e1bcc401c6e3c5a082bcd4c7b6d738beb4aa8c88
kuhaku-space <te4958yu6924@gmail.com>
2021-02-23 11:56:27 +0900

    Update manacher.hpp

--- string/manacher.hpp
+++ string/manacher.hpp
@@ -9,19 +9,19 @@
 template <typename Type>
 struct Manacher {
     Type s;
-    vector<int64_t> data;
+    vector<int> data;
 
     Manacher(const Type &_s) : s(_s), data(_s.size()) {
-        int64_t i = 0, j = 0;
+        int i = 0, j = 0;
         while (i < s.size()) {
             while (i - j >= 0 && i + j < s.size() && s[i - j] == s[i + j]) ++j;
             data[i] = j;
-            int64_t k = 1;
+            int k = 1;
             while (i - k >= 0 && i + k < s.size() && k + data[i - k] < j)
                 data[i + k] = data[i - k], ++k;
             i += k, j -= k;
         }
     }
 
-    const int64_t &operator[](int64_t i) const { return data[i]; }
+    const int &operator[](int i) const { return data[i]; }
 };
