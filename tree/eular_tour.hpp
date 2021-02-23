c425a55e6a096b26be6b71cee93fe90829b4d1c5
kuhaku-space <te4958yu6924@gmail.com>
2021-02-23 11:56:37 +0900

    Update eular_tour.hpp

--- tree/eular_tour.hpp
+++ tree/eular_tour.hpp
@@ -3,37 +3,37 @@
 // https://github.com/beet-aizu/library/blob/master/tree/eulertourforvertex.cpp
 
 struct eular_tour {
-    vector<vector<int64_t>> data;
-    vector<int64_t> ls, rs;
-    int64_t pos;
+    vector<vector<int>> G;
+    vector<int> ls, rs;
+    int pos;
 
-    eular_tour(int64_t n) : data(n), ls(n), rs(n) {}
+    eular_tour(int n) : G(n), ls(n), rs(n) {}
 
-    const int64_t &operator[](int64_t i) const { return ls[i]; }
+    const int &operator[](int i) const { return ls[i]; }
 
-    void add_edge(int64_t a, int64_t b) {
-        data[a].emplace_back(b);
-        data[b].emplace_back(a);
+    void add_edge(int a, int b) {
+        G[a].emplace_back(b);
+        G[b].emplace_back(a);
     }
 
-    void dfs(int64_t v, int64_t p) {
+    void dfs(int v, int p) {
         ls[v] = pos++;
-        for (int64_t u : data[v]) {
+        for (auto &u : G[v]) {
             if (u != p) dfs(u, v);
             rs[v] = pos;
         }
     }
 
-    void build(int64_t r = 0) {
+    void build(int r = 0) {
         pos = 0;
         dfs(r, -1);
     }
 
-    const int64_t &get_l(int64_t i) const { return ls[i]; }
-    const int64_t &get_r(int64_t i) const { return rs[i]; }
+    const int &get_l(int i) const { return ls[i]; }
+    const int &get_r(int i) const { return rs[i]; }
 
     template <class F>
-    void query(int64_t v, F f) {
+    void query(int v, F f) {
         f(ls[v], rs[v]);
     }
 };
