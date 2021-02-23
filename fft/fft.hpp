7368002185a486a871c82b603727b15dcc9cbd76
kuhaku-space <te4958yu6924@gmail.com>
2021-02-24 05:05:44 +0900

    Update fft.hpp
    Fix #10

--- fft/fft.hpp
+++ fft/fft.hpp
@@ -4,28 +4,28 @@ struct FFT {
     using CP = complex<double>;
 
     void _fft(vector<CP> &a, bool inv) {
-        int64_t N = a.size();
+        int N = a.size();
         static bool is_first = true;
         static vector<CP> vbw(30), vibw(30);
         if (is_first) {
             is_first = false;
-            for (int64_t i = 0; i < 30; ++i) {
+            for (int i = 0; i < 30; ++i) {
                 vbw[i] = polar(1.0, 2.0 * PI / (1 << (i + 1)));
                 vibw[i] = polar(1.0, -2.0 * PI / (1 << (i + 1)));
             }
         }
-        for (int64_t i = 0, j = 1; j < N - 1; ++j) {
-            for (int64_t k = N >> 1; k > (i ^= k); k >>= 1)
+        for (int i = 0, j = 1; j < N - 1; ++j) {
+            for (int k = N >> 1; k > (i ^= k); k >>= 1)
                 ;
             if (i > j) swap(a[i], a[j]);
         }
-        for (int64_t k = 0, t = 1; t < N; ++k, t <<= 1) {
+        for (int k = 0, t = 1; t < N; ++k, t <<= 1) {
             CP bw = vbw[k];
             if (inv) bw = vibw[k];
-            for (int64_t i = 0; i < N; i += t * 2) {
+            for (int i = 0; i < N; i += t * 2) {
                 CP w(1.0);
-                for (int64_t j = 0; j < t; ++j) {
-                    int64_t l = i + j, r = i + j + t;
+                for (int j = 0; j < t; ++j) {
+                    int l = i + j, r = i + j + t;
                     CP c = a[l], d = a[r] * w;
                     a[l] = c + d;
                     a[r] = c - d;
@@ -34,54 +34,53 @@ struct FFT {
             }
         }
         if (inv) {
-            for (int64_t i = 0; i < N; ++i) a[i] /= N;
+            for (int i = 0; i < N; ++i) a[i] /= N;
         }
     }
 
-    vector<int64_t> convolution(const vector<int64_t> &a,
-                                const vector<int64_t> &b) {
-        int64_t n = a.size() + b.size() - 1;
-        int64_t N = 1;
+    template <class T>
+    vector<T> convolution(const vector<T> &a, const vector<T> &b) {
+        int n = a.size() + b.size() - 1;
+        int N = 1;
         while (N < n) N <<= 1;
 
         vector<CP> va(N), vb(N);
-        for (int64_t i = 0; i < a.size(); ++i) va[i] = CP(a[i]);
-        for (int64_t i = 0; i < b.size(); ++i) vb[i] = CP(b[i]);
+        for (int i = 0; i < a.size(); ++i) va[i] = CP(a[i]);
+        for (int i = 0; i < b.size(); ++i) vb[i] = CP(b[i]);
 
         _fft(va, false);
         _fft(vb, false);
 
-        for (int64_t i = 0; i < N; ++i) va[i] *= vb[i];
+        for (int i = 0; i < N; ++i) va[i] *= vb[i];
 
         _fft(va, true);
 
-        vector<int64_t> ans(n);
-        for (int64_t i = 0; i < n; ++i) {
-            ans[i] = int64_t(va[i].real() + 0.5);
-        }
+        vector<T> ans(n);
+        for (int i = 0; i < n; ++i) ans[i] = T(va[i].real() + 0.5);
+
+        return ans;
     }
 
-    void convolution_self(vector<int64_t> &a, const vector<int64_t> &b) {
-        int64_t n = a.size() + b.size() - 1;
-        int64_t N = 1;
+    template <class T>
+    void convolution_self(vector<T> &a, const vector<T> &b) {
+        int n = a.size() + b.size() - 1;
+        int N = 1;
         while (N < n) N <<= 1;
 
         vector<CP> va(N), vb(N);
-        for (int64_t i = 0; i < a.size(); ++i) va[i] = CP(a[i]);
+        for (int i = 0; i < a.size(); ++i) va[i] = CP(a[i]);
 
-        for (int64_t i = 0; i < b.size(); ++i) vb[i] = CP(b[i]);
+        for (int i = 0; i < b.size(); ++i) vb[i] = CP(b[i]);
 
         _fft(va, false);
         _fft(vb, false);
 
-        for (int64_t i = 0; i < N; ++i) va[i] *= vb[i];
+        for (int i = 0; i < N; ++i) va[i] *= vb[i];
 
         _fft(va, true);
 
         a.resize(n);
-        for (int64_t i = 0; i < n; ++i) {
-            a[i] = int64_t(va[i].real() + 0.5);
-        }
+        for (int i = 0; i < n; ++i) a[i] = T(va[i].real() + 0.5);
     }
 };
 
