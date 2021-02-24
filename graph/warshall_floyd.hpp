b599bb28f6f32887e09ec926dad6f404d7d233ca
kuhaku-space <te4958yu6924@gmail.com>
2021-02-25 04:26:46 +0900

    Rename warshall_floyd.hpp

---
+++ graph/warshall_floyd.hpp
@@ -0,0 +1,15 @@
+#include "_base.hpp"
+#include "graph/graph_matrix.hpp"
+
+template <class T>
+void warshall_floyd(Graph<T> &g) {
+    int n = g.size();
+    for (int i = 0; i < n; ++i) g[i][i] = 0;
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                chmin(g[i][j], g[i][k] + g[k][j]);
+            }
+        }
+    }
+}
