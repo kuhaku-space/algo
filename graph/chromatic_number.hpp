8b0ffa70126ba8752afa139fb78b33c3b4ebd34d
kuhaku-space <te4958yu6924@gmail.com>
2021-02-25 05:01:22 +0900

    Update chromatic_number.hpp

--- graph/chromatic_number.hpp
+++ graph/chromatic_number.hpp
@@ -8,12 +8,11 @@ int chromatic_number(const Graph<bool> &G) {
     for (int i = 0; i < n; ++i) {
         int s = 1 << i;
         for (int j = 0; j < n; ++j) {
-            if (G[i][j])
-                s |= 1 << j;
+            if (G[i][j]) s |= 1 << j;
         }
         neighbor[i] = s;
     }
-    
+
     vector<int> v(1 << n);
     v[0] = 1;
     for (int s = 1; s < 1 << n; ++s) {
@@ -28,8 +27,7 @@ int chromatic_number(const Graph<bool> &G) {
     auto _pow = [](auto f, int64_t a, int n) {
         int64_t res = 1;
         for (; n > 0; n >>= 1) {
-            if (n & 1)
-                res = f(res * a);
+            if (n & 1) res = f(res * a);
             a = f(a * a);
         }
         return res;
@@ -40,11 +38,15 @@ int chromatic_number(const Graph<bool> &G) {
         int mid = (low + high) >> 1;
         int64_t g = 0;
         for (int s = 0; s < 1 << n; ++s) {
-            if ((n - __builtin_popcount(s)) & 1) g = _mod(g + _MOD - _pow(_mod, v[s], mid));
-            else g = _mod(g + _pow(_mod, v[s], mid));
+            if ((n - __builtin_popcount(s)) & 1)
+                g = _mod(g + _MOD - _pow(_mod, v[s], mid));
+            else
+                g = _mod(g + _pow(_mod, v[s], mid));
         }
-        if (g != 0) high = mid;
-        else low = mid;
+        if (g != 0)
+            high = mid;
+        else
+            low = mid;
     }
     return high;
 }
