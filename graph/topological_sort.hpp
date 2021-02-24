b871bec6de1d29ec69903f09ab7e37c7a8a1ee64
kuhaku-space <te4958yu6924@gmail.com>
2021-02-24 21:04:46 +0900

    Update topological_sort.hpp

--- graph/topological_sort.hpp
+++ graph/topological_sort.hpp
@@ -1,7 +1,33 @@
 #include "_base.hpp"
 #include "graph/graph.hpp"
 
-// verify : https://onlinejudge.u-aizu.ac.jp/problems/GRL_4_B 21/02/23
+// verify : https://onlinejudge.u-aizu.ac.jp/problems/GRL_4_A 21/02/24
+
+template <class T>
+bool is_cycle(const Graph<T> &g) {
+    int n = g.size();
+    bitset<1 << 20> is_seen, is_finish;
+    bool res = false;
+
+    auto dfs = [&](auto self, int idx) {
+        if (is_finish[idx]) return;
+        is_seen.set(idx);
+        for (auto &e : g[idx]) {
+            if (res |= is_seen[e.to]) return;
+            self(self, e.to);
+        }
+        is_seen.reset(idx);
+        is_finish.set(idx);
+    };
+
+    for (int i = 0; i < n; ++i) {
+        is_seen.reset();
+        dfs(dfs, i);
+    }
+    return res;
+}
+
+// verify : https://onlinejudge.u-aizu.ac.jp/problems/GRL_4_B 21/02/24
 
 template <class T>
 vector<int> topological_sort(const Graph<T> &g) {
@@ -9,7 +35,7 @@ vector<int> topological_sort(const Graph<T> &g) {
     vector<int> res;
     bitset<1 << 20> is_seen;
     auto dfs = [&](auto self, int v) {
-        if (is_seen[v]) continue;
+        if (is_seen[v]) return;
         is_seen.set(v);
         for (auto &e : g[v])
             if (!is_seen[e.to]) self(self, e.to);
