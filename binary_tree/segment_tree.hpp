2bdfa9f174fdeb2d0569990f1a5aa6b7bdcc163e
kuhaku-space <te4958yu6924@gmail.com>
2021-02-26 19:34:25 +0900

    Update segment_tree.hpp

--- binary_tree/segment_tree.hpp
+++ binary_tree/segment_tree.hpp
@@ -9,11 +9,13 @@
 template <class T, class F>
 struct segment_tree {
     int N;
-    T e;
-    F f;
+    const T e;
+    const F op;
     vector<T> data;
 
-    segment_tree(int _n, T _e, F _f) : f(_f), e(_e) { init(_n); }
+    segment_tree(int _n, const T &_e, const F &_op) : e(_e), op(_op) {
+        init(_n);
+    }
 
     const T &operator[](int i) const { return data[i + N]; }
     T &operator[](int i) { return data[i + N]; }
@@ -23,38 +25,36 @@ struct segment_tree {
     void init(int n) {
         for (N = 1; N < n; N <<= 1)
             ;
-        data.assign(N * 2, e);
+        data.assign(N << 1, e);
     }
 
     template <class U>
-    void build(vector<U> v) {
-        for (int i = 0; i < v.size(); ++i) data[N + i] = v[i];
+    void build(const vector<U> &v) {
+        for (int i = 0; i < v.size(); ++i) data[N + i] = T(v[i]);
         for (int i = N - 1; i >= 1; --i)
-            data[i] = f(data[i * 2], data[i * 2 + 1]);
+            data[i] = op(data[i * 2], data[i * 2 + 1]);
     }
 
-    void update(int k, T x) {
+    void update(int k, const T &x) {
         assert(k >= 0 && k < N);
-        k += N;
-        data[k] = x;
-        while ((k >>= 1) >= 1) data[k] = f(data[k * 2], data[k * 2 + 1]);
+        data[k += N] = x;
+        while ((k >>= 1) >= 1) data[k] = op(data[k * 2], data[k * 2 + 1]);
     }
 
-    void add(int k, T x) {
+    void add(int k, const T &x) {
         assert(k >= 0 && k < N);
-        k += N;
-        data[k] += x;
-        while ((k >>= 1) >= 1) data[k] = f(data[k * 2], data[k * 2 + 1]);
+        data[k += N] += x;
+        while ((k >>= 1) >= 1) data[k] = op(data[k * 2], data[k * 2 + 1]);
     }
 
     T query(int a, int b) {
         assert(a >= 0 && a <= N);
         assert(b >= 0 && b <= N);
-        T res = e;
+        T l = e, r = e;
         for (a += N, b += N; a < b; a >>= 1, b >>= 1) {
-            if (a & 1) res = f(data[a++], res);
-            if (b & 1) res = f(res, data[--b]);
+            if (a & 1) l = op(l, data[a++]);
+            if (b & 1) r = op(data[--b], r);
         }
-        return res;
+        return op(l, r);
     }
 };
