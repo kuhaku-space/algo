22735138a06aacb27927ca98947b1f59f63bb461
kuhaku-space <te4958yu6924@gmail.com>
2021-03-16 12:39:00 +0900

    Update chokudai_search.hpp

--- marathon/chokudai_search.hpp
+++ marathon/chokudai_search.hpp
@@ -4,7 +4,6 @@ constexpr int N = 1000;
 constexpr int H = 125;
 constexpr int W = 8;
 
-constexpr int BEAM_SIZE = 100;
 constexpr double TL = 1.9;
 
 struct chokudai_search {
@@ -29,11 +28,10 @@ struct chokudai_search {
     };
 
     array<int, N> solve() {
-        auto st = clock();
         vector<priority_queue<State>> v(N + 1);
         v[0].push(State());
         int t = 0;
-        while ((double)(clock() - st) / CLOCKS_PER_SEC < TL) {
+        while ((double)clock() / CLOCKS_PER_SEC < TL) {
             if (v[t].empty()) {
                 if ((++t) >= N) t = 0;
                 continue;
@@ -41,7 +39,7 @@ struct chokudai_search {
             State now = v[t].top();
             v[t].pop();
             for (int col = 0; col < W; ++col) {
-                State s = now.place(t, col);
+                v[t + 1].push(now.place(t, col));
             }
             if ((++t) >= N) t = 0;
         }
