2ccecaa53020431dd79144b4b1eedcb85cfc536f
kuhaku-space <te4958yu6924@gmail.com>
2021-03-30 01:35:37 +0900

    Update _base.hpp

--- _base.hpp
+++ _base.hpp
@@ -9,6 +9,7 @@ bool chmin(T &a, const U &b) {
     return b < a ? a = b, true : false;
 }
 constexpr int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
 constexpr int MOD = 1000000007;
 constexpr int MOD_N = 998244353;
 constexpr double EPS = 1e-7;
