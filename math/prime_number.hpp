c44590f5898a70356bb15b607b4569d58676e9ef
kuhaku-space <te4958yu6924@gmail.com>
2021-03-30 02:50:01 +0900

    Update prime_number.hpp

--- math/prime_number.hpp
+++ math/prime_number.hpp
@@ -3,17 +3,18 @@
 struct prime_number {
     const int sz = 1 << 22;
     bitset<1 << 22> is_not_prime;
-    vector<int64_t> data;
+    vector<int> data;
 
     prime_number() { init(); }
 
     void init() {
         is_not_prime[0] = is_not_prime[1] = true;
-        for (int64_t i = 2; i < sz; ++i) {
+        for (int i = 2; i < sz; ++i) {
             if (!is_not_prime[i]) {
                 data.emplace_back(i);
-                for (int64_t j = 2; i * j < sz; ++j) {
-                    is_not_prime[i * j] = true;
+                if ((int64_t)i * i >= sz) continue;
+                for (int j = i * i; j < sz; j += i) {
+                    is_not_prime[j] = true;
                 }
             }
         }
@@ -23,74 +24,64 @@ struct prime_number {
         assert(n >= 0);
         if (n < sz) return !is_not_prime[n];
         for (auto i : data) {
-            if (i * i > n) break;
+            if ((int64_t)i * i > n) break;
             if (n % i == 0) return false;
         }
         return true;
     }
 
-    vector<int64_t> prime_numbers(int64_t n) {
-        vector<int64_t> res;
-        for (int i = 2; i <= n; ++i) {
-            if (!is_not_prime[i])
-                res.emplace_back(i);
+    vector<int> prime_numbers(int x) {
+        vector<int> res;
+        for (int i = 2; i <= x; ++i) {
+            if (is_prime(i)) res.emplace_back(i);
         }
         return res;
     }
 
     // 素因数分解
-    vector<pair<int64_t, int64_t>> prime_factorization(int64_t n) {
-        vector<pair<int64_t, int64_t>> res;
-
+    template <class T>
+    vector<pair<T, int>> prime_factorization(T x) {
+        if (x == 1) return vector<T>(1, {1, 1});
+        vector<pair<T, int>> res;
         for (auto i : data) {
-            int64_t cnt = 0;
-            for (; n % i == 0; n /= i) cnt++;
+            int cnt = 0;
+            for (; x % i == 0; x /= i) cnt++;
             if (cnt) res.emplace_back(i, cnt);
-            if (n < i * i) break;
+            if ((int64_t)i * i > x) break;
         }
-        if (n != 1) res.emplace_back(n, 1);
-
-        return res;
-    }
-
-    int64_t pow_int(int64_t x, int64_t n) {
-        assert(n >= 0);
-        int64_t res = 1;
-        for (; n > 0; n >>= 1) {
-            if (n & 1) res *= x;
-            x *= x;
-        }
+        if (x != 1) res.emplace_back(x, 1);
         return res;
     }
 
     // 約数列挙
-    vector<int64_t> divisors(int64_t n) {
-        auto v = prime_factorization(n);
-
-        vector<int64_t> res, a, b, cp;
+    template <class T>
+    vector<T> divisors(T x) {
+        auto v = prime_factorization(x);
+        vector<T> res, a, b, cp;
         res.emplace_back(1);
         for (auto p : v) {
-            cp.resize(res.size());
+            int n = res.size();
+            cp.resize(n);
             copy(res.begin(), res.end(), cp.begin());
-            a.resize(res.size());
-            for (int64_t k = 1; k <= p.second; ++k) {
-                int64_t t = pow_int(p.first, k);
-                for (int64_t i = 0; i < a.size(); ++i) a[i] = cp[i] * t;
+            a.resize(n);
+            T t = 1;
+            for (int k = 0; k < p.second; ++k) {
+                t *= p.first;
+                for (int i = 0; i < n; ++i) a[i] = cp[i] * t;
                 merge(res.begin(), res.end(), a.begin(), a.end(),
                       back_inserter(b));
                 swap(res, b);
                 b.clear();
             }
         }
-
         return res;
     }
 
     // 因数分解列挙
-    vector<vector<int64_t>> factorization(int64_t n) {
-        vector<vector<int64_t>> res;
-
-        auto f = [&](auto self, vector<int64_t> v, int64_t a) -> void {
+    template <class T>
+    vector<vector<T>> factorization(T x) {
+        vector<vector<T>> res;
+        auto f = [&](auto self, vector<T> v, T a) -> void {
             if (a == 1) res.emplace_back(v);
             for (auto i : divisors(a)) {
                 if (i == 1 || (!v.empty() && v.back() > i)) continue;
@@ -99,8 +90,7 @@ struct prime_number {
                 v.pop_back();
             }
         };
-
-        f(f, vector<int64_t>(), n);
+        f(f, vector<T>(), x);
         return res;
     }
 };
