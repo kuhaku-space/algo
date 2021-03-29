1a51016f64181851b0988ea1cb46fb96b3bf4712
kuhaku-space <te4958yu6924@gmail.com>
2021-03-30 02:50:03 +0900

    Create prime_number_fast.hpp

---
+++ math/prime_number_fast.hpp
@@ -0,0 +1,43 @@
+#include "_base.hpp"
+
+struct prime_number {
+    const int sz = 1 << 22;
+    vector<int> factor;
+
+    prime_number() { init(); }
+
+    void init() {
+        factor.resize(sz);
+        for (int i = 2; i < sz; ++i) {
+            if (factor[i] < i) continue;
+            if ((int64_t)i * i > sz) continue;
+            for (int j = i * i; j < sz; j += i) {
+                if (factor[j] == j) factor[j] = i;
+            }
+        }
+    }
+
+    bool is_prime(int x) {
+        assert(0 <= x && x < sz);
+        return factor[x] == x;
+    }
+
+    vector<int> prime_numbers(int x) {
+        vector<int> res;
+        for (int i = 2; i <= x; ++i) {
+            if (is_prime(i)) res.emplace_back(i);
+        }
+        return res;
+    }
+
+    // 素因数分解
+    vector<int> prime_factorization(int x) {
+        vector<int> res;
+        while (x > 1) {
+            res.emplace_back(factor[x]);
+            x /= factor[x];
+        }
+        return res;
+    }
+};
+prime_number pn;
