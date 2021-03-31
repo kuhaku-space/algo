dd1d1c0a84eb821a498cad512e9ff5b8024d4ec0
kuhaku-space <te4958yu6924@gmail.com>
2021-03-31 23:40:07 +0900

    Update doubling.hpp

--- algo/doubling.hpp
+++ algo/doubling.hpp
@@ -2,24 +2,22 @@
 
 struct Doubling {
     const int size = 64;
-    int64_t n;
-    vector<vector<int64_t>> data;
+    int n;
+    vector<vector<int>> data;
 
-    Doubling(int64_t _n) : n(_n) {
-        data = vector<vector<int64_t>>(size, vector<int64_t>(n));
-    }
+    Doubling(int _n) : n(_n), data(size, vector<int>(_n)) {}
 
-    void build(const vector<int64_t> &v) {
-        for (int64_t i = 0; i < n; ++i) data[0][i] = v[i];
+    void build(const vector<int> &v) {
+        for (int i = 0; i < n; ++i) data[0][i] = v[i];
 
-        for (int64_t i = 0; i < size - 1; ++i) {
-            for (int64_t j = 0; j < n; ++j) {
+        for (int i = 0; i < size - 1; ++i) {
+            for (int j = 0; j < n; ++j) {
                 data[i + 1][j] = data[i][data[i][j]];
             }
         }
     }
 
-    int64_t solve(int64_t f, int64_t k) {
+    int solve(int f, int64_t k) {
         for (int cnt = 0; k > 0; k >>= 1, ++cnt) {
             if (k & 1) f = data[cnt][f];
         }
