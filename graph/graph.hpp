bb212b5cebd92875a26e169e15693cb60aae1d6c
kuhaku-space <te4958yu6924@gmail.com>
2021-03-31 23:41:05 +0900

    Update graph.hpp

--- graph/graph.hpp
+++ graph/graph.hpp
@@ -25,10 +25,33 @@ struct Graph {
     int size() const { return V; }
 
     void add_edge(int a, int b, T d = T(1)) {
-        edges[a].push_back(edge{a, b, d});
+        edges[a].emplace_back(edge{a, b, d});
     }
     void add_edges(int a, int b, T d = T(1)) {
         edges[a].push_back(edge{a, b, d});
         edges[b].push_back(edge{b, a, d});
+    }
+};
+
+template <>
+struct Graph<void> {
+    int V;
+    vector<vector<int>> edges;
+
+    Graph(int v) : V(v), edges(v) {}
+
+    const auto &operator[](int i) const { return edges[i]; }
+    auto &operator[](int i) { return edges[i]; }
+    const auto begin() const { return edges.begin(); }
+    auto begin() { return edges.begin(); }
+    const auto end() const { return edges.end(); }
+    auto end() { return edges.end(); }
+
+    int size() const { return V; }
+
+    void add_edge(int a, int b) { edges[a].emplace_back(b); }
+    void add_edges(int a, int b) {
+        edges[a].emplace_back(b);
+        edges[b].emplace_back(a);
     }
 };
