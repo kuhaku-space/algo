2ad3a564fe28ef43ceb9103a02e7b31e74e9622b
kuhaku-space <te4958yu6924@gmail.com>
2021-03-31 23:40:51 +0900

    Update heap.hpp

--- data_struct/heap.hpp
+++ data_struct/heap.hpp
@@ -3,30 +3,29 @@
 template <class T>
 struct heap {
     vector<T> data;
-    int64_t sz;
+    int sz;
 
-    heap() : data(1 << 20, numeric_limit<T>::min()), sz(0) {}
+    heap() : data(1), sz(0) {}
 
     void push(T x) {
-        data[++sz] = x;
-        for (int it = sz; it > 1 && data[it] > data[it / 2]; it >>= 1) {
-            swap(data[it], data[it / 2]);
+        data.emplace_back(x);
+        for (int it = ++sz; it > 1 && data[it] > data[it >> 1]; it >>= 1) {
+            swap(data[it], data[it >> 1]);
         }
     }
 
-    T top() const { return data[1]; }
+    auto top() const { return data[1]; }
 
     void pop() {
-        data[1] = data[sz];
-        data[sz--] = numeric_limits<T>::min();
-        for (int it = 1; data[it] < min(data[it * 2], data[it * 2 + 1]);) {
+        data[1] = data[sz--];
+        data.pop_back();
+        for (int it = 1; data[it] < min(data[it << 1], data[(it << 1) | 1]);) {
             it <<= 1;
-            if (data[it] < data[it + 1]) ++it;
+            if (data[it] < data[it | 1]) ++it;
             swap(data[it], data[it >> 1]);
         }
     }
 
     int size() const { return sz; }
-
     bool empty() const { return sz == 0; }
 };
