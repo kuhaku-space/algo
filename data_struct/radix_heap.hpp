d7f43d081aaa8fb09ec091d12c8cecb84c2ce92a
kuhaku-space <te4958yu6924@gmail.com>
2021-03-31 23:48:31 +0900

    Create radix_heap.hpp

---
+++ data_struct/radix_heap.hpp
@@ -0,0 +1,89 @@
+#include "_base.hpp"
+
+struct radix_heap {
+    vector<unsigned int> v[33];
+    unsigned int last, sz;
+    bool is_top;
+
+    radix_heap() : last(), sz(), is_top() {}
+
+    int bsr(unsigned int x) {
+        if (x == 0) return -1;
+        return __builtin_clz(x);
+    }
+
+    void push(unsigned int x) {
+        assert(x >= last);
+        ++sz;
+        v[bsr(x ^ last) + 1].emplace_back(x);
+    }
+
+    unsigned int top() {
+        if (is_top) pop();
+        is_top = true;
+        return last;
+    }
+
+    void pop() {
+        if (is_top) {
+            is_top = false;
+            return;
+        }
+        assert(sz);
+        if (!v[0].size()) {
+            int i = 1;
+            while (!v[i].size()) ++i;
+            auto new_last = *min_element(v[i].begin(), v[i].end());
+            for (auto x : v[i]) {
+                v[bsr(x ^ new_last) + 1].emplace_back(x);
+            }
+            last = new_last;
+            v[i].clear();
+        }
+        --sz;
+        v[0].pop_back();
+    }
+};
+
+struct radix_heap_64 {
+    vector<uint64_t> v[33];
+    uint64_t last, sz;
+    bool is_top;
+
+    radix_heap_64() : last(), sz(), is_top() {}
+
+    int bsr(uint64_t x) {
+        if (x == 0) return -1;
+        return __builtin_clz(x);
+    }
+
+    void push(uint64_t x) {
+        assert(x >= last);
+        ++sz;
+        v[bsr(x ^ last) + 1].emplace_back(x);
+    }
+
+    uint64_t top() {
+        if (!is_top) pop();
+        is_top = true;
+        return last;
+    }
+
+    void pop() {
+        if (is_top) {
+            is_top = false;
+            return;
+        }
+        assert(sz);
+        if (!v[0].size()) {
+            int i = 1;
+            while (!v[i].size()) ++i;
+            auto new_last = *min_element(v[i].begin(), v[i].end());
+            for (auto x : v[i]) v[bsr(x ^ new_last) + 1].emplace_back(x);
+            last = new_last;
+            v[i].clear();
+        }
+        --sz;
+        v[0].pop_back();
+    }
+};
