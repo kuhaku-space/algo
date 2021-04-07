6bffbf26e4690577006629f5d577d276ac9f5c21
kuhaku-space <te4958yu6924@gmail.com>
2021-04-08 08:12:44 +0900

    Update topological_sort.hpp

--- graph/topological_sort.hpp
+++ graph/topological_sort.hpp
@@ -6,22 +6,22 @@
 template <class T>
 bool is_cycle(const Graph<T> &g) {
     int n = g.size();
-    bitset<1 << 20> is_seen, is_finish;
+    vector<bool> is_seen(n), is_finish(n);
     bool res = false;
 
     auto dfs = [&](auto self, int idx) {
         if (is_finish[idx]) return;
-        is_seen.set(idx);
+        is_seen[idx] = true;
         for (auto &e : g[idx]) {
             if (res |= is_seen[e.to]) return;
             self(self, e.to);
         }
-        is_seen.reset(idx);
-        is_finish.set(idx);
+        is_seen[idx] = false;
+        is_finish[idx] = true;
     };
 
     for (int i = 0; i < n; ++i) {
-        is_seen.reset();
+        is_seen.assign(n, false);
         dfs(dfs, i);
     }
     return res;
@@ -33,10 +33,10 @@ template <class T>
 vector<int> topological_sort(const Graph<T> &g) {
     int n = g.size();
     vector<int> res;
-    bitset<1 << 20> is_seen;
+    vector<bool> is_seen;
     auto dfs = [&](auto self, int v) {
         if (is_seen[v]) return;
-        is_seen.set(v);
+        is_seen[v] = true;
         for (auto &e : g[v])
             if (!is_seen[e.to]) self(self, e.to);
         res.emplace_back(v);
