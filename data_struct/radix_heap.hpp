e1c6113b9e8f6f8fda1689207db4cb9a6bbd1b29
kuhaku-space <te4958yu6924@gmail.com>
2021-04-08 05:48:40 +0900

    Update radix_heap.hpp

--- data_struct/radix_heap.hpp
+++ data_struct/radix_heap.hpp
@@ -9,7 +9,7 @@ struct radix_heap {
 
     int bsr(unsigned int x) {
         if (x == 0) return -1;
-        return __builtin_clz(x);
+        return 31-__builtin_clz(x);
     }
 
     void push(unsigned int x) {
@@ -46,7 +46,7 @@ struct radix_heap {
 };
 
 struct radix_heap_64 {
-    vector<uint64_t> v[33];
+    vector<uint64_t> v[65];
     uint64_t last, sz;
     bool is_top;
 
@@ -54,7 +54,7 @@ struct radix_heap_64 {
 
     int bsr(uint64_t x) {
         if (x == 0) return -1;
-        return __builtin_clz(x);
+        return 63-__builtin_clz(x);
     }
 
     void push(uint64_t x) {
@@ -84,6 +84,60 @@ struct radix_heap_64 {
             v[i].clear();
         }
         --sz;
+        v[0].pop_back();
+    }
+};
+
+template <class T>
+struct radix_heap_dijkstra {
+    vector<T> v[33];
+    T last;
+    unsigned int sz;
+    bool is_top;
+
+    radix_heap() : last(), sz(), is_top() {}
+
+    int bsr(unsigned int x) {
+        if (x == 0) return -1;
+        return 31-__builtin_clz(x);
+    }
+
+    bool empty() const {
+        return sz == 0;
+    }
+    auto size() const {
+        return sz;
+    }
+
+    void push(T x) {
+        assert(x.dist >= last.dist);
+        ++sz;
+        v[bsr(x.dist ^ last.dist) + 1].emplace_back(x);
+    }
+
+    auto top() {
+        if (!is_top) pop();
+        is_top = true;
+        return last;
+    }
+
+    void pop() {
+        if (is_top) {
+            is_top = false;
+            return;
+        }
+        assert(sz);
+        if (!v[0].size()) {
+            int i = 1;
+            while (!v[i].size()) ++i;
+            auto new_last = *min_element(v[i].begin(), v[i].end());
+            for (auto x : v[i]) {
+                v[bsr(x.dist ^ new_last.dist) + 1].emplace_back(x);
+            }
+            v[i].clear();
+        }
+        --sz;
+        last = v[0].back();
         v[0].pop_back();
     }
 };
