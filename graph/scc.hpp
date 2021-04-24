aa3f321c8a971fd90a98f428058971d7e10615ee
kuhaku-space <te4958yu6924@gmail.com>
2021-04-24 20:03:15 +0900

    Update scc.hpp

--- graph/scc.hpp
+++ graph/scc.hpp
@@ -38,8 +38,7 @@ vector<int> scc(const Graph<T> &g) {
 
 template <class T>
 Graph<T> make_DAG(const Graph<T> &g, const vector<int> &v) {
-    int n = *max_element(v.begin(), v.end()) + 1;
-    Graph<T> res(n);
+    Graph<T> res(*max_element(v.begin(), v.end()) + 1);
     for (auto &es : g) {
         for (auto &e : es) {
             int x = v[e.from], y = v[e.to];
@@ -49,10 +48,42 @@ Graph<T> make_DAG(const Graph<T> &g, const vector<int> &v) {
     return res;
 }
 
+vector<int> scc(const Graph<void> &g) {
+    int n = g.size();
+    Graph<void> rg(n);
+    vector<int> comp(n, -1), order;
+    vector<bool> used(n);
+
+    for (int i = 0; i < n; ++i) {
+        for (auto &e : g[i]) rg.add_edge(e, i);
+    }
+
+    auto dfs = [&](auto self, int idx) {
+        if (used[idx]) return;
+        used[idx] = true;
+        for (auto &e : g[idx]) self(self, e);
+        order.emplace_back(idx);
+    };
+    auto rdfs = [&](auto self, int idx, int cnt) {
+        if (~comp[idx]) return;
+        comp[idx] = cnt;
+        for (auto &e : rg[idx]) self(self, e, cnt);
+    };
+
+    for (int i = 0; i < n; i++) dfs(dfs, i);
+    reverse(order.begin(), order.end());
+    int ptr = 0;
+    for (auto i : order) {
+        if (comp[i] == -1) rdfs(rdfs, i, ptr++);
+    }
+
+    return comp;
+};
+
 Graph<void> make_DAG(const Graph<void> &g, const vector<int> &v) {
-    int n = *max_element(v.begin(), v.end()) + 1;
-    Graph<void> res(n);
-    for (int i = 0; i < g.size(); ++i) {
+    int n = g.size();
+    Graph<void> res(*max_element(v.begin(), v.end()) + 1);
+    for (int i = 0; i < n; ++i) {
         for (auto &e : g[i]) {
             int x = v[i], y = v[e];
             if (x != y) res.add_edge(x, y);
