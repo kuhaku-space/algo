3b1851f6a76c9fdc542d81738b5f7a56b9772bb9
kuhaku-space <te4958yu6924@gmail.com>
2021-06-07 22:35:42 +0900

    Update dijkstra.hpp

--- graph/dijkstra.hpp
+++ graph/dijkstra.hpp
@@ -21,27 +21,6 @@ vector<T> dijkstra(const Graph<T> &g, int s = 0,
     };
     vector<T> dist(g.size(), inf);
     priority_queue<_edge, vector<_edge>, greater<_edge>> p_que;
-    p_que.push(_edge{s, T()});
-    while (!p_que.empty()) {
-        _edge e = p_que.top();
-        p_que.pop();
-        if (chmin(dist[e.to], e.dist))
-            for (auto &i : g[e.to]) p_que.push(_edge{i.to, e.dist + i.dist});
-    }
-    return dist;
-}
-
-template <class T>
-vector<T> dijkstra_fast(const Graph<T> &g, int s = 0,
-                        T inf = numeric_limits<T>::max()) {
-    struct _edge {
-        int to;
-        T dist;
-        bool operator<(const _edge &rhs) const { return dist < rhs.dist; }
-        bool operator>(const _edge &rhs) const { return rhs < *this; }
-    };
-    vector<T> dist(g.size(), inf);
-    priority_queue<_edge, vector<_edge>, greater<_edge>> p_que;
     dist[s] = T();
     p_que.push(_edge{s, T()});
     while (!p_que.empty()) {
@@ -57,8 +36,29 @@ vector<T> dijkstra_fast(const Graph<T> &g, int s = 0,
 }
 
 template <class T>
-vector<T> dijkstra_faster(const Graph<T> &g, int s = 0,
-                          T inf = numeric_limits<T>::max()) {
+vector<T> dijkstra_late(const Graph<T> &g, int s = 0,
+                        T inf = numeric_limits<T>::max()) {
+    struct _edge {
+        int to;
+        T dist;
+        bool operator<(const _edge &rhs) const { return dist < rhs.dist; }
+        bool operator>(const _edge &rhs) const { return rhs < *this; }
+    };
+    vector<T> dist(g.size(), inf);
+    priority_queue<_edge, vector<_edge>, greater<_edge>> p_que;
+    p_que.push(_edge{s, T()});
+    while (!p_que.empty()) {
+        _edge e = p_que.top();
+        p_que.pop();
+        if (chmin(dist[e.to], e.dist))
+            for (auto &i : g[e.to]) p_que.push(_edge{i.to, e.dist + i.dist});
+    }
+    return dist;
+}
+
+template <class T>
+vector<T> dijkstra_fast(const Graph<T> &g, int s = 0,
+                        T inf = numeric_limits<T>::max()) {
     struct _edge {
         int to;
         T dist;
