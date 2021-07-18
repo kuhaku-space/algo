e72d168cfa55cb061fe399f5f206ff7f8a51d7a0
kuhaku-space <te4958yu6924@gmail.com>
2021-07-19 01:55:44 +0900

    Update cht.hpp

--- geometry/cht.hpp
+++ geometry/cht.hpp
@@ -9,13 +9,10 @@ struct CHT {
     vector<pair<T, T>> lines;
     F comp;
     int head;
-    CHT(T _e, F _comp) : comp(_comp), head(0) {
-        lines.emplace_back(0, _e);
-    };
+    CHT(T _e, F _comp) : comp(_comp), head(0) { lines.emplace_back(0, _e); };
 
     bool check(pair<T, T> l1, pair<T, T> l2, pair<T, T> l3) {
-        if (l1 < l3)
-            swap(l1, l3);
+        if (l1 < l3) swap(l1, l3);
         return (l3.second - l2.second) * (l2.first - l1.first) >=
                (l2.second - l1.second) * (l3.first - l2.first);
     }
@@ -28,12 +25,8 @@ struct CHT {
         lines.emplace_back(line);
     }
 
-    T f(int i, T x) {
-        return lines[i].first * x + lines[i].second;
-    }
-    T f(pair<T, T> line, T x) {
-        return line.first * x + line.second;
-    }
+    T f(int i, T x) { return lines[i].first * x + lines[i].second; }
+    T f(pair<T, T> line, T x) { return line.first * x + line.second; }
 
     T get(T x) {
         // while(lines.size() >= 2 + head && comp(f(head, x), f(head + 1, x))){
@@ -43,7 +36,7 @@ struct CHT {
         int low = -1, high = lines.size() - 1;
         while (high - low > 1) {
             int mid = (high + low) / 2;
-            if(comp(f(mid, x), f(mid + 1, x)))
+            if (comp(f(mid, x), f(mid + 1, x)))
                 low = mid;
             else
                 high = mid;
