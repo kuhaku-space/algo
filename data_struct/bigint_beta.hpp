e13d5143a6d4533f47f2183457c40a346ddb1c6f
kuhaku-space <te4958yu6924@gmail.com>
2021-07-19 01:55:31 +0900

    Update bigint_beta.hpp

--- data_struct/bigint_beta.hpp
+++ data_struct/bigint_beta.hpp
@@ -12,11 +12,10 @@ struct BigInt {
     vector<int> data;
 
     BigInt() {}
-    BigInt (string s) : data(s.size()) {
+    BigInt(string s) : data(s.size()) {
         bool flg = s[0] == '-';
         reverse(s.begin(), s.end());
-        if (flg)
-            s.pop_back(), data.pop_back();
+        if (flg) s.pop_back(), data.pop_back();
         for (int i = 0; i < s.size(); ++i) {
             if (flg)
                 data[i] = -(s[i] - '0');
@@ -28,26 +27,23 @@ struct BigInt {
     BigInt &operator+=(const BigInt &rhs) {
         int n = max(size(), rhs.size());
         data.resize(n, 0);
-        for (int i = 0; i < rhs.size(); ++i) 
-            data[i] += rhs.data[i];
+        for (int i = 0; i < rhs.size(); ++i) data[i] += rhs.data[i];
         format();
         return *this;
     }
     BigInt &operator-=(const BigInt &rhs) {
         int n = max(size(), rhs.size());
         data.resize(n, 0);
-        for (int i = 0; i < rhs.size(); ++i) 
-            data[i] -= rhs.data[i];
+        for (int i = 0; i < rhs.size(); ++i) data[i] -= rhs.data[i];
         format();
         return *this;
     }
     BigInt &operator*=(const BigInt &rhs) {
         const int mod = 469762049;
-        NTT<469762049, 3> ntt; // 2^26 * 7 + 1
+        NTT<469762049, 3> ntt;  // 2^26 * 7 + 1
         ntt.convolution_self(data, rhs.data);
         for (int i = 0; i < size(); ++i) {
-            if (data[i] >= mod / 2)
-                data[i] -= mod;
+            if (data[i] >= mod / 2) data[i] -= mod;
         }
         format();
         return *this;
@@ -55,28 +51,18 @@ struct BigInt {
 
     BigInt operator-() const {
         BigInt res(*this);
-        for (int i = 0; i < res.size(); ++i) 
-            res.data[i] = -res.data[i];
+        for (int i = 0; i < res.size(); ++i) res.data[i] = -res.data[i];
         return res;
     }
-    BigInt operator+(const BigInt &rhs) const {
-        return BigInt(*this) += rhs;
-    }
-    BigInt operator-(const BigInt &rhs) const {
-        return BigInt(*this) -= rhs;
-    }
-    BigInt operator*(const BigInt &rhs) const {
-        return BigInt(*this) *= rhs;
-    }
+    BigInt operator+(const BigInt &rhs) const { return BigInt(*this) += rhs; }
+    BigInt operator-(const BigInt &rhs) const { return BigInt(*this) -= rhs; }
+    BigInt operator*(const BigInt &rhs) const { return BigInt(*this) *= rhs; }
 
     bool operator<(const BigInt &rhs) const {
-        if (is_negative() ^ rhs.is_negative())
-            return back() < rhs.back();
-        if (size() ^ rhs.size())
-            return size() < rhs.size();
+        if (is_negative() ^ rhs.is_negative()) return back() < rhs.back();
+        if (size() ^ rhs.size()) return size() < rhs.size();
         for (int i = size() - 1; i >= 0; --i) {
-            if (data[i] ^ rhs.data[i])
-                return data[i] < rhs.data[i];
+            if (data[i] ^ rhs.data[i]) return data[i] < rhs.data[i];
         }
         return false;
     }
@@ -85,12 +71,12 @@ struct BigInt {
         string s;
         bool flg = rhs.is_negative();
         for (auto i : rhs.data) {
-            if (flg) s.push_back(char('0' - i));
+            if (flg)
+                s.push_back(char('0' - i));
             else
                 s.push_back(char('0' + i));
         }
-        if (rhs.is_negative())
-            s.push_back('-');
+        if (rhs.is_negative()) s.push_back('-');
         reverse(s.begin(), s.end());
         os << s;
         return os;
@@ -103,22 +89,16 @@ struct BigInt {
         return is;
     }
 
-    inline int size() const {
-        return data.size();
-    }
+    inline int size() const { return data.size(); }
 
     int back() const {
         assert(data.size() > 0);
         return data.back();
     }
 
-    bool is_negative() const {
-        return back() < 0;
-    }
+    bool is_negative() const { return back() < 0; }
 
-    inline bool is_zero() const {
-        return data.size() == 1 && data[0] == 0;
-    }
+    inline bool is_zero() const { return data.size() == 1 && data[0] == 0; }
 
     void format() {
         while (size() > 1 && back() == 0) data.pop_back();
@@ -127,10 +107,8 @@ struct BigInt {
         for (int i = 0; i < size() - 1; ++i) {
             data[i + 1] += data[i] / 10;
             data[i] %= 10;
-            if (flg && data[i] > 0)
-                ++data[i + 1], data[i] -= 10;
-            if (!flg && data[i] < 0)
-                --data[i + 1], data[i] += 10;
+            if (flg && data[i] > 0) ++data[i + 1], data[i] -= 10;
+            if (!flg && data[i] < 0) --data[i + 1], data[i] += 10;
         }
         while (size() > 1 && back() == 0) data.pop_back();
     }
