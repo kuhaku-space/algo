5243351862b39e032c8977e47073799ea40e8be1
kuhaku-space <te4958yu6924@gmail.com>
2021-07-26 03:56:16 +0900

    Update graph.hpp

--- graph/graph.hpp
+++ graph/graph.hpp
@@ -24,8 +24,30 @@ struct Graph {
         edges[a].emplace_back(edge{a, b, d});
     }
     void add_edges(int a, int b, T d = T(1)) {
-        edges[a].push_back(edge{a, b, d});
-        edges[b].push_back(edge{b, a, d});
+        edges[a].emplace_back(edge{a, b, d});
+        edges[b].emplace_back(edge{b, a, d});
+    }
+    void input_edge(int m, bool zero_based = false) {
+        for (int i = 0; i < m; ++i) {
+            int a, b;
+            T d;
+            cin >> a >> b >> d;
+            if (zero_based)
+                this->add_edge(a, b, d);
+            else
+                this->add_edge(a - 1, b - 1, d);
+        }
+    }
+    void input_edges(int m, bool zero_based = false) {
+        for (int i = 0; i < m; ++i) {
+            int a, b;
+            T d;
+            cin >> a >> b >> d;
+            if (zero_based)
+                this->add_edges(a, b, d);
+            else
+                this->add_edges(a - 1, b - 1, d);
+        }
     }
 };
 
@@ -46,5 +68,25 @@ struct Graph<void> {
     void add_edges(int a, int b) {
         edges[a].emplace_back(b);
         edges[b].emplace_back(a);
+    }
+    void input_edge(int m, bool zero_based = false) {
+        for (int i = 0; i < m; ++i) {
+            int a, b;
+            cin >> a >> b;
+            if (zero_based)
+                this->add_edge(a, b);
+            else
+                this->add_edge(a - 1, b - 1);
+        }
+    }
+    void input_edges(int m, bool zero_based = false) {
+        for (int i = 0; i < m; ++i) {
+            int a, b;
+            cin >> a >> b;
+            if (zero_based)
+                this->add_edges(a, b);
+            else
+                this->add_edges(a - 1, b - 1);
+        }
     }
 };
