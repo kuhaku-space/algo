f39aab84019ec8cde0d02cfedf6a479472bfcaf9
kuhaku-space <te4958yu6924@gmail.com>
2021-08-15 22:40:37 +0900

    Update union_find.hpp

--- tree/union_find.hpp
+++ tree/union_find.hpp
@@ -1,20 +1,29 @@
 #include "_base.hpp"
 
+/*
+ * verify :
+ * https://atcoder.jp/contests/abc214/tasks/abc214_d 21/08/15
+ */
+
 struct union_find {
     vector<int> data;
 
-    union_find(int _n) : data(_n) {}
+    union_find(int _n) : data(_n, -1) {}
 
     int root(int x) {
         return data[x] < 0 ? x : data[x] = root(data[x]);
+    }
+
+    bool is_root(int x) {
+        return data[x] < 0;
     }
 
     void unite(int x, int y) {
         x = root(x), y = root(y);
         if (x != y) {
             if (data[y] < data[x]) swap(x, y);
-            data[y] = x;
             data[x] += data[y];
+            data[y] = x;
         }
     }
 
