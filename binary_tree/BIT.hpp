c698c467c79f6fae786b42c0bd55cea52244197f
kuhaku-space <te4958yu6924@gmail.com>
2021-09-03 12:39:40 +0900

    Update BIT.hpp

--- binary_tree/BIT.hpp
+++ binary_tree/BIT.hpp
@@ -3,20 +3,22 @@
 // 一点加算、区間総和
 template <class T>
 struct BIT {
-    const int N;
+    int N;
     vector<T> data;
 
     BIT() {}
     BIT(int n, T e = T(0)) : N(n + 1), data(n + 1, e) {}
-    BIT(const vector<T> &v) : N(v.size() + 1), data(v.size() + 1) { build(v); }
+    BIT(const vector<T> &v) : N(v.size() + 1), data(v.size() + 1) {
+        this->build(v);
+    }
 
-    const T &operator[](int i) const { return at(i); }
-    const T at(int k) const { return sum(k + 1) - sum(k); }
+    const T &operator[](int i) const { return this->at(i); }
+    const T at(int k) const { return this->sum(k + 1) - this->sum(k); }
 
     template <class U>
     void build(const vector<U> &v) {
         int n = v.size();
-        for (int i = 0; i < n; ++i) add(i, v[i]);
+        for (int i = 0; i < n; ++i) this->add(i, v[i]);
     }
 
     // v[k] += w
@@ -26,7 +28,7 @@ struct BIT {
     }
 
     // v[k] = x
-    void update(int k, T x) { add(k, x - at(k)); }
+    void update(int k, T x) { this->add(k, x - this->at(k)); }
 
     // v[0] + ... + v[k - 1]
     T sum(int k) const {
@@ -36,7 +38,7 @@ struct BIT {
         return res;
     }
     // v[a] + ... + v[b - 1]
-    T sum(int a, int b) const { return sum(b) - sum(a); }
+    T sum(int a, int b) const { return this->sum(b) - this->sum(a); }
 
     int lower_bound(T x) const {
         if (x <= 0) return 0;
