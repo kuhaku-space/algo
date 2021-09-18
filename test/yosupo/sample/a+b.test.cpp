0cd9ef7068dea66bf71bd16670d41d4eb1259482
kuhaku-space <te4958yu6924@gmail.com>
2021-09-18 15:01:36 +0900

    Create a+b.test.cpp

---
+++ test/yosupo/sample/a+b.test.cpp
@@ -0,0 +1,9 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include <bits/stdc++.h>
+
+int main(void) {
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a + b << std::endl;
+    return 0;
+}
