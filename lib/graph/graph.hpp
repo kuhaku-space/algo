c31a0b0d210b68d7ca0bf916eb3d34a41ebee305
kuhaku-space <te4958yu6924@gmail.com>
2021-09-22 06:52:24 +0900

    Update graph.hpp

--- lib/graph/graph.hpp
+++ lib/graph/graph.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "template/template.hpp"
 
 template <class T>
@@ -6,26 +7,24 @@ struct Graph {
         int from, to;
         T dist;
 
-        bool operator<(const edge &rhs) const { return dist < rhs.dist; }
+        bool operator<(const edge &rhs) const { return this->dist < rhs.dist; }
         bool operator>(const edge &rhs) const { return rhs < *this; }
     };
     vector<vector<edge>> edges;
 
     Graph(int v) : edges(v) {}
 
-    const auto &operator[](int i) const { return edges[i]; }
-    auto &operator[](int i) { return edges[i]; }
-    const auto begin() const { return edges.begin(); }
-    auto begin() { return edges.begin(); }
-    const auto end() const { return edges.end(); }
-    auto end() { return edges.end(); }
-    auto size() const { return edges.size(); }
-    void add_edge(int a, int b, T d = T(1)) {
-        edges[a].emplace_back(edge{a, b, d});
-    }
+    const auto &operator[](int i) const { return this->edges[i]; }
+    auto &operator[](int i) { return this->edges[i]; }
+    const auto begin() const { return this->edges.begin(); }
+    auto begin() { return this->edges.begin(); }
+    const auto end() const { return this->edges.end(); }
+    auto end() { return this->edges.end(); }
+    auto size() const { return this->edges.size(); }
+    void add_edge(int a, int b, T d = T(1)) { this->edges[a].emplace_back(edge{a, b, d}); }
     void add_edges(int a, int b, T d = T(1)) {
-        edges[a].emplace_back(edge{a, b, d});
-        edges[b].emplace_back(edge{b, a, d});
+        this->edges[a].emplace_back(edge{a, b, d});
+        this->edges[b].emplace_back(edge{b, a, d});
     }
     void input_edge(int m, bool zero_based = false) {
         for (int i = 0; i < m; ++i) {
@@ -57,17 +56,17 @@ struct Graph<void> {
 
     Graph(int v) : edges(v) {}
 
-    const auto &operator[](int i) const { return edges[i]; }
-    auto &operator[](int i) { return edges[i]; }
-    const auto begin() const { return edges.begin(); }
-    auto begin() { return edges.begin(); }
-    const auto end() const { return edges.end(); }
-    auto end() { return edges.end(); }
-    auto size() const { return edges.size(); }
-    void add_edge(int a, int b) { edges[a].emplace_back(b); }
+    const auto &operator[](int i) const { return this->edges[i]; }
+    auto &operator[](int i) { return this->edges[i]; }
+    const auto begin() const { return this->edges.begin(); }
+    auto begin() { return this->edges.begin(); }
+    const auto end() const { return this->edges.end(); }
+    auto end() { return this->edges.end(); }
+    auto size() const { return this->edges.size(); }
+    void add_edge(int a, int b) { this->edges[a].emplace_back(b); }
     void add_edges(int a, int b) {
-        edges[a].emplace_back(b);
-        edges[b].emplace_back(a);
+        this->edges[a].emplace_back(b);
+        this->edges[b].emplace_back(a);
     }
     void input_edge(int m, bool zero_based = false) {
         for (int i = 0; i < m; ++i) {
