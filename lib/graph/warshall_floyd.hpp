d9648a5143fba5898b1f698c5f9e4199638195e5
kuhaku-space <te4958yu6924@gmail.com>
2021-09-22 06:52:45 +0900

    Update warshall_floyd.hpp

--- lib/graph/warshall_floyd.hpp
+++ lib/graph/warshall_floyd.hpp
@@ -1,8 +1,9 @@
+#pragma once
 #include "template/template.hpp"
-#include "graph/graph_matrix.hpp"
+#include "graph/matrix_graph.hpp"
 
 template <class T>
-void warshall_floyd(Graph<T> &g) {
+void warshall_floyd(matrix_graph<T> &g) {
     int n = g.size();
     for (int i = 0; i < n; ++i) g[i][i] = 0;
     for (int k = 0; k < n; ++k) {
