e5ae26e83f6e215712dea437f7b2362ec5c0777e
kuhaku-space <te4958yu6924@gmail.com>
2021-09-23 07:20:32 +0900

    Update geometry.hpp

--- lib/geometry/geometry.hpp
+++ lib/geometry/geometry.hpp
@@ -1,67 +1,212 @@
 #include "template/template.hpp"
 
-template <typename T>
-struct geometry {
-    inline bool eq(T a, T b = T(0)) { return a == b; }
-    using Point = complex<T>;
-    const Point PINF = Point(INF, INF);
-    using Points = vector<Point>;
+template <class T, class U>
+constexpr bool eq(T a, U b) {
+    return abs(a - b) < EPS;
+}
+template <class T>
+struct Point {
+    T x, y;
+    constexpr Point() : x(), y() {}
+    constexpr Point(T _x, T _y = T()) : x(_x), y(_y) {}
 
-    struct Line {
-        // a * x + b * y = c
-        T a, b, c;
-
-        Line(T a, T b, T c) : a(a), b(b), c(c) {}
-
-        Line(Point p1, Point p2) { drow(p1, p2); }
-
-        bool operator==(const Line &p) const {
-            return eq(a * p.b, b * p.a) && eq(a * p.c, c * p.a);
-        }
-
-        void drow(Point p1, Point p2) {
-            a = (p2.imag() - p1.imag());
-            b = (p1.real() - p2.real());
-            c = a * p1.real() + b * p1.imag();
-        }
-
-        T dist(Point p) {
-            T res = a * p.real() + b * p.imag() - c;
-            if (res < T(0)) res *= T(-1);
-            res /= a * a + b * b;
-            return res;
-        }
-
-        bool is_parallel(Line l) { return eq(a * l.b, b * l.a); }
-
-        bool in_line(Point p) { return eq(a * p.real() + b * p.imag(), c); }
-    };
-
-    Point rotate(const Point p, const Point center, const double theta) {
-        const Point d = p - center;
-        return center + d * Point(cos(theta), sin(theta));
-        // return center + d * Point(cosl(theta), sinl(theta));
+    constexpr Point &operator+=(const Point &rhs) {
+        this->x += rhs.x, this->y += rhs.y;
+        return *this;
     }
-
-    Point intersection(Line l1, Line l2) {
-        T det = l1.a * l2.b - l2.a * l1.b;
-        if (eq(det)) return PINF;
-
-        Point res;
-        res.real((l2.b * l1.c - l1.b * l2.c) / det);
-        res.imag((l1.a * l2.c - l2.a * l1.c) / det);
-        return res;
+    constexpr Point &operator-=(const Point &rhs) {
+        this->x -= rhs.x, this->y -= rhs.y;
+        return *this;
     }
-
-    // 垂直二等分線
-    Line perpendicular_bisector(Point p1, Point p2) {
-        if (eq(p1, p2)) return Line(PINF, PINF);
-        Point m = (p1 + p2) / T(2);
-        return Line(m, rotate(p1, m, PI / 2));
+    constexpr Point &operator*=(T rhs) {
+        this->x *= rhs, this->y *= rhs;
+        return *this;
     }
+    constexpr Point &operator/=(T rhs) {
+        this->x /= rhs, this->y /= rhs;
+        return *this;
+    }
+    constexpr Point operator+(const Point &rhs) const { return Point(*this) += rhs; }
+    constexpr Point operator-(const Point &rhs) const { return Point(*this) -= rhs; }
+    constexpr Point operator*(T rhs) const { return Point(*this) *= rhs; }
+    constexpr Point operator/(T rhs) const { return Point(*this) /= rhs; }
 
-    Line drow_vertical_line(Point p, Line l) {
-        if (l.in_line(p)) return Line(Point(INF, INF), INF);
-        return Line(p, p + Point(l.a, l.b));
+    constexpr T norm() const { return this->x * this->x + this->y * this->y; }
+    T abs() const { return sqrt(this->norm()); }
+    constexpr T dot(const Point<T> &p) const { return this->x * p.x + this->y * p.y; }
+    constexpr T cross(const Point<T> &p) const { return this->x * p.y - this->y * p.x; }
+
+    friend istream &operator>>(istream &is, Point<T> &rhs) {
+        T x, y;
+        is >> x >> y;
+        rhs = Point<T>(x, y);
+        return (is);
+    }
+    friend ostream &operator<<(ostream &os, const Point<T> &rhs) {
+        return os << rhs.x << ' ' << rhs.y;
     }
 };
+
+template <class T>
+using Points = vector<Point<T>>;
+
+// ノルム
+template <class T>
+constexpr T norm(Point<T> p) {
+    return p.x * p.x + p.y * p.y;
+}
+// 絶対値
+template <class T>
+T abs(Point<T> p) {
+    return sqrt(norm(p));
+}
+// 内積
+template <class T>
+constexpr T dot(Point<T> a, Point<T> b) {
+    return a.x * b.x + a.y * b.y;
+}
+// 外積
+template <class T>
+constexpr T cross(Point<T> a, Point<T> b) {
+    return a.x * b.y - a.y * b.x;
+}
+
+template <class T>
+int ccw(const Point<T> &a, Point<T> b, Point<T> c) {
+    b -= a, c -= a;
+    if (cross(b, c) > EPS) return +1;   // "COUNTER_CLOCKWISE"
+    if (cross(b, c) < -EPS) return -1;  // "CLOCKWISE"
+    if (dot(b, c) < 0) return +2;       // "ONLINE_BACK"
+    if (norm(b) < norm(c)) return -2;   // "ONLINE_FRONT"
+    return 0;                           // "ON_SEGMENT"
+}
+
+template <class T>
+struct Line {
+    Point<T> a, b;
+
+    constexpr Line() : a(), b() {}
+    constexpr Line(const Point<T> &_a, const Point<T> &_b) : a(_a), b(_b) {}
+
+    constexpr bool in_line(Point<T> p) const { return eq(cross(p - a, p - b), 0); }
+
+    // 射影
+    constexpr Point<T> proj(const Point<T> &p) const {
+        return a + (b - a) * dot(b - a, p - a) / norm(b - a);
+    }
+    // 反射
+    constexpr Point<T> refl(const Point<T> &p) const { return this->proj(p) * 2 - p; }
+
+    T dist(Point<T> p) { return abs(p - this->proj(p)); }
+};
+
+template <class T>
+constexpr Point<T> PINF = Point<T>(numeric_limits<T>::max(), numeric_limits<T>::max());
+template <class T>
+constexpr Line<T> LINF = Line<T>(PINF<T>, PINF<T>);
+
+template <class T>
+bool parallel(const Line<T> &a, const Line<T> &b) {
+    return eq(cross(a.b - a.a, b.b - b.a), 0.0);
+}
+template <class T>
+bool orthogonal(const Line<T> &a, const Line<T> &b) {
+    return eq(dot(a.a - a.b, b.a - b.b), 0.0);
+}
+template <class T>
+bool intersection(const Line<T> &a, const Line<T> &b) {
+    return ccw(a.a, a.b, b.a) * ccw(a.a, a.b, b.b) <= 0 &&
+           ccw(b.a, b.b, a.a) * ccw(b.a, b.b, a.b) <= 0;
+}
+template <class T>
+Point<T> cross_point(const Line<T> &a, const Line<T> &b) {
+    T d = cross(b.b - b.a, b.b - a.a);
+    T e = cross(b.b - b.a, a.b - a.a);
+    if (eq(d, 0) && eq(e, 0)) return a.a;
+    if (eq(e, 0)) return PINF<T>;
+    return a.a + (a.b - a.a) * d / e;
+}
+
+// 垂直二等分線
+template <class T>
+Line<T> perpendicular_bisector(Point<T> p1, Point<T> p2) {
+    if (eq(p1, p2)) return LINF<T>;
+    Point<T> m = (p1 + p2) / 2.0L, d = p2 - p1;
+    return Line<T>(m, m + d * Point<T>(cos(PI / 2), sin(PI / 2)));
+}
+
+template <class T>
+Line<T> vertical_line(Point<T> p, Line<T> l) {
+    if (l.in_line(p)) return LINF<T>;
+    return Line<T>(p, l.proj(p));
+}
+
+template <class T>
+Point<T> rotate(const Point<T> &p, const Point<T> &center, const double theta) {
+    const Point<T> d = p - center;
+    return center + d * Point<T>(cos(theta), sin(theta));
+}
+
+template <class T>
+T triangle_area(Point<T> p1, Point<T> p2, Point<T> p3) {
+    p1 -= p3;
+    p2 -= p3;
+    return abs(p1.x * p2.y + p1.y * p2.x) / 2;
+}
+
+template <class T>
+struct Circle {
+    Point<T> p;
+    T r;
+
+    constexpr Circle() : p(), r() {}
+    constexpr Circle(Point<T> _p, T _r) : p(_p), r(_r) {}
+};
+
+template <class T>
+int checker(Circle<T> c1, Circle<T> c2) {
+    if (c1.r < c2.r) swap(c1, c2);
+    T d = abs(c1.p - c2.p);
+    if (c1.r + c2.r < d) return 4;
+    if (eq(c1.r + c2.r, d)) return 3;
+    if (c1.r - c2.r < d) return 2;
+    if (eq(c1.r - c2.r, d)) return 1;
+    return 0;
+}
+
+template <class T>
+bool intersect(Circle<T> c1, Circle<T> c2) {
+    int tmp = checker(c1, c2);
+    return tmp >= 1 && tmp <= 3;
+}
+
+template <class T>
+Points<T> cross_point(const Circle<T> c1, const Circle<T> c2) {
+    T d = abs(c1.p - c2.p);
+    int tmp = checker(c1, c2);
+    Points<T> res;
+    if (tmp == 3) res.push_back((c1.p * c2.r + c2.p * c1.r) / d);
+    if (tmp == 2) {
+        T a = acos((c1.r * c1.r + d * d - c2.r * c2.r) / (2.0 * c1.r * d));
+        T t = atan2(c2.p.imag() - c1.p.imag(), c2.p.real() - c1.p.real());
+        res.emplace_back(c1.p + Point<T>(cos(t + a) * c1.r, sin(t + a) * c1.r));
+        res.emplace_back(c1.p + Point<T>(cos(t - a) * c1.r, sin(t - a) * c1.r));
+    }
+    return res;
+}
+
+// 最小包含円
+template <class T>
+Circle<T> min_ball(Points<T> &ps, Point<T> p = Point<T>(0.0, 0.0)) {
+    T r = 0.0, move = 0.5;
+    Point<T> k(0.0, 0.0);
+    while (move > EPS * EPS) {
+        r = 0.0;
+        for (auto i : ps)
+            if (chmax(r, abs(p - i))) k = i;
+        p += (k - p) * move;
+        move *= 0.999;
+    }
+
+    return Circle<T>(p, r);
+}
---
+++ lib/geometry/geometry3d.hpp
@@ -0,0 +1,39 @@
+#include "template/template.hpp"
+
+template <class T>
+struct Pos {
+    T x, y, z;
+    constexpr Pos() : x(), y(), z() {}
+    constexpr Pos(T _x, T _y, T _z) : x(_x), y(_y), z(_z) {}
+};
+
+template <class T>
+struct Circle3 {
+    Pos<T> p;
+    T r;
+    constexpr Circle3(Pos<T> _p, T _r) : p(_p), r(_r) {}
+};
+
+template <class T>
+Circle3<T> min_ball(vector<Pos<T>> &ps, Pos<T> p = {0.0, 0.0, 0.0}) {
+    auto dist = [](const Pos<T> &a, const Pos<T> &b) -> T {
+        T dx = a.x - b.x;
+        T dy = a.y - b.y;
+        T dz = a.z - b.z;
+        return sqrt(dx * dx + dy * dy + dz * dz);
+    };
+
+    T r = 0.0, move = 0.5;
+    Pos<T> k = {0.0, 0.0, 0.0};
+    while (move > EPS * EPS) {
+        r = 0.0;
+        for (Pos<T> i : ps)
+            if (chmax(r, dist(p, i))) k = i;
+        p.x += (k.x - p.x) * move;
+        p.y += (k.y - p.y) * move;
+        p.x += (k.z - p.z) * move;
+        move *= 0.999;
+    }
+
+    return Circle3<T>(p, r);
+}
