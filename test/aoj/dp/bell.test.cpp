ce53fe186445f2f13933d0556b4f12a0f3437714
kuhaku-space <te4958yu6924@gmail.com>
2021-09-26 10:14:33 +0900

    Add verify

---
+++ test/aoj/dp/bell.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/5/DPL_5_I"
+#include "math/enumeration.hpp"
+#include "template/atcoder.hpp"
+
+int main(void) {
+    sonic();
+    int n, k;
+    cin >> n >> k;
+    co(enume.stirling(n, k));
+
+    return 0;
+}
