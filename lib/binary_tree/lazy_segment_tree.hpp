ebd0585a56c11f2718d79cf16c16fc57fae24c00
kuhaku-space <te4958yu6924@gmail.com>
2021-09-27 11:59:54 +0900

    Update lazy_segment_tree

--- lib/binary_tree/lazy_segment_tree.hpp
+++ lib/binary_tree/lazy_segment_tree.hpp
@@ -11,14 +11,17 @@ struct lazy_segment_tree {
     vector<T> data;
     vector<U> lazy;
 
-    lazy_segment_tree(int _n, T &&_e, U &&_id, F &&_op, G &&_mapp, H &&_comp)
+    lazy_segment_tree(int _n, T _e, U _id, F &&_op, G &&_mapp, H &&_comp)
+        : e(_e), id(_id), op(_op), mapp(_mapp), comp(_comp) {
+        init(_n);
+    }
+    lazy_segment_tree(int _n, T _e, U _id, const F &_op, const G &_mapp, const H &_comp)
         : e(_e), id(_id), op(_op), mapp(_mapp), comp(_comp) {
         init(_n);
     }
 
     void init(int n) {
-        for (N = 1, logN = 0; N < n; N <<= 1, ++logN)
-            ;
+        for (N = 1, logN = 0; N < n; N <<= 1, ++logN) {}
         data.assign(N << 1, e);
         lazy.assign(N << 1, id);
     }
@@ -26,22 +29,20 @@ struct lazy_segment_tree {
     template <class V>
     void build(const vector<V> &v) {
         for (int i = 0; i < v.size(); ++i) data[N + i] = v[i];
-        for (int i = N - 1; i >= 1; --i)
-            data[i] = op(data[i * 2], data[i * 2 + 1]);
+        for (int i = N - 1; i >= 1; --i) data[i] = op(data[i * 2], data[i * 2 + 1]);
     }
 
     void apply(int k, const U &x) {
-        assert(k > 0 && k < N << 1);
+        assert(0 < k && k < N << 1);
         lazy[k] = comp(x, lazy[k]);
 
         if ((k >>= 1) >= 1) {
-            data[k] = op(mapp(lazy[k * 2], data[k * 2]),
-                         mapp(lazy[k * 2 + 1], data[k * 2 + 1]));
+            data[k] = op(mapp(lazy[k * 2], data[k * 2]), mapp(lazy[k * 2 + 1], data[k * 2 + 1]));
         }
     }
 
     T eval(int k) {
-        assert(k > 0 && k < N << 1);
+        assert(0 < k && k < N << 1);
         if (lazy[k] == id) return data[k];
         if (k < N) {
             lazy[k * 2] = comp(lazy[k], lazy[k * 2]);
@@ -54,8 +55,8 @@ struct lazy_segment_tree {
 
     void update(int a, const U &x) { return add(a, a + 1, x); }
     void update(int a, int b, const U &x) {
-        assert(a >= 0 && a <= N);
-        assert(b >= 0 && b <= N);
+        assert(0 <= a && a <= N);
+        assert(0 <= b && b <= N);
         int l = a + N, r = b + N - 1;
         for (int i = logN; i >= 0; --i) eval(l >> i), eval(r >> i);
         for (a += N, b += N; a < b; a >>= 1, b >>= 1) {
@@ -66,8 +67,8 @@ struct lazy_segment_tree {
     }
 
     T query(int a, int b) {
-        assert(a >= 0 && a <= N);
-        assert(b >= 0 && b <= N);
+        assert(0 <= a && a <= N);
+        assert(0 <= b && b <= N);
         a += N, b += N;
         for (int i = logN; i >= 0; --i) eval(a >> i), eval((b - 1) >> i);
         T l = e, r = e;
