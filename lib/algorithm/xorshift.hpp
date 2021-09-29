517670d63d09ce4932c08f324143dc8cd5cf41be
kuhaku-space <te4958yu6924@gmail.com>
2021-09-29 10:00:44 +0900

    Create xorshift

---
+++ lib/algorithm/xorshift.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "template/template.hpp"
+
+struct Xorshift {
+    unsigned int x, y, z, w;
+
+    Xorshift() { x = 123456789, y = 362436069, z = 521288629, w = 88675123; }
+
+    unsigned int operator()() {
+        unsigned int t = (x ^ (x << 11));
+        x = y, y = z, z = w;
+        return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+    }
+
+    double rand_d() { return (double)(*this)() / (1LL << 32); }
+};
