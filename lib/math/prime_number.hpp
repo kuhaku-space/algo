0797cd4d13df2fd671bf0b4d40f23cdd5fe38cb4
kuhaku-space <te4958yu6924@gmail.com>
2021-10-02 14:40:13 +0900

    Use static constexpr in prime_number

--- lib/math/prime_number.hpp
+++ lib/math/prime_number.hpp
@@ -1,8 +1,8 @@
 #include "template/template.hpp"
 
 struct prime_number {
-    const int sz = 1 << 22;
-    bitset<1 << 22> is_not_prime;
+    static constexpr int sz = 1 << 22;
+    bitset<sz> is_not_prime;
     vector<int> data;
 
     prime_number() { init(); }
@@ -15,16 +15,14 @@ struct prime_number {
                 if ((int64_t)i * i >= sz) continue;
                 if (i == 2) {
                     for (int j = i * i; j < sz; j += i) is_not_prime[j] = true;
-
                 } else {
-                    for (int j = i * i; j < sz; j += i << 1)
-                        is_not_prime[j] = true;
+                    for (int j = i * i; j < sz; j += i << 1) is_not_prime[j] = true;
                 }
             }
         }
     }
 
-    bool is_prime(int64_t n) {
+    constexpr bool is_prime(int64_t n) const {
         assert(n >= 0);
         if (n < sz) return !is_not_prime[n];
         for (auto i : data) {
@@ -34,7 +32,7 @@ struct prime_number {
         return true;
     }
 
-    vector<int> prime_numbers(int x) {
+    vector<int> prime_numbers(int x) const {
         vector<int> res;
         for (auto i : data) {
             if (i > x) break;
@@ -45,13 +43,13 @@ struct prime_number {
 
     /**
      * @brief 素因数分解
-     * 
-     * @tparam T 
-     * @param x 
-     * @return vector<pair<T, int>> 
+     *
+     * @tparam T
+     * @param x
+     * @return vector<pair<T, int>>
      */
     template <class T>
-    vector<pair<T, int>> prime_factorization(T x) {
+    vector<pair<T, int>> prime_factorization(T x) const {
         if (x == 1) return vector<pair<T, int>>(1, {1, 1});
         vector<pair<T, int>> res;
         for (auto i : data) {
@@ -66,13 +64,13 @@ struct prime_number {
 
     /**
      * @brief 約数列挙
-     * 
-     * @tparam T 
-     * @param x 
-     * @return vector<T> 
+     *
+     * @tparam T
+     * @param x
+     * @return vector<T>
      */
     template <class T>
-    vector<T> divisors(T x) {
+    vector<T> divisors(T x) const {
         if (x == 1) return vector<T>(1, 1);
         auto v = this->prime_factorization(x);
         vector<T> res;
@@ -80,12 +78,9 @@ struct prime_number {
         for (auto p : v) {
             int n = res.size();
             res.resize(n * (p.second + 1));
-            for (int i = 0; i < n * p.second; ++i) {
-                res[n + i] = res[i] * p.first;
-            }
+            for (int i = 0; i < n * p.second; ++i) { res[n + i] = res[i] * p.first; }
             for (int i = 1; i <= p.second; ++i) {
-                inplace_merge(res.begin(), res.begin() + n * i,
-                              res.begin() + n * (i + 1));
+                inplace_merge(res.begin(), res.begin() + n * i, res.begin() + n * (i + 1));
             }
         }
         return res;
@@ -93,7 +88,7 @@ struct prime_number {
 
     // 因数分解列挙
     template <class T>
-    vector<vector<T>> factorization(T x) {
+    vector<vector<T>> factorization(T x) const {
         vector<vector<T>> res;
         auto f = [&](auto self, vector<T> v, T a) -> void {
             if (a == 1) res.emplace_back(v);
@@ -108,4 +103,3 @@ struct prime_number {
         return res;
     }
 };
-prime_number pn;
