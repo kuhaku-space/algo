d1667ad2e34e2ed4c74f5027bdb5901213a83ea3
kuhaku-space <te4958yu6924@gmail.com>
2021-10-26 00:20:40 +0900

    Add this in fraction struct

--- lib/math/fraction.hpp
+++ lib/math/fraction.hpp
@@ -6,13 +6,7 @@ struct Fraction {
 
     Fraction() : x(0), y(1) {}
 
-    Fraction(int64_t _x, int64_t _y = 1) : x(_x), y(_y) { common(); }
-
-    void common() {
-        int64_t g = gcd(x, y);
-        if (g) x /= g, y /= g;
-        if (y < 0) x *= -1, y *= -1;
-    }
+    Fraction(int64_t _x, int64_t _y = 1) : x(_x), y(_y) { this->common(); }
 
     int64_t gcd(int64_t a, int64_t b) const {
         while (b) {
@@ -25,72 +19,58 @@ struct Fraction {
     Fraction &operator+=(const Fraction &rhs) {
         x = x * rhs.y + y * rhs.x;
         y *= rhs.y;
-        common();
+        this->common();
         return *this;
     }
     Fraction &operator-=(const Fraction &rhs) {
         x = x * rhs.y - y * rhs.x;
         y *= rhs.y;
-        common();
+        this->common();
         return *this;
     }
     Fraction &operator*=(const Fraction &rhs) {
         x *= rhs.x, y *= rhs.y;
-        common();
+        this->common();
         return *this;
     }
     Fraction &operator/=(const Fraction &rhs) {
         x *= rhs.y, y *= rhs.x;
-        common();
+        this->common();
         return *this;
     }
 
     Fraction &operator++() {
-        x += y;
+        this->x += this->y;
         return *this;
     }
     Fraction operator++(int) {
         Fraction tmp(*this);
-        operator++();
+        this->operator++();
         return tmp;
     }
     Fraction &operator--() {
-        x -= y;
+        this->x -= this->y;
         return *this;
     }
     Fraction operator--(int) {
         Fraction tmp(*this);
-        operator--();
+        this->operator--();
         return tmp;
     }
 
     Fraction operator-() const { return Fraction(-x, y); }
 
-    Fraction operator+(const Fraction &rhs) const {
-        return Fraction(*this) += rhs;
-    }
-    Fraction operator-(const Fraction &rhs) const {
-        return Fraction(*this) -= rhs;
-    }
-    Fraction operator*(const Fraction &rhs) const {
-        return Fraction(*this) *= rhs;
-    }
-    Fraction operator/(const Fraction &rhs) const {
-        return Fraction(*this) /= rhs;
-    }
+    Fraction operator+(const Fraction &rhs) const { return Fraction(*this) += rhs; }
+    Fraction operator-(const Fraction &rhs) const { return Fraction(*this) -= rhs; }
+    Fraction operator*(const Fraction &rhs) const { return Fraction(*this) *= rhs; }
+    Fraction operator/(const Fraction &rhs) const { return Fraction(*this) /= rhs; }
 
-    bool operator==(const Fraction &rhs) const {
-        return x == rhs.x && y == rhs.y;
-    }
-    bool operator!=(const Fraction &rhs) const {
-        return x != rhs.x || y != rhs.y;
-    }
+    bool operator==(const Fraction &rhs) const { return x == rhs.x && y == rhs.y; }
+    bool operator!=(const Fraction &rhs) const { return x != rhs.x || y != rhs.y; }
     bool operator<(const Fraction &rhs) const { return x * rhs.y < rhs.x * y; }
     bool operator>(const Fraction &rhs) const { return x * rhs.y > rhs.x * y; }
 
-    friend ostream &operator<<(ostream &os, const Fraction &rhs) {
-        return os << rhs.to_double();
-    }
+    friend ostream &operator<<(ostream &os, const Fraction &rhs) { return os << rhs.to_double(); }
 
     friend istream &operator>>(istream &is, Fraction &a) {
         string s;
@@ -111,4 +91,11 @@ struct Fraction {
     }
 
     double to_double() const { return (double)x / y; }
+
+  private:
+    void common() {
+        int64_t g = gcd(x, y);
+        if (g) x /= g, y /= g;
+        if (y < 0) x *= -1, y *= -1;
+    }
 };
