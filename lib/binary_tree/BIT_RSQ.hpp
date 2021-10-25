45b7e050cd5804cbc1a2b0ae165eef1607d97a9c
kuhaku-space <te4958yu6924@gmail.com>
2021-10-26 00:42:47 +0900

    Update doxygen in BIT_RSQ

--- lib/binary_tree/BIT_RSQ.hpp
+++ lib/binary_tree/BIT_RSQ.hpp
@@ -1,21 +1,17 @@
-#include "template/template.hpp"
 #include "binary_tree/BIT.hpp"
+#include "template/template.hpp"
 
 template <class T>
 struct BIT_RSQ {
-    BIT<T> p, q;
-
-    BIT_RSQ(int n) : p(n), q(n) {}
+    BIT_RSQ(int n) : p(n + 1), q(n + 1) {}
 
     void init(int n) {
-        p = BIT<T>(n);
-        q = BIT<T>(n);
+        p = BIT<T>(n + 1);
+        q = BIT<T>(n + 1);
     }
 
-    auto operator[](int i) const { return this->at(i); }
-
-    // v[k]
-    auto at(int k) const { return this->sum(k + 1) - this->sum(k); }
+    auto operator[](int i) const { this->sum(i + 1) - this->sum(i); }
+    auto at(int k) const { return this->operator[](k); }
 
     template <class U>
     void build(const vector<U> &v) {
@@ -26,21 +22,51 @@ struct BIT_RSQ {
         }
     }
 
-    void update(int k, T x) { this->add(k, k + 1, x - this->at(k)); }
+    /**
+     * @brief v[k] = val
+     *
+     * @param k index of array
+     * @param val new value
+     */
+    void update(int k, T val) { this->add(k, k + 1, val - this->at(k)); }
 
-    // v[k] += w
-    void add(int k, T w) { this->add(k, k + 1, w); }
-    // v[a ... b - 1] += w
-    void add(int a, int b, T w) {
-        p.add(a, -w * a);
-        p.add(b, w * b);
-        q.add(a, w);
-        q.add(b, -w);
+    /**
+     * @brief v[k] += val
+     *
+     * @param k index of array
+     * @param val
+     */
+    void add(int k, T val) { this->add(k, k + 1, val); }
+    /**
+     * @brief v[a ... b-1] += val
+     *
+     * @param a first index of array
+     * @param b last index of array
+     * @param val
+     */
+    void add(int a, int b, T val) {
+        p.add(a, -val * a);
+        p.add(b, val * b);
+        q.add(a, val);
+        q.add(b, -val);
     }
 
-    // v[0] + ... + v[k - 1]
+    /**
+     * @brief v[0] + ... v[k - 1]
+     *
+     * @param k index of array
+     * @return auto
+     */
     auto sum(int k) const { return p.sum(k) + q.sum(k) * k; }
-
-    // v[a] + ... + v[b - 1]
+    /**
+     * @brief v[a] + ... + v[b - 1]
+     *
+     * @param a first index of array
+     * @param b last index of array
+     * @return auto
+     */
     auto sum(int a, int b) const { return this->sum(b) - this->sum(a); }
+
+  private:
+    BIT<T> p, q;
 };
