67d0360bf622a96f68379d197cac4f9789473199
kuhaku-space <te4958yu6924@gmail.com>
2021-11-14 11:20:35 +0900

    Update uinon_find.hpp

--- lib/tree/union_find.hpp
+++ lib/tree/union_find.hpp
@@ -10,20 +10,23 @@ struct union_find {
     union_find() : data() {}
     union_find(int _n) : data(_n, -1) {}
 
-    int root(int x) { return data[x] < 0 ? x : data[x] = this->root(data[x]); }
+    int root(int x) { return this->data[x] < 0 ? x : this->data[x] = this->root(data[x]); }
+    int get_root(int x) { return this->root(x); }
 
-    bool is_root(int x) { return data[x] < 0; }
+    bool is_root(int x) { return this->data[x] < 0; }
 
     void unite(int x, int y) {
         x = this->root(x), y = this->root(y);
         if (x != y) {
-            if (data[x] > data[y]) swap(x, y);
-            data[x] += data[y];
-            data[y] = x;
+            if (this->data[x] > this->data[y]) swap(x, y);
+            this->data[x] += this->data[y];
+            this->data[y] = x;
         }
     }
 
-    int size(int x) { return -data[this->root(x)]; }
+    int size(int x) { return -(this->data[this->root(x)]); }
+    int get_size(int x) { return this->size(x); }
 
     bool same(int x, int y) { return this->root(x) == this->root(y); }
+    bool is_same(int x, int y) { return this->same(x, y); }
 };
