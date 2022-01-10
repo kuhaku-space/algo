a3b1ae1ebb51d38aa7fa2c238c95c8f625b58273
kuhaku-space <te4958yu6924@gmail.com>
2022-01-10 21:02:45 +0900

    Update segment tree

--- lib/segment_tree/segment_tree.hpp
+++ lib/segment_tree/segment_tree.hpp
@@ -1,82 +1,113 @@
+#include "math/pow.hpp"
 #include "template/template.hpp"
 
 /**
  * @brief セグメント木
+ * @details [参考](https://noshi91.hatenablog.com/entry/2020/04/22/212649)
  *
- * @tparam T 要素の型
- * @tparam F 関数の型
+ * @tparam M
+ * @tparam F
  */
-template <class T, class F>
+template <class M, class F>
 struct segment_tree {
-    int N;
-    const T e;
-    const F op;
-    vector<T> data;
+    using T = typename M::value_type;
 
     segment_tree() {}
-    /**
-     * @brief Construct a new segment tree object
-     *
-     * @param _n length of array
-     * @param _e identity element
-     * @param _op binary operation
-     */
-    segment_tree(int _n, T _e, F &&_op) : e(_e), op(_op) { this->init(_n, _e); }
-    /**
-     * @brief Construct a new segment tree object
-     *
-     * @param _n length of array
-     * @param _e identity element
-     * @param _op binary operation
-     */
-    segment_tree(int _n, T _e, const F &_op) : e(_e), op(_op) { this->init(_n, _e); }
+    segment_tree(int n) { this->init(n, M::id); }
+    segment_tree(int n, T e) { this->init(n, e); }
 
-    const T &operator[](int i) const { return this->data[i + this->N]; }
+    const T &operator[](int i) const { return this->data[i + this->_size]; }
     T at(int k) const { return this->operator[](k); }
     T get(int k) const { return this->operator[](k); }
 
     void init(int n, const T val) {
-        for (this->N = 1; this->N < n; this->N <<= 1) {}
-        this->data.assign(this->N << 1, val);
+        this->_size = ceil_pow2(n);
+        this->data.assign(this->_size << 1, val);
     }
 
     template <class U>
     void build(const vector<U> &v) {
-        for (int i = 0, n = v.size(); i < n; ++i) this->data[this->N + i] = T(v[i]);
-        for (int i = this->N - 1; i >= 1; --i)
-            this->data[i] = this->op(this->data[i * 2], this->data[i * 2 + 1]);
+        for (int i = 0, n = v.size(); i < n; ++i) this->data[this->_size + i] = T(v[i]);
+        for (int i = this->_size - 1; i >= 1; --i)
+            this->data[i] = M::op(this->data[i * 2], this->data[i * 2 + 1]);
     }
 
-    void update(int k, T val) {
-        assert(0 <= k && k < this->N);
-        this->data[k += this->N] = val;
-        while ((k >>= 1) >= 1) this->data[k] = this->op(this->data[k * 2], this->data[k * 2 + 1]);
-    }
-    void add(int k, T val) {
-        assert(0 <= k && k < this->N);
-        this->data[k += this->N] += val;
-        while ((k >>= 1) >= 1) this->data[k] = this->op(this->data[k * 2], this->data[k * 2 + 1]);
+    template <class U>
+    void apply(int k, U val) {
+        assert(0 <= k && k < this->_size);
+        k += this->_size;
+        this->data[k] = F::f(val, this->data[k]);
+        while ((k >>= 1) >= 1) this->data[k] = M::op(this->data[k * 2], this->data[k * 2 + 1]);
     }
 
-    bool chmax(int k, T val) {
-        if (this->at(k) >= val) return false;
-        this->update(k, val);
-        return true;
-    }
-    bool chmin(int k, T val) {
-        if (this->at(k) <= val) return false;
-        this->update(k, val);
-        return true;
+    void set(int k, T val) {
+        assert(0 <= k && k < this->_size);
+        k += this->_size;
+        this->data[k] = val;
+        while ((k >>= 1) >= 1) this->data[k] = M::op(this->data[k * 2], this->data[k * 2 + 1]);
     }
 
     T all_prod() const { return this->data[1]; }
     T prod(int a, int b) const {
-        assert(0 <= a && b <= this->N);
-        T l = e, r = e;
-        for (a += this->N, b += this->N; a < b; a >>= 1, b >>= 1) {
-            if (a & 1) l = this->op(l, this->data[a++]);
-            if (b & 1) r = this->op(this->data[--b], r);
+        assert(0 <= a && b <= this->_size);
+        T l = M::id, r = M::id;
+        for (a += this->_size, b += this->_size; a < b; a >>= 1, b >>= 1) {
+            if (a & 1) l = M::op(l, this->data[a++]);
+            if (b & 1) r = M::op(this->data[--b], r);
         }
-        return this->op(l, r);
+        return M::op(l, r);
+    }
+
+  private:
+    int _size;
+    vector<T> data;
+};
+
+template <class T>
+struct Sum {
+    using value_type = T;
+    static constexpr T id = 0;
+    static T op(T l, T r) { return l + r; }
+};
+
+template <class T>
+struct Min {
+    using value_type = T;
+    static constexpr T id = numeric_limits<T>::max();
+    static T op(T l, T r) { return min(l, r); }
+};
+
+template <class T>
+struct Max {
+    using value_type = T;
+    static constexpr T id = numeric_limits<T>::min();
+    static T op(T l, T r) { return max(l, r); }
+};
+
+struct Add {
+    template <class T, class U>
+    static T f(U val, T x) {
+        return val + x;
+    }
+};
+
+struct Update {
+    template <class T, class U>
+    static T f(U val, T x) {
+        return val;
+    }
+};
+
+struct Chmin {
+    template <class T, class U>
+    static T f(U val, T x) {
+        return min((T)val, x);
+    }
+};
+
+struct Chmax {
+    template <class T, class U>
+    static T f(U val, T x) {
+        return max((T)val, x);
     }
 };
