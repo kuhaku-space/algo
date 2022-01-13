367a0afc4961a4ffd76fdd2863c5565ed613be92
kuhaku-space <te4958yu6924@gmail.com>
2022-01-14 01:38:48 +0900

    Add test

---
+++ test/aoj/grl/dijkstra.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/1/GRL_1_A"
+#include "graph/dijkstra.hpp"
+#include "graph/graph.hpp"
+#include "template/atcoder.hpp"
+
+int main(void) {
+    sonic();
+    int n, m, r;
+    cin >> n >> m >> r;
+    Graph<int> g(n);
+    g.input_edge(m, true);
+
+    auto dist = dijkstra(g, r, Inf);
+    rep(i, n) {
+        if (dist[i] != Inf)
+            co(dist[i]);
+        else
+            co("INF");
+    }
+
+    return 0;
+}
