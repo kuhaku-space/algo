6bf7f94e975d7c7c637080e1b84804788bcb0f4b
kuhaku-space <te4958yu6924@gmail.com>
2022-01-14 01:36:18 +0900

    Update dijkstra

--- lib/graph/dijkstra_radix_heap.hpp
+++ lib/graph/dijkstra_radix_heap.hpp
@@ -1,3 +1,4 @@
+#include "data_structure/radix_heap.hpp"
 #include "graph/graph.hpp"
 #include "template/template.hpp"
 
@@ -6,21 +7,19 @@ vector<T> dijkstra(const Graph<T> &g, int s = 0, T inf = numeric_limits<T>::max(
     struct _edge {
         int to;
         T dist;
-        constexpr _edge() : to(), dist() {}
-        constexpr _edge(int _to, T _dist) : to(_to), dist(_dist) {}
-        bool operator<(const _edge &rhs) const { return this->dist < rhs.dist; }
+        bool operator<(const _edge &rhs) const { return dist < rhs.dist; }
         bool operator>(const _edge &rhs) const { return rhs < *this; }
     };
     vector<T> dist(g.size(), inf);
-    priority_queue<_edge, vector<_edge>, greater<_edge>> p_que;
+    radix_heap_dijkstra<_edge> p_que;
     dist[s] = T();
-    p_que.emplace(s, T());
+    p_que.push(_edge{s, T()});
     while (!p_que.empty()) {
         _edge e = p_que.top();
         p_que.pop();
         if (dist[e.to] < e.dist) continue;
         for (auto &i : g[e.to]) {
-            if (chmin(dist[i.to], e.dist + i.dist)) p_que.emplace(i.to, e.dist + i.dist);
+            if (chmin(dist[i.to], e.dist + i.dist)) p_que.push(_edge{i.to, e.dist + i.dist});
         }
     }
     return dist;
