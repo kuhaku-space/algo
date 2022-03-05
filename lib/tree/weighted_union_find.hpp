e211e9fdf73fa6909ebe97d45b3ced0a2546bb0b
kuhaku-space <te4958yu6924@gmail.com>
2022-03-06 07:48:22 +0900

    Add docs
    - Add docs methods unite and diff

--- lib/tree/weighted_union_find.hpp
+++ lib/tree/weighted_union_find.hpp
@@ -22,6 +22,15 @@ struct weighted_union_find {
         return this->_weights[t];
     }
 
+    /**
+     * @brief v[y] = v[x] + w
+     *
+     * @param x
+     * @param y
+     * @param w
+     * @return true unite correctly
+     * @return false already united
+     */
     bool unite(int x, int y, T w) {
         w += this->get_weight(x) - this->get_weight(y);
         x = find(x), y = find(y);
@@ -36,7 +45,18 @@ struct weighted_union_find {
         return true;
     }
 
+    bool same(int x, int y) { return this->find(x) == this->find(y); }
+    bool is_same(int x, int y) { return this->same(x, y); }
+
+    /**
+     * @brief get diff between x and y
+     *
+     * @param x
+     * @param y
+     * @return T v[y] - v[x]
+     */
     T diff(int x, int y) { return this->get_weight(y) - this->get_weight(x); }
+    T get_diff(int x, int y) { return this->diff(x, y); }
 
   private:
     std::vector<int> _data;
