ffcdecb99cddf812c7b72810cd8d90904fb62f23
kuhaku-space <te4958yu6924@gmail.com>
2022-03-05 09:13:48 +0900

    Add docs
    - Add docs to a function kruskal

--- lib/graph/kruskal.hpp
+++ lib/graph/kruskal.hpp
@@ -1,20 +1,24 @@
-#include "template/template.hpp"
 #include "graph/graph.hpp"
+#include "template/template.hpp"
 #include "tree/union_find.hpp"
 
-// verify : https://onlinejudge.u-aizu.ac.jp/problems/GRL_2_A 21/02/24
-
-// 最小全域木
+/**
+ * @brief 最小全域木
+ *
+ * @tparam T
+ * @param g
+ * @return vector<typename Graph<T>::edge>
+ */
 template <class T>
 vector<typename Graph<T>::edge> kruskal(const Graph<T> &g) {
     using _edge = typename Graph<T>::edge;
     union_find uf(g.size());
-    vector<_edge> res;
-    vector<_edge> edge_list;
+    std::vector<_edge> res;
+    std::vector<_edge> edge_list;
     for (auto &v : g) {
         for (auto &e : v) edge_list.emplace_back(e);
     }
-    sort(edge_list.begin(), edge_list.end());
+    std::sort(edge_list.begin(), edge_list.end());
     for (auto &e : edge_list) {
         if (!uf.is_same(e.from, e.to)) {
             uf.unite(e.from, e.to);
