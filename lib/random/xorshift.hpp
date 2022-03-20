3aa075dcd27ab0e41bf8bdeb3e489d02f2dd0915
kuhaku-space <te4958yu6924@gmail.com>
2022-03-20 20:48:17 +0900

    Improve xorshift
    - Move xorshift to random
    - Add methods serialize, deserialize, rand_range and random
    - Add docs of methods

---
+++ lib/random/xorshift.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "random/split_mix_64.hpp"
+#include "template/template.hpp"
+
+/**
+ * @brief 擬似乱数生成器
+ * @details 周期：$2^128-1$
+ */
+struct Xorshift {
+    using state_type = std::array<std::uint32_t, 4>;
+    using result_type = std::uint32_t;
+    constexpr Xorshift() : state{123456789, 362436069, 521288629, 88675123} {}
+
+    constexpr result_type min() { return std::numeric_limits<result_type>::min(); }
+    constexpr result_type max() { return std::numeric_limits<result_type>::max(); }
+    result_type operator()() {
+        std::uint32_t t = (state[0] ^ (state[0] << 11));
+        state[0] = state[1], state[1] = state[2], state[2] = state[3];
+        return state[3] = (state[3] ^ (state[3] >> 19)) ^ (t ^ (t >> 8));
+    }
+    bool operator==(const Xorshift& rhs) noexcept { return (this->state == rhs.state); }
+    bool operator!=(const Xorshift& rhs) noexcept { return (this->state != rhs.state); }
+
+    constexpr state_type serialize() const noexcept { return this->state; }
+    constexpr void deserialize(const state_type state) noexcept { this->state = state; }
+
+    /**
+     * @brief a以上b以下の整数を生成
+     *
+     * @param a
+     * @param b
+     * @return int [a, b]
+     */
+    int rand_range(int a, int b) { return a + this->operator()() % (b - a + 1); }
+
+    /**
+     * @brief 0.0以上1.0未満の浮動小数点数を生成
+     *
+     * @return double [0.0, 1.0)
+     */
+    double random() { return (double)this->operator()() / this->max(); }
+
+  private:
+    state_type state;
+};
