aa5892fe497c4e9d05d39d8f07b0babbb3019341
kuhaku-space <te4958yu6924@gmail.com>
2022-04-10 06:07:31 +0900

    Add function for tree
    - Add a function tree_bfs
    - Add a function tree_dfs

---
+++ lib/tree/tree_bfs.hpp
@@ -0,0 +1,20 @@
+#include "graph/graph.hpp"
+#include "template/template.hpp"
+
+template <class T>
+std::vector<int> tree_bfs(const Graph<T> &g, int r = 0) {
+    int pos = 0;
+    std::vector<int> res;
+    std::vector<bool> visited(g.size());
+    res.emplace_back(r);
+    visited[r] = true;
+    while (pos < res.size()) {
+        auto index = res[pos++];
+        for (auto &e : g[index]) {
+            if (visited[e.to()]) continue;
+            res.emplace_back(e.to());
+            visited[e.to()] = true;
+        }
+    }
+    return res;
+}
