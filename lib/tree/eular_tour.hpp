f8b336f5cc2cd5edcd2dd2116975eed6a37d5be5
kuhaku-space <te4958yu6924@gmail.com>
2022-04-10 06:04:36 +0900

    Improve eular tour
    - Add private access modifier to properties build and dfs
    - Add std namespace
    - Update docs

--- lib/tree/eular_tour.hpp
+++ lib/tree/eular_tour.hpp
@@ -1,35 +1,21 @@
+#include "graph/graph.hpp"
 #include "template/template.hpp"
 
 /**
  * @brief オイラーツアー
  * @details [参考](https://github.com/beet-aizu/library/blob/master/tree/eulertourforvertex.cpp)
  *
+ * @tparam T 辺の重みの型
  */
+template <class T>
 struct eular_tour {
-    eular_tour(int n) : G(n), ls(n), rs(n), pos() {}
+    eular_tour(const Graph<T> &_g, int r = 0) : g(_g), ls(_g.size()), rs(_g.size()), pos() {
+        this->build(r);
+    }
 
-    pair<int, int> operator[](int i) const { return make_pair(this->ls[i], this->rs[i]); }
+    std::pair<int, int> operator[](int i) const { return {this->ls[i], this->rs[i]}; }
 
     int size() const { return this->pos; }
-
-    void add_edge(int a, int b) {
-        this->G[a].emplace_back(b);
-        this->G[b].emplace_back(a);
-    }
-    void add_edges(int a, int b) { this->add_edge(a, b); }
-
-    void dfs(int v, int p) {
-        this->ls[v] = this->pos++;
-        for (auto &u : this->G[v]) {
-            if (u != p) this->dfs(u, v);
-            this->rs[v] = this->pos;
-        }
-    }
-
-    void build(int r = 0) {
-        this->pos = 0;
-        this->dfs(r, -1);
-    }
 
     int get_l(int i) const { return this->ls[i]; }
     int get_r(int i) const { return this->rs[i]; }
@@ -40,7 +26,20 @@ struct eular_tour {
     }
 
   private:
-    vector<vector<int>> G;
-    vector<int> ls, rs;
+    const Graph<T> &g;
+    std::vector<int> ls, rs;
     int pos;
+
+    void build(int r = 0) {
+        this->pos = 0;
+        this->dfs(r, -1);
+    }
+
+    void dfs(int v, int p) {
+        this->ls[v] = this->pos++;
+        for (auto &u : this->g[v]) {
+            if (u != p) this->dfs(u, v);
+            this->rs[v] = this->pos;
+        }
+    }
 };
