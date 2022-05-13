d3ccfe6ff2094895789e25d922d561fbd3f655e3
kuhaku-space <te4958yu6924@gmail.com>
2022-05-14 05:39:48 +0900

    Improve persistent union find
    - Fix bug

--- lib/_new/persistent_union_find.hpp
+++ lib/_new/persistent_union_find.hpp
@@ -4,7 +4,7 @@
 struct persistent_union_find {
     constexpr persistent_union_find() : data() {}
     persistent_union_find(const persistent_array<int> &_data) : data(_data) {}
-    persistent_union_find(int _n) : data(_n, -1) {}
+    persistent_union_find(int n) : data(n, -1) {}
 
     int root(int x) const {
         int y = this->data[x];
@@ -18,10 +18,8 @@ struct persistent_union_find {
         x = this->root(x), y = this->root(y);
         if (x == y) return *this;
         int a = this->data[x], b = this->data[y];
-        if (a > b) { swap(x, y); }
-        auto u = this->data.set(x, a + b);
-        auto v = this->data.set(y, x);
-        return persistent_union_find(v);
+        if (a > b) swap(x, y);
+        return persistent_union_find(this->data.set(x, a + b).set(y, x));
     }
 
     int size(int x) const { return -(this->data[this->root(x)]); }
