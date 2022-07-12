b3224c264505b57711a2713c3f65cc39fadaa092
kuhaku-space <te4958yu6924@gmail.com>
2022-07-13 04:24:35 +0900

    Add test
    - Add test of hld

---
+++ test/aoj/grl/range_query_on_tree.test.cpp
@@ -0,0 +1,47 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/5/GRL_5_D"
+#include "binary_tree/BIT.hpp"
+#include "template/atcoder.hpp"
+#include "tree/hld.hpp"
+
+int main(void) {
+    int n;
+    cin >> n;
+    BIT<ll> bit(n);
+    HLD hld(n);
+    rep (i, n) {
+        int k;
+        cin >> k;
+        rep (j, k) {
+            int c;
+            cin >> c;
+            hld.add_edge(i, c);
+        }
+    }
+    hld.build();
+
+    int q;
+    cin >> q;
+    rep (i, q) {
+        int id;
+        cin >> id;
+        if (id == 0) {
+            int v, w;
+            cin >> v >> w;
+            auto f = [&](int a, int b) {
+                bit.add(a, w);
+            };
+            hld.for_each_edge(hld.get_parent(v), v, f);
+        } else {
+            int v;
+            cin >> v;
+            ll ans = 0;
+            auto f = [&](int a, int b) {
+                ans += bit.sum(a, b);
+            };
+            hld.for_each_edge(0, v, f);
+            co(ans);
+        }
+    }
+
+    return 0;
+}
