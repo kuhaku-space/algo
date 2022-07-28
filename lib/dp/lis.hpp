399ac97bf566f9c5c389c689c9da6348e903078a
kuhaku-space <te4958yu6924@gmail.com>
2022-07-29 05:35:35 +0900

    Add function make_LIS

--- lib/dp/lis.hpp
+++ lib/dp/lis.hpp
@@ -5,16 +5,44 @@
  *
  * @tparam T 配列の型
  * @param v 配列
- * @return int 最長増加部分列の長さ
+ * @retval int 最長増加部分列の長さ
  */
 template <class T>
 int LIS(const std::vector<T> &v) {
     int n = v.size();
     std::vector<T> dp;
-    for (int i = 0; i < n; ++i) {
-        auto it = std::lower_bound(dp.begin(), dp.end(), v[i]);
-        if (it == dp.end()) dp.emplace_back(v[i]);
-        else *it = v[i];
+    for (auto x : v) {
+        auto it = std::lower_bound(dp.begin(), dp.end(), x);
+        if (it == dp.end()) dp.emplace_back(x);
+        else *it = x;
     }
     return dp.size();
+}
+
+/**
+ * @brief 最長増加部分列
+ *
+ * @tparam T 配列の型
+ * @param v 配列
+ * @retval std::vector<int> 最長増加部分列のインデックス
+ */
+template <class T>
+std::vector<int> make_LIS(const std::vector<T> &v) {
+    int n = v.size();
+    std::vector<std::pair<T, int>> dp;
+    std::vector<int> pr(n, -1);
+    for (int i = 0; i < n; ++i) {
+        std::pair<T, int> p(v[i], -i);
+        auto it = std::lower_bound(dp.begin(), dp.end(), p);
+        if (it != dp.begin()) pr[i] = -std::prev(it)->second;
+        if (it == dp.end()) dp.emplace_back(p);
+        else *it = p;
+    }
+
+    std::vector<int> res;
+    for (int x = -dp.back().second; x != -1; x = pr[x]) {
+        res.emplace_back(x);
+    }
+    std::reverse(res.begin(), res.end());
+    return res;
 }
