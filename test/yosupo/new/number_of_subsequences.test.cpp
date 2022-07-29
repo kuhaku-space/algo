e0b3f59eccafb1801537bf9c28a1c9707b595131
kuhaku-space <te4958yu6924@gmail.com>
2022-07-29 15:11:11 +0900

    Add test of function compress

---
+++ test/yosupo/new/number_of_subsequences.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/number_of_subsequences"
+#include "algorithm/compress.hpp"
+#include "math/modint.hpp"
+#include "template/atcoder.hpp"
+
+using Mint = ModInt<>;
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    cin >> a;
+    auto v = compress(a);
+    Mint ans = 0;
+    vector<Mint> dp(*max_element(all(a)) + 1);
+    for (auto x : a) {
+        auto t = ans - dp[x] + 1;
+        ans += t;
+        dp[x] += t;
+    }
+    co(ans);
+
+    return 0;
+}
