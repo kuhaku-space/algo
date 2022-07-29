48ea6ebc16411184f69ae4cadf9fb46d11e4b3a8
kuhaku-space <te4958yu6924@gmail.com>
2022-07-29 13:32:19 +0900

    Improve compress
    - Rename class to coordinate_compression
    - Add function compress

--- lib/algorithm/compress.hpp
+++ lib/algorithm/compress.hpp
@@ -6,12 +6,12 @@
  * @tparam T 要素の型
  */
 template <class T>
-struct Compress {
-    Compress() {}
-    Compress(const vector<T>& _data) : data(_data) { this->build(); }
+struct coordinate_compression {
+    coordinate_compression() = default;
+    coordinate_compression(const vector<T> &_data) : data(_data) { this->build(); }
 
-    const T& operator[](int i) const { return this->data[i]; }
-    T& operator[](int i) { return this->data[i]; }
+    const T &operator[](int i) const { return this->data[i]; }
+    T &operator[](int i) { return this->data[i]; }
 
     void add(T x) { this->data.emplace_back(x); }
 
@@ -19,8 +19,8 @@ struct Compress {
         std::sort(this->data.begin(), this->data.end());
         this->data.erase(std::unique(this->data.begin(), this->data.end()), this->data.end());
     }
-    void build(const vector<T>& _data) {
-        this->data = _data;
+    void build(const vector<T> &v) {
+        this->data = v;
         std::sort(this->data.begin(), this->data.end());
         this->data.erase(std::unique(this->data.begin(), this->data.end()), this->data.end());
     }
@@ -40,3 +40,18 @@ struct Compress {
   private:
     std::vector<T> data;
 };
+
+/**
+ * @brief 座標圧縮
+ *
+ * @tparam T 要素の型
+ * @param v
+ * @return std::vector<T>
+ */
+template <class T>
+std::vector<T> compress(const std::vector<T> &v) {
+    coordinate_compression cps(v);
+    std::vector<T> res;
+    for (auto x : v) res.emplace_back(cps.get(x));
+    return res;
+}
