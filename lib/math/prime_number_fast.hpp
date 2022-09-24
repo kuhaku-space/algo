477895aacbec6cf048636366cedbf374936419aa
kuhaku-space <te4958yu6924@gmail.com>
2022-09-24 19:16:25 +0900

    Improve class eratosthenes

--- lib/math/prime_number_fast.hpp
+++ lib/math/prime_number_fast.hpp
@@ -1,44 +1,32 @@
 #include "template/template.hpp"
 
-struct prime_number {
-    static constexpr int sz = 1 << 22;
-    array<int, sz> factor;
-
-    prime_number() { init(); }
-
-    void init() {
-        iota(factor.begin(), factor.end(), 0);
-        for (int i = 2; i < sz; ++i) {
-            if (factor[i] < i) continue;
-            if ((int64_t)i * i > sz) continue;
-            for (int j = i * i; j < sz; j += i) {
-                if (factor[j] == j) factor[j] = i;
+template <int N = (1 << 22)>
+struct eratosthenes {
+    constexpr eratosthenes() : prime_number() {
+        this->prime_number.set();
+        this->prime_number[0] = this->prime_number[1] = false;
+        for (int i = 2; i <= N; ++i) {
+            if (!this->prime_number[i]) continue;
+            if ((std::int64_t)i * i > N) continue;
+            for (int j = i * i; j <= N; j += i) {
+                if (this->prime_number[j]) this->prime_number[j] = false;
             }
         }
     }
 
     bool is_prime(int x) const {
-        assert(0 <= x && x < sz);
-        return this->factor[x] == x;
+        return this->prime_number[x];
     }
 
-    vector<int> prime_numbers(int x) const {
-        if (x < 2) return vector<int>();
-        vector<int> res = {2};
+    std::vector<int> prime_numbers(int x) const {
+        if (x < 2) return std::vector<int>();
+        std::vector<int> res = {2};
         for (int i = 3; i <= x; i += 2) {
             if (this->is_prime(i)) res.emplace_back(i);
         }
         return res;
     }
 
-    // 素因数分解
-    vector<int> prime_factorization(int x) {
-        vector<int> res;
-        while (x > 1) {
-            res.emplace_back(factor[x]);
-            x /= factor[x];
-        }
-        return res;
-    }
+  private:
+    std::bitset<N + 1> prime_number;
 };
-prime_number pn;
