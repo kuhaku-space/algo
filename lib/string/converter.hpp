cbd1664d7be2eb4869e57bb38f185a396e6d16e6
kuhaku-space <te4958yu6924@gmail.com>
2023-02-11 17:20:29 +0900

    Add `string_converter` class

---
+++ lib/string/converter.hpp
@@ -0,0 +1,43 @@
+#include "template/template.hpp"
+
+struct string_converter {
+    char type(const char &c) const {
+        return (std::islower(c) ? 'a' : std::isupper(c) ? 'A' : std::isdigit(c) ? '0' : 0);
+    }
+    int convert(const char &c) {
+        if (!start) start = type(c);
+        return c - start;
+    }
+    int convert(const char &c, const std::string &chars) { return chars.find(c); }
+    template <typename T>
+    auto convert(const T &v) {
+        std::vector<decltype(convert(v[0]))> res;
+        res.reserve(v.size());
+        for (auto &&e : v) res.emplace_back(convert(e));
+        return res;
+    }
+    template <typename T>
+    auto convert(const T &v, const std::string &chars) {
+        std::vector<decltype(convert(v[0], chars))> res;
+        res.reserve(v.size());
+        for (auto &&e : v) res.emplace_back(convert(e, chars));
+        return res;
+    }
+    int operator()(const char &v, char s = 0) {
+        start = s;
+        return convert(v);
+    }
+    int operator()(const char &v, const std::string &chars) { return convert(v, chars); }
+    template <typename T>
+    auto operator()(const T &v, char s = 0) {
+        start = s;
+        return convert(v);
+    }
+    template <typename T>
+    auto operator()(const T &v, const std::string &chars) {
+        return convert(v, chars);
+    }
+
+  private:
+    char start = 0;
+} to_int;
