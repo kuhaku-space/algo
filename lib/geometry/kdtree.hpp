a159d6a7e56fd447842e9d15774058a48226b256
kuhaku-space <te4958yu6924@gmail.com>
2023-05-07 21:03:10 +0900

    Improve `kdtree` class

--- lib/geometry/kdtree.hpp
+++ lib/geometry/kdtree.hpp
@@ -1,113 +1,127 @@
 #include "template/template.hpp"
 
-/*
- * reference :
- * https://tjkendev.github.io/procon-library/cpp/range_query/kd-tree.html
- * verify :
- * https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_C 21/03/29
- * https://atcoder.jp/contests/arc010/tasks/arc010_4 21/03/30
+/**
+ * @brief kd-tree
+ *
+ * @see https://tjkendev.github.io/procon-library/cpp/range_query/kd-tree.html
  */
 struct kdtree {
-    struct Point {
+  private:
+    struct _point {
         int id;
         int x, y;
+
+        constexpr _point() : id(), x(), y() {}
+        constexpr _point(int id, int x, int y) : id(id), x(x), y(y) {}
     };
-    struct Node {
+    struct _node {
         int left, right;
-        Point p;
+        _point p;
+
+        constexpr _node() : left(), right(), p() {}
+        constexpr _node(int left, int right, _point p) : left(left), right(right), p(p) {}
     };
 
-    std::vector<Point> points;
-    std::vector<Node> nodes;
-
+  public:
     kdtree() : points(), nodes() {}
 
     void add(int x, int y) {
-        int id = points.size();
-        points.emplace_back(Point{id, x, y});
+        int id = this->points.size();
+        this->points.emplace_back(id, x, y);
     }
 
-    int make(int l, int r, int depth) {
+    void build() { this->build(0, this->points.size(), 0); }
+
+    std::int64_t find(int x, int y) { return this->find(0, x, y, 0, -1); }
+
+    // [sx, tx) * [sy, ty)
+    std::vector<int> find(int sx, int tx, int sy, int ty) {
+        return this->find(0, sx, tx, sy, ty, 0);
+    }
+
+  private:
+    std::vector<_point> points;
+    std::vector<_node> nodes;
+
+    int build(int l, int r, int depth) {
         if (l == r) {
             return -1;
         }
         if (r - l == 1) {
-            int res = nodes.size();
-            nodes.emplace_back(Node{-1, -1, points[l]});
+            int res = this->nodes.size();
+            this->nodes.emplace_back(-1, -1, this->points[l]);
             return res;
         }
         int mid = (l + r) >> 1;
         if (depth & 1) {
-            std::sort(points.begin() + l, points.begin() + r, [](const Point &a, const Point &b) {
-                return a.x < b.x;
-            });
+            std::sort(this->points.begin() + l, this->points.begin() + r,
+                      [](const _point &a, const _point &b) {
+                          return a.x < b.x;
+                      });
         } else {
-            std::sort(points.begin() + l, points.begin() + r, [](const Point &a, const Point &b) {
-                return a.y < b.y;
-            });
+            std::sort(this->points.begin() + l, this->points.begin() + r,
+                      [](const _point &a, const _point &b) {
+                          return a.y < b.y;
+                      });
         }
-        int res = nodes.size();
-        nodes.emplace_back(Node{});
-        int left = make(l, mid, depth + 1);
-        int right = make(mid + 1, r, depth + 1);
-        nodes[res] = Node{left, right, points[mid]};
+        int res = this->nodes.size();
+        this->nodes.emplace_back();
+        int left = this->build(l, mid, depth + 1);
+        int right = this->build(mid + 1, r, depth + 1);
+        this->nodes[res] = _node(left, right, this->points[mid]);
         return res;
     }
-    void build() { make(0, points.size(), 0); }
 
     std::int64_t find(int idx, int x, int y, int depth, std::int64_t r) {
         if (idx == -1) return r;
-        Point &p = nodes[idx].p;
+        _point &p = this->nodes[idx].p;
         std::int64_t d = std::int64_t(x - p.x) * (x - p.x) + std::int64_t(y - p.y) * (y - p.y);
         if (r == -1 || d < r) r = d;
 
         if (depth & 1) {
-            if (nodes[idx].left != -1 && x - r <= p.x) {
-                r = find(nodes[idx].left, x, y, depth + 1, r);
+            if (this->nodes[idx].left != -1 && x - r <= p.x) {
+                r = this->find(this->nodes[idx].left, x, y, depth + 1, r);
             }
-            if (nodes[idx].right != -1 && p.x <= x + r) {
-                r = find(nodes[idx].right, x, y, depth + 1, r);
+            if (this->nodes[idx].right != -1 && p.x <= x + r) {
+                r = this->find(this->nodes[idx].right, x, y, depth + 1, r);
             }
         } else {
-            if (nodes[idx].left != -1 && y - r <= p.y) {
-                r = find(nodes[idx].left, x, y, depth + 1, r);
+            if (this->nodes[idx].left != -1 && y - r <= p.y) {
+                r = this->find(this->nodes[idx].left, x, y, depth + 1, r);
             }
-            if (nodes[idx].right != -1 && p.y <= y + r) {
-                r = find(nodes[idx].right, x, y, depth + 1, r);
+            if (this->nodes[idx].right != -1 && p.y <= y + r) {
+                r = this->find(this->nodes[idx].right, x, y, depth + 1, r);
             }
         }
         return r;
     }
-    std::int64_t find(int x, int y) { return find(0, x, y, 0, -1); }
 
-    // [sx, tx) * [sy, ty)
     std::vector<int> find(int idx, int sx, int tx, int sy, int ty, int depth) {
         std::vector<int> res;
-        Point &p = nodes[idx].p;
+        _point &p = this->nodes[idx].p;
         if (sx <= p.x && p.x < tx && sy <= p.y && p.y < ty) {
             res.emplace_back(p.id);
         }
 
         if (depth & 1) {
-            if (nodes[idx].left != -1 && sx <= p.x) {
-                auto v = find(nodes[idx].left, sx, tx, sy, ty, depth + 1);
+            if (this->nodes[idx].left != -1 && sx <= p.x) {
+                auto v = this->find(this->nodes[idx].left, sx, tx, sy, ty, depth + 1);
                 res.insert(res.end(), v.begin(), v.end());
             }
-            if (nodes[idx].right != -1 && p.x < tx) {
-                auto v = find(nodes[idx].right, sx, tx, sy, ty, depth + 1);
+            if (this->nodes[idx].right != -1 && p.x < tx) {
+                auto v = this->find(this->nodes[idx].right, sx, tx, sy, ty, depth + 1);
                 res.insert(res.end(), v.begin(), v.end());
             }
         } else {
-            if (nodes[idx].left != -1 && sy <= p.y) {
-                auto v = find(nodes[idx].left, sx, tx, sy, ty, depth + 1);
+            if (this->nodes[idx].left != -1 && sy <= p.y) {
+                auto v = this->find(this->nodes[idx].left, sx, tx, sy, ty, depth + 1);
                 res.insert(res.end(), v.begin(), v.end());
             }
-            if (nodes[idx].right != -1 && p.y < ty) {
-                auto v = find(nodes[idx].right, sx, tx, sy, ty, depth + 1);
+            if (this->nodes[idx].right != -1 && p.y < ty) {
+                auto v = this->find(this->nodes[idx].right, sx, tx, sy, ty, depth + 1);
                 res.insert(res.end(), v.begin(), v.end());
             }
         }
         return res;
     }
-    std::vector<int> find(int sx, int tx, int sy, int ty) { return find(0, sx, tx, sy, ty, 0); }
 };
