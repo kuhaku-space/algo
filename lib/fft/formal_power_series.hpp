53144a7d7a602bdc62f0557b7a94868d6efe3162
kuhaku-space <te4958yu6924@gmail.com>
2023-05-17 11:39:38 +0900

    Improve `convolution` function
    `NTT` クラスにまとめていたものを `convolution` 関数に変更する．

--- lib/fft/formal_power_series.hpp
+++ lib/fft/formal_power_series.hpp
@@ -2,61 +2,84 @@
 #include "math/modint.hpp"
 #include "template/template.hpp"
 
-template <class T>
-struct formal_power_series {
-    std::vector<T> data;
+template <class mint>
+struct formal_power_series : vector<mint> {
+    using fps = formal_power_series;
 
-    formal_power_series(std::vector<T> _v) : data(_v) {}
+    formal_power_series(std::vector<T> &&v) : vector<mint>(v) {}
+    formal_power_series(const std::vector<T> &v) : vector<mint>(v) {}
 
-    const Mint &operator[](const std::int64_t x) const { return data[x]; }
-    Mint &operator[](const std::int64_t x) { return data[x]; }
+    constexpr fps &operator+=(const mint &v) {
+        if (this->empty()) this->resize(1);
+        (*this)[0] += v;
+        return *this;
+    }
+    constexpr fps &operator+=(const fps &f) {
+        if (f.size() > this->size()) this->resize(f.size());
+        for (int i = 0; i < (int)f.size(); ++i) (*this)[i] += f[i];
+        return this->normalize();
+    }
+    constexpr fps &operator-=(const mint &v) {
+        if (this->empty()) this->resize(1);
+        (*this)[0] -= v;
+        return *this;
+    }
+    constexpr fps &operator-=(const fps &f) {
+        if (f.size() > this->size()) this->resize(f.size());
+        for (int i = 0; i < (int)f.size(); ++i) (*this)[i] -= f[i];
+        return this->normalize();
+    }
+    constexpr fps &operator*=(const mint &v) {
+        for (int i = 0; i < (int)this->size(); ++i) (*this)[i] *= v;
+        return *this;
+    }
+    constexpr fps &operator*=(const fps &f) { return *this = convolution((*this), f); }
+    constexpr fps &operator/=(const mint &v) {
+        assert(v != 0);
+        mint iv = modinv(v);
+        for (int i = 0; i < (int)this->size(); ++i) (*this)[i] *= iv;
+        return *this;
+    }
+    constexpr fps &operator<<=(int x) {
+        fps res(x, 0);
+        res.insert(res.end(), begin(*this), end(*this));
+        return *this = res;
+    }
+    constexpr fps &operator>>=(int x) {
+        fps res;
+        res.insert(res.end(), begin(*this) + x, end(*this));
+        return *this = res;
+    }
 
-    inline size_t size() const { return data.size(); }
+    constexpr fps operator+(const mint &v) const { return fps(*this) += v; }
+    constexpr fps operator+(const fps &f) const { return fps(*this) += f; }
+    constexpr fps operator-(const mint &v) const { return fps(*this) -= v; }
+    constexpr fps operator-(const fps &f) const { return fps(*this) -= f; }
+    constexpr fps operator*(const mint &v) const { return fps(*this) *= v; }
+    constexpr fps operator*(const fps &f) const { return fps(*this) *= f; }
+    constexpr fps operator/(const mint &v) const { return fps(*this) /= v; }
+    constexpr fps operator<<(int x) const { return fps(*this) <<= x; }
+    constexpr fps operator>>(int x) const { return fps(*this) >>= x; }
 
-    void resize(size_t _sz) { data.resize(_sz); }
+    constexpr fps operator+() const { return fps(*this); }
+    constexpr fps operator-() const {
+        fps res = *this;
+        for (int i = 0; i < (int)res.size(); ++i) res[i] = -res[i];
+        return res;
+    }
 
-    void pow(std::int64_t x) {
-        std::int64_t n = size();
-        std::vector<std::int64_t> ans(n);
-        ans[0] = T(1);
-        for (; x > 0; x >>= 1) {
-            if (x & 1) {
-                ntt.convolution_self(ans, data);
-                ans.resize(n);
-            }
-            ntt.convolution_self(data, data);
-            data.resize(n);
+    constexpr fps pre(int sz) const {
+        return fps(std::begin(*this), std::begin(*this) + std::min((int)this->size(), sz));
+    }
+    constexpr fps inv(int deg) {
+        assert((*this)[0] != 0);
+        if (deg < 0) deg = (int)v.size();
+        fps res({mint(1) / (*this)[0]});
+        for (int i = 1; i < deg; i <<= 1) {
+            res = (res + res - res * res * this->pre(i << 1)).pre(i << 1);
         }
-        swap(data, ans);
+        res.resize(deg);
+        return res;
     }
-
-    void conv_naive(const formal_power_series &a) {
-        std::int64_t n = size() + a.size() - 1;
-        std::vector<T> ans(n);
-        for (std::int64_t i = 0; i < a.size(); ++i) {
-            if (a[i] == 0) continue;
-            for (std::int64_t j = 0; j < size(); ++j) {
-                ans[i + j] += data[j] * a[i];
-            }
-        }
-        swap(data, ans);
-    }
-
-    // data * (1 - x^n)
-    void mul(std::int64_t n) {
-        for (std::int64_t i = size() - 1; i >= n; --i) data[i] -= data[i - n];
-    }
-
-    // data / (1 - x^n)
-    void div(std::int64_t n) {
-        for (std::int64_t i = n; i < size(); ++i) data[i] += data[i - n];
-    }
-
-    void cumsum() {
-        for (std::int64_t i = 1; i < size(); ++i) data[i] += data[i - 1];
-    }
-
-    void cumsum_inv() {
-        for (std::int64_t i = size() - 1; i > 0; --i) data[i] -= data[i - 1];
-    }
+    constexpr fps inv() { return inv(v.size()); }
 };
