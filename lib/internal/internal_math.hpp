e5b1ed87db5d864dace37e89fb11ea53a85ba037
kuhaku-space <te4958yu6924@gmail.com>
2023-05-17 11:33:44 +0900

    Improve modint
    `ModInt` クラスを `static_modint` クラスに変更する．
    `dynamic_modint` クラスを追加する．

---
+++ lib/internal/internal_math.hpp
@@ -0,0 +1,170 @@
+#pragma once
+#include "template/template.hpp"
+
+namespace internal {
+
+// @param m `1 <= m`
+// @return x mod m
+constexpr std::int64_t safe_mod(std::int64_t x, std::int64_t m) {
+    x %= m;
+    if (x < 0) x += m;
+    return x;
+}
+
+// Fast modular multiplication by barrett reduction
+// Reference: https://en.wikipedia.org/wiki/Barrett_reduction
+// NOTE: reconsider after Ice Lake
+struct barrett {
+    unsigned int _m;
+    std::uint64_t im;
+
+    // @param m `1 <= m`
+    explicit barrett(unsigned int m) : _m(m), im((std::uint64_t)(-1) / m + 1) {}
+
+    // @return m
+    unsigned int umod() const { return _m; }
+
+    // @param a `0 <= a < m`
+    // @param b `0 <= b < m`
+    // @return `a * b % m`
+    unsigned int mul(unsigned int a, unsigned int b) const {
+        std::uint64_t z = a;
+        z *= b;
+        std::uint64_t x = (std::uint64_t)(((unsigned __int128)(z)*im) >> 64);
+        std::uint64_t y = x * _m;
+        return (unsigned int)(z - y + (z < y ? _m : 0));
+    }
+};
+
+// @param n `0 <= n`
+// @param m `1 <= m`
+// @return `(x ** n) % m`
+constexpr std::int64_t pow_mod_constexpr(std::int64_t x, std::int64_t n, int m) {
+    if (m == 1) return 0;
+    unsigned int _m = (unsigned int)(m);
+    std::uint64_t r = 1;
+    std::uint64_t y = safe_mod(x, m);
+    while (n) {
+        if (n & 1) r = (r * y) % _m;
+        y = (y * y) % _m;
+        n >>= 1;
+    }
+    return r;
+}
+
+// Reference:
+// M. Forisek and J. Jancina,
+// Fast Primality Testing for Integers That Fit into a Machine Word
+// @param n `0 <= n`
+constexpr bool is_prime_constexpr(int n) {
+    if (n <= 1) return false;
+    if (n == 2 || n == 7 || n == 61) return true;
+    if (n % 2 == 0) return false;
+    std::int64_t d = n - 1;
+    while (d % 2 == 0) d /= 2;
+    constexpr std::int64_t bases[3] = {2, 7, 61};
+    for (std::int64_t a : bases) {
+        std::int64_t t = d;
+        std::int64_t y = pow_mod_constexpr(a, t, n);
+        while (t != n - 1 && y != 1 && y != n - 1) {
+            y = y * y % n;
+            t <<= 1;
+        }
+        if (y != n - 1 && t % 2 == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+template <int n>
+constexpr bool is_prime = is_prime_constexpr(n);
+
+// @param b `1 <= b`
+// @return pair(g, x) s.t. g = gcd(a, b), xa = g (mod b), 0 <= x < b/g
+constexpr std::pair<std::int64_t, std::int64_t> inv_gcd(std::int64_t a, std::int64_t b) {
+    a = safe_mod(a, b);
+    if (a == 0) return {b, 0};
+    std::int64_t s = b, t = a;
+    std::int64_t m0 = 0, m1 = 1;
+    while (t) {
+        std::int64_t u = s / t;
+        s -= t * u;
+        m0 -= m1 * u;
+        auto tmp = s;
+        s = t;
+        t = tmp;
+        tmp = m0;
+        m0 = m1;
+        m1 = tmp;
+    }
+    if (m0 < 0) m0 += b / s;
+    return {s, m0};
+}
+
+// Compile time primitive root
+// @param m must be prime
+// @return primitive root (and minimum in now)
+constexpr int primitive_root_constexpr(int m) {
+    if (m == 2) return 1;
+    if (m == 167772161) return 3;
+    if (m == 469762049) return 3;
+    if (m == 754974721) return 11;
+    if (m == 998244353) return 3;
+    int divs[20] = {};
+    divs[0] = 2;
+    int cnt = 1;
+    int x = (m - 1) / 2;
+    while (x % 2 == 0) x /= 2;
+    for (int i = 3; (std::int64_t)(i)*i <= x; i += 2) {
+        if (x % i == 0) {
+            divs[cnt++] = i;
+            while (x % i == 0) {
+                x /= i;
+            }
+        }
+    }
+    if (x > 1) {
+        divs[cnt++] = x;
+    }
+    for (int g = 2;; g++) {
+        bool ok = true;
+        for (int i = 0; i < cnt; i++) {
+            if (pow_mod_constexpr(g, (m - 1) / divs[i], m) == 1) {
+                ok = false;
+                break;
+            }
+        }
+        if (ok) return g;
+    }
+}
+template <int m>
+constexpr int primitive_root = primitive_root_constexpr(m);
+
+// @param n `n < 2^32`
+// @param m `1 <= m < 2^32`
+// @return sum_{i=0}^{n-1} floor((ai + b) / m) (mod 2^64)
+std::uint64_t floor_sum_unsigned(std::uint64_t n, std::uint64_t m, std::uint64_t a,
+                                 std::uint64_t b) {
+    std::uint64_t ans = 0;
+    while (true) {
+        if (a >= m) {
+            ans += n * (n - 1) / 2 * (a / m);
+            a %= m;
+        }
+        if (b >= m) {
+            ans += n * (b / m);
+            b %= m;
+        }
+
+        std::uint64_t y_max = a * n + b;
+        if (y_max < m) break;
+        // y_max < m * (n + 1)
+        // floor(y_max / m) <= n
+        n = (std::uint64_t)(y_max / m);
+        b = (std::uint64_t)(y_max % m);
+        std::swap(m, a);
+    }
+    return ans;
+}
+
+}  // namespace internal
