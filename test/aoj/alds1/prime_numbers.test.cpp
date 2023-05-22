dfc82b3b0461f0cdc5b57b01812967a38f2b145a
kuhaku-space <te4958yu6924@gmail.com>
2023-05-22 16:35:27 +0900

    Update test

--- test/aoj/alds1/prime_numbers.test.cpp
+++ test/aoj/alds1/prime_numbers.test.cpp
@@ -1,11 +1,10 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/1/ALDS1_1_C"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_1_C"
 #include "math/prime_number.hpp"
 #include "template/atcoder.hpp"
 
 prime_number pn;
 
 int main(void) {
-    sonic();
     int n;
     cin >> n;
     int ans = 0;
