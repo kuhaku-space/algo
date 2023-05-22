a5b96e7e152b50a06ac392054b39117ce8cc377b
kuhaku-space <te4958yu6924@gmail.com>
2023-05-22 16:57:35 +0900

    Update test

--- test/aoj/grl/bellman_ford.test.cpp
+++ test/aoj/grl/bellman_ford.test.cpp
@@ -1,26 +1,23 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all/GRL_1_B"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/GRL_1_B"
 #include "graph/bellman_ford.hpp"
 #include "template/atcoder.hpp"
 
 int main(void) {
-    sonic();
     int n, m, r;
     cin >> n >> m >> r;
     Graph<int> g(n);
     g.input_edge(m, 0);
 
     auto dist = bellman_ford(g, r, Inf);
-    rep(i, n) {
+    rep (i, n) {
         if (dist[i] == -Inf) {
             co("NEGATIVE CYCLE");
             return 0;
         }
     }
-    rep(i, n) {
-        if (dist[i] != Inf)
-            co(dist[i]);
-        else
-            co("INF");
+    rep (i, n) {
+        if (dist[i] != Inf) co(dist[i]);
+        else co("INF");
     }
 
     return 0;
