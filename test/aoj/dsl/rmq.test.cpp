6221ba7917e58b6d740cb4d11a4ad323b45700c9
kuhaku-space <te4958yu6924@gmail.com>
2023-07-12 07:28:48 +0900

    Update test

--- test/aoj/dsl/rmq.test.cpp
+++ test/aoj/dsl/rmq.test.cpp
@@ -1,19 +1,16 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_A"
-#include "template/atcoder.hpp"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_A"
 #include "segment_tree/segment_tree.hpp"
+#include "template/atcoder.hpp"
 
 int main(void) {
-    sonic();
     int n, q;
-    cin >> n >> q;
+    std::cin >> n >> q;
     segment_tree<Min<int>> st(n, (1LL << 31) - 1);
-    rep(i, q) {
+    while (q--) {
         int com, x, y;
-        cin >> com >> x >> y;
-        if (com == 0)
-            st.set(x, y);
-        else
-            co(st.prod(x, y + 1));
+        std::cin >> com >> x >> y;
+        if (com == 0) st.set(x, y);
+        else co(st.prod(x, y + 1));
     }
 
     return 0;
