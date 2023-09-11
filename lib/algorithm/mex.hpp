234f7f020cfc3792275a3f9503293979e2341552
kuhaku-space <te4958yu6924@gmail.com>
2023-09-12 08:13:11 +0900

    Rename field

--- lib/algorithm/mex.hpp
+++ lib/algorithm/mex.hpp
@@ -5,7 +5,7 @@
  *
  */
 struct minimum_excluded {
-    minimum_excluded() : n(), _size(), is_exist(64), v() {}
+    minimum_excluded() : n(), _size(), exists(64), v() {}
 
     constexpr int operator()() const noexcept { return this->n; }
     constexpr int get() const noexcept { return this->n; }
@@ -13,30 +13,30 @@ struct minimum_excluded {
     void add(int x) {
         if (x < 0) return;
         ++this->_size;
-        if (this->_size == (int)this->is_exist.size()) {
-            this->is_exist.resize(this->_size << 1);
+        if (this->_size == (int)this->exists.size()) {
+            this->exists.resize(this->_size << 1);
             int cnt = 0;
             for (int i = 0; i < (int)this->v.size(); ++i) {
-                if (this->v[i] < (int)this->is_exist.size()) {
-                    if (this->is_exist[this->v[i]]) --this->_size;
-                    else this->is_exist[this->v[i]] = true;
+                if (this->v[i] < (int)this->exists.size()) {
+                    if (this->exists[this->v[i]]) --this->_size;
+                    else this->exists[this->v[i]] = true;
                 } else {
                     this->v[cnt++] = this->v[i];
                 }
             }
             this->v.erase(this->v.begin() + cnt, this->v.end());
         }
-        if (x < (int)this->is_exist.size()) {
-            if (this->is_exist[x]) --this->_size;
-            else this->is_exist[x] = true;
+        if (x < (int)this->exists.size()) {
+            if (this->exists[x]) --this->_size;
+            else this->exists[x] = true;
         } else {
             this->v.emplace_back(x);
         }
-        while (this->is_exist[this->n]) ++this->n;
+        while (this->exists[this->n]) ++this->n;
     }
 
   private:
     int n, _size;
-    std::vector<bool> is_exist;
+    std::vector<bool> exists;
     std::vector<int> v;
 };
