e61991e12a115298458729005d196ad8e27da4d6
kuhaku-space <te4958yu6924@gmail.com>
2023-09-19 16:56:06 +0900

    Improve `HLD` class
    `g` フィールドを削除する．
    `Graph` クラスをコンストラクタに渡すことで構築する．

--- lib/tree/hld.hpp
+++ lib/tree/hld.hpp
@@ -8,38 +8,9 @@
  */
 struct HLD {
     HLD() = default;
-    HLD(int n) : _size(n), g(n), vid(n, -1), nxt(n), sub(n, 1), par(n, -1), inv(n) {}
-
     template <class T>
     HLD(const Graph<T> &g, int r = 0) : HLD(g.size()) {
-        for (auto &es : g) {
-            for (auto &e : es) {
-                g[e.from()].emplace_back(e.to());
-            }
-        }
-        build(r);
-    }
-
-    void add_edge(int u, int v) { add_edges(u, v); }
-    void add_edges(int u, int v) {
-        g[u].emplace_back(v);
-        g[v].emplace_back(u);
-    }
-
-    void input_edge(int base = 1) { input_edges(base); }
-    void input_edges(int base = 1) {
-        for (int i = 0; i < _size - 1; ++i) {
-            int u, v;
-            std::cin >> u >> v;
-            add_edges(u - base, v - base);
-        }
-    }
-
-    void build(int r = 0) {
-        int pos = 0;
-        dfs_sz(r);
-        nxt[r] = r;
-        dfs_hld(r, pos);
+        build(g, r);
     }
 
     int get(int v) const { return vid[v]; }
@@ -106,28 +77,46 @@ struct HLD {
 
   private:
     int _size;
-    std::vector<std::vector<int>> g;
     std::vector<int> vid, nxt, sub, par, inv;
 
-    void dfs_sz(int v) {
-        auto &es = g[v];
-        if (~(par[v])) es.erase(find(es.begin(), es.end(), par[v]));
+    HLD(int n) : _size(n), vid(n, -1), nxt(n), sub(n, 1), par(n, -1), inv(n) {}
 
-        for (auto &u : es) {
+    template <class T>
+    void build(const Graph<T> &g, int r = 0) {
+        std::vector<int> heavy_path(_size, -1);
+        dfs_sz(g, r, heavy_path);
+        nxt[r] = r;
+        int pos = 0;
+        dfs_hld(g, r, pos, heavy_path);
+    }
+
+    template <class T>
+    void dfs_sz(const Graph<T> &g, int v, std::vector<int> &heavy_path) {
+        int max_sub = 0;
+        for (auto &e : g[v]) {
+            int u = e.to();
+            if (u == par[v]) continue;
             par[u] = v;
-            dfs_sz(u);
+            dfs_sz(g, u, heavy_path);
             sub[v] += sub[u];
-            if (sub[u] > sub[es[0]]) std::swap(u, es[0]);
+            if (chmax(max_sub, sub[u])) heavy_path[v] = u;
         }
     }
 
-    void dfs_hld(int v, int &pos) {
+    template <class T>
+    void dfs_hld(const Graph<T> &g, int v, int &pos, const std::vector<int> &heavy_path) {
         vid[v] = pos++;
         inv[vid[v]] = v;
-        for (auto u : g[v]) {
-            if (u == par[v]) continue;
-            nxt[u] = (u == g[v][0] ? nxt[v] : u);
-            dfs_hld(u, pos);
+        int hp = heavy_path[v];
+        if (hp != -1) {
+            nxt[hp] = nxt[v];
+            dfs_hld(g, hp, pos, heavy_path);
+        }
+        for (auto &e : g[v]) {
+            int u = e.to();
+            if (u == par[v] || u == heavy_path[v]) continue;
+            nxt[u] = u;
+            dfs_hld(g, u, pos, heavy_path);
         }
     }
 };
