f3607141ecb0f2c2f578187c5a71e86d3fa080d5
kuhaku-space <te4958yu6924@gmail.com>
2023-09-19 15:31:01 +0900

    Update tests

--- test/aoj/grl/range_query_on_tree.test.cpp
+++ test/aoj/grl/range_query_on_tree.test.cpp
@@ -1,39 +1,39 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/5/GRL_5_D"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/GRL_5_D"
 #include "binary_tree/fenwick_tree.hpp"
 #include "template/atcoder.hpp"
 #include "tree/hld.hpp"
 
 int main(void) {
     int n;
-    cin >> n;
-    fenwick_tree<ll> ft(n);
-    HLD hld(n);
+    std::cin >> n;
+    Graph<void> g(n);
     rep (i, n) {
         int k;
-        cin >> k;
-        rep (j, k) {
+        std::cin >> k;
+        while (k--) {
             int c;
-            cin >> c;
-            hld.add_edge(i, c);
+            std::cin >> c;
+            g.add_edges(i, c);
         }
     }
-    hld.build();
 
+    HLD hld(g);
+    fenwick_tree<ll> ft(n);
     int q;
     cin >> q;
     while (q--) {
         int id;
-        cin >> id;
+        std::cin >> id;
         if (id == 0) {
             int v, w;
-            cin >> v >> w;
+            std::cin >> v >> w;
             auto f = [&](int a, int b) {
                 ft.add(a, w);
             };
             hld.for_each_edge(hld.get_parent(v), v, f);
         } else {
             int v;
-            cin >> v;
+            std::cin >> v;
             ll ans = 0;
             auto f = [&](int a, int b) {
                 ans += ft.sum(a, b);
