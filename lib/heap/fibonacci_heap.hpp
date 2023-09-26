665628014012dea845be5dd977f76ba09648eb1c
kuhaku-space <te4958yu6924@gmail.com>
2023-09-26 20:36:48 +0900

    Remove this pointer in `fibonacci_heap` class

--- lib/heap/fibonacci_heap.hpp
+++ lib/heap/fibonacci_heap.hpp
@@ -26,30 +26,30 @@ struct fibonacci_heap {
 
         void add_child(pointer node) {
             node->parent = this;
-            if (this->child) this->child->insert_left(node);
-            else this->child = node;
-            ++(this->order);
+            if (child) child->insert_left(node);
+            else child = node;
+            ++order;
         }
         void insert_left(pointer node) {
             node->right = this;
-            node->left = this->left;
-            this->left->right = node;
-            this->left = node;
+            node->left = left;
+            left->right = node;
+            left = node;
         }
         void insert_right(pointer node) {
             node->left = this;
-            node->right = this->right;
-            this->right->left = node;
-            this->right = node;
+            node->right = right;
+            right->left = node;
+            right = node;
         }
 
         pointer erase() {
-            this->parent = nullptr;
-            if (this->left == this) return nullptr;
-            this->left->right = this->right;
-            this->right->left = this->left;
-            auto res = this->left;
-            this->left = this->right = this;
+            parent = nullptr;
+            if (left == this) return nullptr;
+            left->right = right;
+            right->left = left;
+            auto res = left;
+            left = right = this;
             return res;
         }
 
@@ -61,40 +61,40 @@ struct fibonacci_heap {
 
     fibonacci_heap() : _root(nullptr), _size(), comp() {}
 
-    bool empty() const { return this->_size == 0; }
-    constexpr int size() const { return this->_size; }
-    constexpr auto top() const { return this->_root->get_pair(); }
+    constexpr bool empty() const { return _size == 0; }
+    constexpr int size() const { return _size; }
+    constexpr auto top() const { return _root->get_pair(); }
 
     auto push(Key key, Value value) {
-        ++(this->_size);
+        ++_size;
         auto node = new _node(key, value);
-        if (!this->_root) {
-            this->_root = node;
+        if (!_root) {
+            _root = node;
         } else {
-            this->_root->insert_left(node);
-            if (comp(this->_root->value, value)) this->_root = this->_root->left;
+            _root->insert_left(node);
+            if (comp(_root->value, value)) _root = _root->left;
         }
         return node;
     }
-    auto emplace(Key key, Value value) { return this->push(key, value); }
+    auto emplace(Key key, Value value) { return push(key, value); }
 
     void pop() {
-        --(this->_size);
-        if (this->_root->child) {
-            auto child = this->_root->child, left = child->left;
-            this->_root->left->right = child;
-            child->left->right = this->_root;
-            child->left = this->_root->left;
-            this->_root->left = left;
+        --_size;
+        if (_root->child) {
+            auto child = _root->child, left = child->left;
+            _root->left->right = child;
+            child->left->right = _root;
+            child->left = _root->left;
+            _root->left = left;
         }
-        this->_root = this->_root->erase();
-        if (!this->_root) return;
+        _root = _root->erase();
+        if (!_root) return;
 
         node_ptr nodes[30] = {};
-        while (this->_root) {
-            auto node = this->_root;
+        while (_root) {
+            auto node = _root;
             auto order = node->order;
-            this->_root = this->_root->erase();
+            _root = _root->erase();
             while (nodes[order]) {
                 if (comp(node->value, nodes[order]->value)) std::swap(node, nodes[order]);
                 node->add_child(nodes[order]);
@@ -105,10 +105,10 @@ struct fibonacci_heap {
         }
 
         for (auto node : nodes) {
-            if (node && (!this->_root || comp(this->_root->value, node->value))) this->_root = node;
+            if (node && (!_root || comp(_root->value, node->value))) _root = node;
         }
         for (auto node : nodes) {
-            if (node && node != this->_root) this->_root->insert_left(node);
+            if (node && node != _root) _root->insert_left(node);
         }
     }
 
@@ -116,7 +116,7 @@ struct fibonacci_heap {
         if (comp(node->value, value)) node->value = value;
         else return;
         if (!node->parent) {
-            if (comp(this->_root->value, value)) this->_root = node;
+            if (comp(_root->value, value)) _root = node;
             return;
         } else if (!comp(node->parent->value, node->value)) {
             return;
@@ -126,8 +126,8 @@ struct fibonacci_heap {
             node->damaged = false;
             parent->child = node->erase();
             --(parent->order);
-            this->_root->insert_left(node);
-            if (comp(this->_root->value, this->_root->left->value)) this->_root = this->_root->left;
+            _root->insert_left(node);
+            if (comp(_root->value, _root->left->value)) _root = _root->left;
             if (!parent->damaged) {
                 parent->damaged = true;
                 break;
