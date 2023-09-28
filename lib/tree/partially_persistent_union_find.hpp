bcb5e2c1935850944a97da88454ab50269e9c013
kuhaku-space <te4958yu6924@gmail.com>
2023-09-28 19:14:01 +0900

    Remove this pointer

--- lib/tree/partially_persistent_union_find.hpp
+++ lib/tree/partially_persistent_union_find.hpp
@@ -8,37 +8,40 @@ struct partially_persistent_union_find {
     partially_persistent_union_find(int _n)
         : _now(0), _data(_n, std::vector<std::pair<int, int>>(1, {0, -1})) {}
 
+    int root(int x) const { return root(x, _now); }
     int root(int x, int t) const {
-        auto p = this->_data[x].back();
+        auto p = _data[x].back();
         if (p.first > t || p.second < 0) return x;
-        return this->root(p.second, t);
+        return root(p.second, t);
     }
-    int get_root(int x, int t) const { return this->root(x, t); }
+    int get_root(int x, int t) const { return root(x, t); }
 
     bool is_root(int x, int t) const {
-        return this->_data[x].back().first > t || this->_data[x].back().second < 0;
+        return _data[x].back().first > t || _data[x].back().second < 0;
     }
 
-    bool same(int x, int y, int t) const { return this->root(x, t) == this->root(y, t); }
-    bool is_same(int x, int y, int t) const { return this->same(x, y, t); }
+    bool same(int x, int y) const { return same(x, y, _now); }
+    bool same(int x, int y, int t) const { return root(x, t) == root(y, t); }
+    bool is_same(int x, int y, int t) const { return same(x, y, t); }
 
+    int size(int x) const { return size(x, _now); }
     int size(int x, int t) const {
-        x = this->root(x, t);
-        return -std::prev(std::upper_bound(this->_data[x].begin(), this->_data[x].end(),
-                                           std::pair<int, int>{t, this->_data.size()}))
+        x = root(x, t);
+        return -std::prev(std::upper_bound(_data[x].begin(), _data[x].end(),
+                                           std::pair<int, int>{t, _data.size()}))
                     ->second;
     }
-    int get_size(int x, int t) const { return this->size(x, t); }
+    int get_size(int x, int t) const { return size(x, t); }
 
     int unite(int x, int y) {
         ++_now;
-        x = this->root(x, _now), y = this->root(y, _now);
-        if (x == y) return _now;
-        auto p = this->_data[x].back(), q = this->_data[y].back();
+        x = root(x, _now), y = root(y, _now);
+        if (x == y) return false;
+        auto p = _data[x].back(), q = _data[y].back();
         if (p.second > q.second) std::swap(x, y), std::swap(p, q);
-        this->_data[x].emplace_back(_now, p.second + q.second);
-        this->_data[y].emplace_back(_now, x);
-        return _now;
+        _data[x].emplace_back(_now, p.second + q.second);
+        _data[y].emplace_back(_now, x);
+        return true;
     }
 
   private:
