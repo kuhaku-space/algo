3eb93cf093d9e0054fd28f4958555c7ad27e3191
kuhaku-space <te4958yu6924@gmail.com>
2023-09-29 20:02:53 +0900

    Remove this pointer

--- lib/segment_tree/dual_segment_tree.hpp
+++ lib/segment_tree/dual_segment_tree.hpp
@@ -13,35 +13,35 @@ struct dual_segment_tree {
     using T = typename M::value_type;
 
   public:
-    dual_segment_tree(int _n, T _e = M::id) { this->init(_n, _e); }
+    dual_segment_tree(int _n, T _e = M::id) { init(_n, _e); }
 
     void init(int n, T e) {
-        this->_log = ceil_pow2(n);
-        this->_size = 1 << this->_log;
-        this->data.assign(this->_size << 1, e);
+        _log = ceil_pow2(n);
+        _size = 1 << _log;
+        data.assign(_size << 1, e);
     }
 
-    T at(int k) { return this->get(k); }
+    T at(int k) { return get(k); }
     T get(int k) {
-        assert(0 <= k && k < this->_size);
-        k += this->_size;
-        for (int i = this->_log; i >= 1; --i) this->push(k >> i);
-        return this->data[k];
+        assert(0 <= k && k < _size);
+        k += _size;
+        for (int i = _log; i >= 1; --i) push(k >> i);
+        return data[k];
     }
 
-    void apply(int a, T val) { this->apply(a, a + 1, val); }
+    void apply(int a, T val) { apply(a, a + 1, val); }
     void apply(int a, int b, T val) {
-        assert(0 <= a && a <= this->_size);
-        assert(0 <= b && b <= this->_size);
-        a += this->_size, b += this->_size;
+        assert(0 <= a && a <= _size);
+        assert(0 <= b && b <= _size);
+        a += _size, b += _size;
         for (int i = _log; i >= 1; --i) {
-            if (((a >> i) << i) != a) this->push(a >> i);
-            if (((b >> i) << i) != b) this->push((b - 1) >> i);
+            if (((a >> i) << i) != a) push(a >> i);
+            if (((b >> i) << i) != b) push((b - 1) >> i);
         }
 
         for (; a < b; a >>= 1, b >>= 1) {
-            if (a & 1) this->all_apply(a++, val);
-            if (b & 1) this->all_apply(--b, val);
+            if (a & 1) all_apply(a++, val);
+            if (b & 1) all_apply(--b, val);
         }
     }
 
@@ -49,10 +49,10 @@ struct dual_segment_tree {
     int _size, _log;
     std::vector<T> data;
 
-    void all_apply(int k, T val) { this->data[k] = M::op(val, this->data[k]); }
+    void all_apply(int k, T val) { data[k] = M::op(val, data[k]); }
     void push(int k) {
-        this->all_apply(2 * k, this->data[k]);
-        this->all_apply(2 * k + 1, this->data[k]);
-        this->data[k] = M::id;
+        all_apply(2 * k, data[k]);
+        all_apply(2 * k + 1, data[k]);
+        data[k] = M::id;
     }
 };
