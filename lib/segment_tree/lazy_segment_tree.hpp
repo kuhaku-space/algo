ad08ed42ca41decd96d2f1b2aa59ad5f8b21b49a
kuhaku-space <te4958yu6924@gmail.com>
2023-10-01 06:28:20 +0900

    Remove this pointer

--- lib/segment_tree/lazy_segment_tree.hpp
+++ lib/segment_tree/lazy_segment_tree.hpp
@@ -22,96 +22,96 @@ struct lazy_segment_tree {
     explicit lazy_segment_tree(const std::vector<T> &v) : _n(int(v.size())) {
         _size = internal::bit_ceil(_n);
         _log = internal::countr_zero(_size);
-        this->data = std::vector<T>(2 * this->_size, S::id);
-        this->lazy = std::vector<U>(this->_size, F::id);
-        for (int i = 0; i < this->_n; i++) this->data[this->_size + i] = v[i];
-        for (int i = this->_size - 1; i >= 1; --i) this->update(i);
+        data = std::vector<T>(2 * _size, S::id);
+        lazy = std::vector<U>(_size, F::id);
+        for (int i = 0; i < _n; i++) data[_size + i] = v[i];
+        for (int i = _size - 1; i >= 1; --i) update(i);
     }
 
     void set(int p, T x) {
-        assert(0 <= p && p < this->_n);
-        p += this->_size;
-        for (int i = this->_log; i >= 1; --i) this->push(p >> i);
-        this->data[p] = x;
-        for (int i = 1; i <= this->_log; ++i) this->update(p >> i);
+        assert(0 <= p && p < _n);
+        p += _size;
+        for (int i = _log; i >= 1; --i) push(p >> i);
+        data[p] = x;
+        for (int i = 1; i <= _log; ++i) update(p >> i);
     }
 
-    T at(int p) { return this->get(p); }
+    T at(int p) { return get(p); }
     T get(int p) {
-        assert(0 <= p && p < this->_n);
-        p += this->_size;
-        for (int i = this->_log; i >= 1; --i) this->push(p >> i);
-        return this->data[p];
+        assert(0 <= p && p < _n);
+        p += _size;
+        for (int i = _log; i >= 1; --i) push(p >> i);
+        return data[p];
     }
 
     void apply(int p, U f) {
-        assert(0 <= p && p < this->_n);
-        p += this->_size;
-        for (int i = this->_log; i >= 1; --i) this->push(p >> i);
-        this->data[p] = F::f(f, this->data[p]);
-        for (int i = 1; i <= this->_log; ++i) this->update(p >> i);
+        assert(0 <= p && p < _n);
+        p += _size;
+        for (int i = _log; i >= 1; --i) push(p >> i);
+        data[p] = F::f(f, data[p]);
+        for (int i = 1; i <= _log; ++i) update(p >> i);
     }
     void apply(int l, int r, U f) {
-        assert(0 <= l && l <= r && r <= this->_n);
+        assert(0 <= l && l <= r && r <= _n);
         if (l == r) return;
 
-        l += this->_size, r += this->_size;
+        l += _size, r += _size;
 
-        for (int i = this->_log; i >= 1; --i) {
-            if (((l >> i) << i) != l) this->push(l >> i);
-            if (((r >> i) << i) != r) this->push((r - 1) >> i);
+        for (int i = _log; i >= 1; --i) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
         }
 
         int l2 = l, r2 = r;
         while (l < r) {
-            if (l & 1) this->all_apply(l++, f);
-            if (r & 1) this->all_apply(--r, f);
+            if (l & 1) all_apply(l++, f);
+            if (r & 1) all_apply(--r, f);
             l >>= 1, r >>= 1;
         }
         l = l2, r = r2;
 
-        for (int i = 1; i <= this->_log; i++) {
-            if (((l >> i) << i) != l) this->update(l >> i);
-            if (((r >> i) << i) != r) this->update((r - 1) >> i);
+        for (int i = 1; i <= _log; i++) {
+            if (((l >> i) << i) != l) update(l >> i);
+            if (((r >> i) << i) != r) update((r - 1) >> i);
         }
     }
 
     T prod(int l, int r) {
-        assert(0 <= l && l <= r && r <= this->_n);
+        assert(0 <= l && l <= r && r <= _n);
         if (l == r) return S::id;
 
-        l += this->_size, r += this->_size;
+        l += _size, r += _size;
 
-        for (int i = this->_log; i >= 1; --i) {
-            if (((l >> i) << i) != l) this->push(l >> i);
-            if (((r >> i) << i) != r) this->push((r - 1) >> i);
+        for (int i = _log; i >= 1; --i) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
         }
 
         T sml = S::id, smr = S::id;
         while (l < r) {
-            if (l & 1) sml = S::op(sml, this->data[l++]);
-            if (r & 1) smr = S::op(this->data[--r], smr);
+            if (l & 1) sml = S::op(sml, data[l++]);
+            if (r & 1) smr = S::op(data[--r], smr);
             l >>= 1, r >>= 1;
         }
 
         return S::op(sml, smr);
     }
 
-    T all_prod() const { return this->data[1]; }
+    T all_prod() const { return data[1]; }
 
   private:
     int _n, _size, _log;
     std::vector<T> data;
     std::vector<U> lazy;
 
-    void update(int k) { this->data[k] = S::op(this->data[2 * k], this->data[2 * k + 1]); }
+    void update(int k) { data[k] = S::op(data[2 * k], data[2 * k + 1]); }
     void all_apply(int k, U f) {
-        this->data[k] = F::f(f, this->data[k]);
-        if (k < _size) this->lazy[k] = F::op(f, this->lazy[k]);
+        data[k] = F::f(f, data[k]);
+        if (k < _size) lazy[k] = F::op(f, lazy[k]);
     }
     void push(int k) {
-        this->all_apply(2 * k, this->lazy[k]);
-        this->all_apply(2 * k + 1, this->lazy[k]);
-        this->lazy[k] = F::id;
+        all_apply(2 * k, lazy[k]);
+        all_apply(2 * k + 1, lazy[k]);
+        lazy[k] = F::id;
     }
 };
