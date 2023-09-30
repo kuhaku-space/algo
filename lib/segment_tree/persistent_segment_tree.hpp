8dfed2c2d5ad8a9765f127b15cc4a7132082f5ea
kuhaku-space <te4958yu6924@gmail.com>
2023-10-01 07:05:39 +0900

    Remove this pointer

--- lib/segment_tree/persistent_segment_tree.hpp
+++ lib/segment_tree/persistent_segment_tree.hpp
@@ -26,25 +26,25 @@ struct persistent_segment_tree {
 
     constexpr persistent_segment_tree() : _size(), root() {}
     constexpr persistent_segment_tree(int n, node_pointer _root) : _size(n), root(_root) {}
-    persistent_segment_tree(int n, T e = Monoid::id) : _size(n), root(this->build(0, n, e)) {}
+    persistent_segment_tree(int n, T e = Monoid::id) : _size(n), root(build(0, n, e)) {}
     template <class U>
     persistent_segment_tree(const std::vector<U> &v)
-        : _size(v.size()), root(this->build(0, v.size(), v)) {}
+        : _size(v.size()), root(build(0, v.size(), v)) {}
 
-    T operator[](int i) const { return this->prod(i, i + 1); }
-    T at(int k) const { return this->operator[](k); }
-    T get(int k) const { return this->operator[](k); }
+    T operator[](int i) const { return prod(i, i + 1); }
+    T at(int k) const { return operator[](k); }
+    T get(int k) const { return operator[](k); }
 
     persistent_segment_tree set(int k, T val) {
-        assert(0 <= k && k < this->_size);
-        return persistent_segment_tree(this->_size, this->set(0, this->_size, k, val, this->root));
+        assert(0 <= k && k < _size);
+        return persistent_segment_tree(_size, set(0, _size, k, val, root));
     }
-    persistent_segment_tree reset(int k) { return this->set(k, Monoid::id); }
+    persistent_segment_tree reset(int k) { return set(k, Monoid::id); }
 
-    T all_prod() const { return this->root->val; }
+    T all_prod() const { return root->val; }
     T prod(int a, int b) const {
-        assert(0 <= a && b <= this->_size);
-        return this->prod(0, this->_size, a, b, this->root);
+        assert(0 <= a && b <= _size);
+        return prod(0, _size, a, b, root);
     }
 
   private:
@@ -58,26 +58,26 @@ struct persistent_segment_tree {
     node_pointer build(int l, int r, T val) const {
         if (l + 1 == r) return new _node(val);
         int m = (l + r) >> 1;
-        return this->merge(this->build(l, m, val), this->build(m, r, val));
+        return merge(build(l, m, val), build(m, r, val));
     }
     template <class U>
     node_pointer build(int l, int r, const std::vector<U> &v) const {
         if (l + 1 == r) return new _node(v[l]);
         int m = (l + r) >> 1;
-        return this->merge(this->build(l, m, v), this->build(m, r, v));
+        return merge(build(l, m, v), build(m, r, v));
     }
 
     node_pointer set(int l, int r, int k, T val, node_pointer node) const {
         if (l + 1 == r) return new _node(val);
         int m = (l + r) >> 1;
-        if (k < m) return this->merge(this->set(l, m, k, val, node->left), node->right);
-        else return this->merge(node->left, this->set(m, r, k, val, node->right));
+        if (k < m) return merge(set(l, m, k, val, node->left), node->right);
+        else return merge(node->left, set(m, r, k, val, node->right));
     }
 
     T prod(int l, int r, int a, int b, node_pointer node) const {
         if (a <= l && r <= b) return node->val;
         if (b <= l || r <= a) return Monoid::id;
         int m = (l + r) >> 1;
-        return Monoid::op(this->prod(l, m, a, b, node->left), this->prod(m, r, a, b, node->right));
+        return Monoid::op(prod(l, m, a, b, node->left), prod(m, r, a, b, node->right));
     }
 };
