d44d0fcea09ecf8046404a0ad244e034be2dbc64
kuhaku-space <te4958yu6924@gmail.com>
2023-10-02 05:19:32 +0900

    Avoid using atcoder.hpp in test

--- test/aoj/alds1/prime_numbers.test.cpp
+++ test/aoj/alds1/prime_numbers.test.cpp
@@ -1,19 +1,19 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_1_C"
+#include <iostream>
 #include "math/prime_number.hpp"
-#include "template/atcoder.hpp"
 
 prime_number pn;
 
 int main(void) {
     int n;
-    cin >> n;
+    std::cin >> n;
     int ans = 0;
     while (n--) {
         int a;
-        cin >> a;
+        std::cin >> a;
         ans += pn.is_prime(a);
     }
-    co(ans);
+    std::cout << ans << std::endl;
 
     return 0;
 }
