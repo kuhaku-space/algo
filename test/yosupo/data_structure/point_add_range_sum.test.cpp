7f8e9ea57d191af5de7d9b3d3b1cee3919ff027a
kuhaku-space <te4958yu6924@gmail.com>
2023-10-01 17:52:17 +0900

    Update test for `fenwick_tree` class

--- test/yosupo/data_structure/point_add_range_sum.test.cpp
+++ test/yosupo/data_structure/point_add_range_sum.test.cpp
@@ -1,21 +1,19 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+#include <iostream>
+#include <iterator>
 #include "binary_tree/fenwick_tree.hpp"
-#include "template/atcoder.hpp"
 
 int main(void) {
-    sonic();
     int n, q;
-    cin >> n >> q;
-    vector<int> a(n);
-    cin >> a;
-    fenwick_tree<ll> ft(a);
+    std::cin >> n >> q;
+    std::vector<int> a(n);
+    std::copy_n(std::istream_iterator<int>(std::cin), n, std::begin(a));
+    fenwick_tree<std::int64_t> ft(a);
     while (q--) {
         int x, y, z;
-        cin >> x >> y >> z;
-        if (x == 0)
-            ft.add(y, z);
-        else
-            co(ft.sum(y, z));
+        std::cin >> x >> y >> z;
+        if (x == 0) ft.add(y, z);
+        else std::cout << ft.sum(y, z) << std::endl;
     }
 
     return 0;
