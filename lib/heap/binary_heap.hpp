83e2bbc2cade12fe0e226801353ede20c34da903
kuhaku-space <te4958yu6924@gmail.com>
2023-10-01 20:32:31 +0900

    Remove this pointer

--- lib/heap/binary_heap.hpp
+++ lib/heap/binary_heap.hpp
@@ -19,11 +19,11 @@ struct binary_heap {
 
         constexpr _node(Key _key, Value _value) : key(_key), value(_value), index() {}
 
-        constexpr bool operator<(const _node &rhs) const { return this->value < rhs.value; }
+        constexpr bool operator<(const _node &rhs) const { return value < rhs.value; }
 
         constexpr std::pair<Key, Value> get_pair() const { return std::make_pair(key, value); }
-        constexpr int get_index() const { return this->index; }
-        constexpr void set_index(int new_index) { this->index = new_index; }
+        constexpr int get_index() const { return index; }
+        constexpr void set_index(int new_index) { index = new_index; }
     };
 
   public:
@@ -31,53 +31,51 @@ struct binary_heap {
 
     binary_heap() : _size(), nodes(1), comp() {}
 
-    std::pair<Key, Value> top() const { return this->nodes[1]->get_pair(); }
-    constexpr int size() const { return this->_size; }
-    constexpr bool empty() const { return this->size() == 0; }
+    std::pair<Key, Value> top() const { return nodes[1]->get_pair(); }
+    constexpr int size() const { return _size; }
+    constexpr bool empty() const { return size() == 0; }
 
     node_pointer push(Key key, Value value) {
         auto node = new _node(key, value);
-        this->nodes.emplace_back(node);
+        nodes.emplace_back(node);
 
-        int index = this->increment_size();
-        while (index > 1 && comp(this->nodes[index >> 1]->value, this->nodes[index]->value)) {
-            std::swap(this->nodes[index], this->nodes[index >> 1]);
-            this->nodes[index]->set_index(index);
+        int index = increment_size();
+        while (index > 1 && comp(nodes[index >> 1]->value, nodes[index]->value)) {
+            std::swap(nodes[index], nodes[index >> 1]);
+            nodes[index]->set_index(index);
             index >>= 1;
         }
-        this->nodes[index]->set_index(index);
+        nodes[index]->set_index(index);
 
         return node;
     }
-    node_pointer emplace(Key key, Value value) { return this->push(key, value); }
+    node_pointer emplace(Key key, Value value) { return push(key, value); }
 
     void pop() {
-        this->nodes[1] = this->nodes[this->decrement_size()];
-        this->nodes.pop_back();
+        nodes[1] = nodes[decrement_size()];
+        nodes.pop_back();
 
         int index = 1 << 1;
-        while (index <= this->size()) {
-            if (index < this->size() &&
-                comp(this->nodes[index]->value, this->nodes[index + 1]->value))
-                ++index;
-            if (comp(this->nodes[index]->value, this->nodes[index >> 1]->value)) break;
-            std::swap(this->nodes[index >> 1], this->nodes[index]);
-            this->nodes[index >> 1]->set_index(index >> 1);
+        while (index <= size()) {
+            if (index < size() && comp(nodes[index]->value, nodes[index + 1]->value)) ++index;
+            if (comp(nodes[index]->value, nodes[index >> 1]->value)) break;
+            std::swap(nodes[index >> 1], nodes[index]);
+            nodes[index >> 1]->set_index(index >> 1);
             index <<= 1;
         }
-        this->nodes[index >> 1]->set_index(index >> 1);
+        nodes[index >> 1]->set_index(index >> 1);
     }
 
     void update(node_pointer node, Value value) {
         if (comp(node->value, value)) node->value = value;
         else return;
         int index = node->get_index();
-        while (index > 1 && comp(this->nodes[index >> 1]->value, this->nodes[index]->value)) {
-            std::swap(this->nodes[index], this->nodes[index >> 1]);
-            this->nodes[index]->set_index(index);
+        while (index > 1 && comp(nodes[index >> 1]->value, nodes[index]->value)) {
+            std::swap(nodes[index], nodes[index >> 1]);
+            nodes[index]->set_index(index);
             index >>= 1;
         }
-        this->nodes[index]->set_index(index);
+        nodes[index]->set_index(index);
     }
 
   private:
@@ -85,6 +83,6 @@ struct binary_heap {
     std::vector<node_pointer> nodes;
     Comp comp;
 
-    constexpr int increment_size() { return ++(this->_size); }
-    constexpr int decrement_size() { return (this->_size)--; }
+    constexpr int increment_size() { return ++(_size); }
+    constexpr int decrement_size() { return (_size)--; }
 };
