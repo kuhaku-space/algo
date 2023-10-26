ce35ba5f7a761b2d19c7a7722fb080fde32e0097
kuhaku-space <te4958yu6924@gmail.com>
2023-10-26 17:04:10 +0900

    Add docs to `longest_common_prefix` function

--- lib/string/lcp.hpp
+++ lib/string/lcp.hpp
@@ -1,5 +1,13 @@
 #include "template/template.hpp"
 
+/**
+ * @brief 最長共通接頭辞
+ *
+ * @tparam T 配列の型
+ * @param a 配列
+ * @param b 配列
+ * @return int 最長共通接頭辞の長さ
+ */
 template <class T>
 int longest_common_prefix(const std::vector<T> &a, const std::vector<T> &b) {
     int m = (int)std::min(a.size(), b.size());
@@ -9,6 +17,13 @@ int longest_common_prefix(const std::vector<T> &a, const std::vector<T> &b) {
     return m;
 }
 
+/**
+ * @brief 最長共通接頭辞
+ *
+ * @param a 文字列
+ * @param b 文字列
+ * @return int 最長共通接頭辞の長さ
+ */
 int longest_common_prefix(const std::string &a, const std::string &b) {
     int m = (int)std::min(a.size(), b.size());
     for (int i = 0; i < m; ++i) {
