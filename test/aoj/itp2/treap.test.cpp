5c97f36c81016c0fc4e5293b4280f59d20234a00
kuhaku-space <te4958yu6924@gmail.com>
2023-11-14 23:34:20 +0900

    Avoid using atcoder.hpp in aoj/itp2

--- test/aoj/itp2/treap.test.cpp
+++ test/aoj/itp2/treap.test.cpp
@@ -1,29 +1,22 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/7/ITP2_7_B"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ITP2_7_B"
 #include "binary_tree/treap.hpp"
-#include "template/atcoder.hpp"
+#include <iostream>
 
 int main(void) {
-    sonic();
     int q;
-    cin >> q;
+    std::cin >> q;
     int size = 0;
     Treap<int> treap;
-    rep(i, q) {
+    while (q--) {
         int x, y;
-        cin >> x >> y;
+        std::cin >> x >> y;
         if (x == 0) {
-            if (!treap.contains(y)) {
-                treap.insert(y);
-                ++size;
-            }
-            co(size);
+            if (!treap.contains(y)) treap.insert(y), ++size;
+            std::cout << size << std::endl;
         } else if (x == 1) {
-            co(treap.contains(y));
+            std::cout << treap.contains(y) << std::endl;
         } else if (x == 2) {
-            if (treap.contains(y)) {
-                treap.erase(y);
-                --size;
-            }
+            if (treap.contains(y)) treap.erase(y), --size;
         }
     }
 
