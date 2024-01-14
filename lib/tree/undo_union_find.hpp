71611eeb492a1c17beb1664c0fe4a0f86e02942b
kuhaku-space <te4958yu6924@gmail.com>
2024-01-14 19:55:38 +0900

    Remove this pointer

--- lib/tree/undo_union_find.hpp
+++ lib/tree/undo_union_find.hpp
@@ -10,39 +10,39 @@ struct undo_union_find {
     undo_union_find() : data(), history() {}
     undo_union_find(int _n) : data(_n, -1), history() {}
 
-    int root(int x) { return this->data[x] < 0 ? x : this->root(this->data[x]); }
-    int get_root(int k) { return this->root(k); }
+    int root(int x) { return data[x] < 0 ? x : root(data[x]); }
+    int get_root(int k) { return root(k); }
 
-    bool is_root(int k) const { return this->data[k] < 0; }
+    bool is_root(int k) const { return data[k] < 0; }
 
-    bool same(int x, int y) { return this->root(x) == this->root(y); }
-    bool is_same(int x, int y) { return this->same(x, y); }
+    bool same(int x, int y) { return root(x) == root(y); }
+    bool is_same(int x, int y) { return same(x, y); }
 
-    int size(int k) { return -this->data[this->root(k)]; }
-    int get_size(int k) { return this->size(k); }
+    int size(int k) { return -data[root(k)]; }
+    int get_size(int k) { return size(k); }
 
     bool unite(int x, int y) {
-        x = this->root(x), y = this->root(y);
-        this->history.emplace(x, this->data[x]);
-        this->history.emplace(y, this->data[y]);
+        x = root(x), y = root(y);
+        history.emplace(x, data[x]);
+        history.emplace(y, data[y]);
         if (x == y) return false;
-        if (this->data[x] > this->data[y]) std::swap(x, y);
-        this->data[x] += this->data[y];
-        this->data[y] = x;
+        if (data[x] > data[y]) std::swap(x, y);
+        data[x] += data[y];
+        data[y] = x;
         return true;
     }
 
     void undo() {
-        this->data[this->history.top().first] = this->history.top().second;
-        this->history.pop();
-        this->data[this->history.top().first] = this->history.top().second;
-        this->history.pop();
+        data[history.top().first] = history.top().second;
+        history.pop();
+        data[history.top().first] = history.top().second;
+        history.pop();
     }
 
-    int snapshot() const { return this->history.size(); }
+    int snapshot() const { return history.size(); }
 
     void rollback(int x = 0) {
-        while ((int)(this->history.size()) > x) this->undo();
+        while ((int)(history.size()) > x) undo();
     }
 
   private:
