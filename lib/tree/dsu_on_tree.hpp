765e68ceae22c6246cf173101e6a2de78e0bb91b
kuhaku-space <te4958yu6924@gmail.com>
2024-04-28 00:39:33 +0900

    Improve `dsu_on_tree` class

--- lib/tree/dsu_on_tree.hpp
+++ lib/tree/dsu_on_tree.hpp
@@ -3,81 +3,88 @@
 #include "graph/graph.hpp"
 #include "template/template.hpp"
 
+template <class T>
 struct dsu_on_tree {
-    template <class T>
-    dsu_on_tree(const Graph<T> &g, int r = 0)
-        : size(g.size()), root(r), par(size, -1), sub(size, 1), euler(size), left(size),
-          right(size) {
+    dsu_on_tree(const Graph<T> &_g, const std::vector<int> &query, int r = 0)
+        : g(_g), size(_g.size()), root(r), par(size, -1), sub(), euler(size), left(size),
+          right(size), heavy_path(size, -1), query_order(query.size()), query_size(size) {
         if (size == 0) return;
-        std::vector<int> heavy_path(size, -1);
-        dfs_sz(g, root, heavy_path);
-        euler[0] = root;
+        for (int x : query) ++query_size[x];
+        sub = query_size;
+        dfs_sz(root);
         if (size > 1) {
-            int pos = 1;
-            dfs_hld(g, root, pos, heavy_path);
+            int pos = 0, len = 0;
+            dfs_hld(root, pos, len);
         }
+        std::vector<int> cnt = left;
+        for (int i = 0; i < (int)query.size(); ++i) query_order[cnt[query[i]]++] = i;
+    }
+    dsu_on_tree(const Graph<T> &_g, int r = 0)
+        : g(_g), size(_g.size()), root(r), par(size, -1), sub(size, 1), euler(size), left(size),
+          right(size), heavy_path(size, -1), query_order(), query_size(size, 1) {
+        if (size == 0) return;
+        dfs_sz(root);
+        if (size > 1) {
+            int pos = 0, len = 0;
+            dfs_hld(root, pos, len);
+        }
+        query_order = euler;
     }
 
-    template <class F, class G, class H, class I>
-    void solve(F &rem, G &clear, H &query_vertex, I &query_edge) {
+    template <class F, class G, class H>
+    void solve(F &rem, G &clear, H &query) {
         auto dsu = [&](auto self, int v) -> void {
-            if (left[v] == right[v]) {
-                query_vertex(v);
+            int hp = heavy_path[v];
+            if (hp == -1) {
+                for (int i = left[v]; i < right[v]; ++i) query(query_order[i]);
                 rem(v);
                 return;
             }
-            for (int i = left[v] + 1; i < size && par[euler[i]] == v; ++i) {
-                self(self, euler[i]);
+            for (auto &e : g[v]) {
+                if (e.to() == par[v] || e.to() == heavy_path[v]) continue;
+                self(self, e.to());
                 clear();
             }
-            self(self, euler[left[v]]);
-            for (int i = left[v] + 1; i < left[euler[left[v]]]; ++i) {
-                query_vertex(euler[i]);
-                query_edge(par[euler[i]], euler[i]);
-            }
-            for (int i = right[euler[left[v]]]; i < right[v]; ++i) {
-                query_vertex(euler[i]);
-                query_edge(par[euler[i]], euler[i]);
-            }
-            query_vertex(v);
+            self(self, hp);
+            for (int i = left[v]; i < left[hp]; ++i) query(query_order[i]);
+            for (int i = right[hp]; i < right[v]; ++i) query(query_order[i]);
             rem(v);
         };
         dsu(dsu, 0);
     }
 
   private:
+    const Graph<T> &g;
     int size, root;
-    std::vector<int> par, sub, euler, left, right;
+    std::vector<int> par, sub, euler, left, right, heavy_path, query_order, query_size;
 
-    template <class T>
-    void dfs_sz(const Graph<T> &g, int v, std::vector<int> &heavy_path) {
+    void dfs_sz(int v) {
         int max_sub = 0;
         for (auto &e : g[v]) {
             int u = e.to();
             if (u == par[v]) continue;
             par[u] = v;
-            dfs_sz(g, u, heavy_path);
+            dfs_sz(u);
             sub[v] += sub[u];
             if (chmax(max_sub, sub[u])) heavy_path[v] = u;
         }
     }
 
-    template <class T>
-    void dfs_hld(const Graph<T> &g, int v, int &pos, std::vector<int> &heavy_path) {
-        left[v] = pos;
+    void dfs_hld(int v, int &pos, int &len) {
+        euler[pos++] = v;
+        left[v] = len;
+        right[v] = len + sub[v];
+        len += query_size[v];
         int hp = heavy_path[v];
-        if (hp != -1) euler[pos++] = hp;
+        if (hp == -1) {
+            right[v] = len;
+            return;
+        }
+        dfs_hld(hp, pos, len);
         for (auto &e : g[v]) {
             int u = e.to();
             if (u == par[v] || u == hp) continue;
-            euler[pos++] = u;
+            dfs_hld(u, pos, len);
         }
-        if (hp != -1) dfs_hld(g, hp, pos, heavy_path);
-        for (auto &e : g[v]) {
-            int u = e.to();
-            if (u == par[v] || u == hp) continue;
-            dfs_hld(g, u, pos, heavy_path);
-        }
-        right[v] = pos;
     }
 };
