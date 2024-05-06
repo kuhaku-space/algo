63cfe9216738eb510d02a057a23ab98144494c7b
kuhaku-space <te4958yu6924@gmail.com>
2024-05-06 21:20:54 +0900

    Add `Mul` monoid class

--- lib/segment_tree/monoid.hpp
+++ lib/segment_tree/monoid.hpp
@@ -16,6 +16,18 @@ struct Add {
 };
 
 template <class T>
+struct Mul {
+    using value_type = T;
+    static constexpr T id = T(1);
+    static constexpr T op(const T &lhs, const T &rhs) { return lhs * rhs; }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return lhs * rhs;
+    }
+};
+
+template <class T>
 struct And {
     using value_type = T;
     static constexpr T id = std::numeric_limits<T>::max();
