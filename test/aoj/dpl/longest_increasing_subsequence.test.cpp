5cbf8241b8ddedf7faf290c5be42c54dcd00c6f0
kuhaku-space <te4958yu6924@gmail.com>
2024-05-15 13:33:43 +0900

    Rename test file

--- test/aoj/dpl/longest_increasing_subsequence.test.cpp
+++ test/aoj/dpl/longest_increasing_subsequence.test.cpp
@@ -9,7 +9,7 @@ int main(void) {
     int n;
     std::cin >> n;
     std::vector<int> a(n);
-    std::copy_n(std::istream_iterator<int>(std::cin), n, std::begin(a));
+    for (int &e : a) std::cin >> e;
     std::cout << longest_increasing_subsequence(a) << std::endl;
 
     return 0;
