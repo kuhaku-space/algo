eb83b6e844567f015171e1e31287f9d779518262
kuhaku-space <te4958yu6924@gmail.com>
2024-05-15 10:21:27 +0900

    Improve fps algorithm

--- lib/fft/formal_power_series.hpp
+++ lib/fft/formal_power_series.hpp
@@ -1,85 +1,140 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <vector>
 #include "fft/ntt.hpp"
-#include "math/modint.hpp"
-#include "template/template.hpp"
+#include "math/sqrt.hpp"
 
-template <class mint>
-struct formal_power_series : std::vector<mint> {
-    using fps = formal_power_series;
+namespace fps {
 
-    formal_power_series(std::vector<T> &&v) : std::vector<mint>(v) {}
-    formal_power_series(const std::vector<T> &v) : std::vector<mint>(v) {}
+template <class mint, internal::is_static_modint_t<mint> * = nullptr>
+std::vector<mint> inv(const std::vector<mint> &h, int deg) {
+    assert(!h.empty() && h[0] != mint(0));
+    std::vector<mint> res(deg);
+    res[0] = h[0].inv();
+    for (int d = 1; d < deg; d <<= 1) {
+        std::vector<mint> f(2 * d), g(2 * d);
+        for (int i = 0; i < std::min((int)h.size(), 2 * d); ++i) f[i] = h[i];
+        for (int i = 0; i < d; ++i) g[i] = res[i];
+        internal::butterfly(f);
+        internal::butterfly(g);
+        for (int i = 0; i < 2 * d; ++i) f[i] *= g[i];
+        internal::butterfly_inv(f);
+        mint id = mint(2 * d).inv();
+        for (int i = 0; i < 2 * d; ++i) f[i] *= id;
+        for (int i = 0; i < d; ++i) f[i] = 0;
+        internal::butterfly(f);
+        for (int i = 0; i < 2 * d; ++i) f[i] *= g[i];
+        internal::butterfly_inv(f);
+        for (int i = 0; i < 2 * d; ++i) f[i] *= id;
+        for (int i = d; i < std::min(2 * d, deg); ++i) res[i] = -f[i];
+    }
+    res.resize(deg);
+    return res;
+}
 
-    constexpr fps &operator+=(const mint &v) {
-        if (this->empty()) this->resize(1);
-        (*this)[0] += v;
-        return *this;
-    }
-    constexpr fps &operator+=(const fps &f) {
-        if (f.size() > this->size()) this->resize(f.size());
-        for (int i = 0; i < (int)f.size(); ++i) (*this)[i] += f[i];
-        return this->normalize();
-    }
-    constexpr fps &operator-=(const mint &v) {
-        if (this->empty()) this->resize(1);
-        (*this)[0] -= v;
-        return *this;
-    }
-    constexpr fps &operator-=(const fps &f) {
-        if (f.size() > this->size()) this->resize(f.size());
-        for (int i = 0; i < (int)f.size(); ++i) (*this)[i] -= f[i];
-        return this->normalize();
-    }
-    constexpr fps &operator*=(const mint &v) {
-        for (int i = 0; i < (int)this->size(); ++i) (*this)[i] *= v;
-        return *this;
-    }
-    constexpr fps &operator*=(const fps &f) { return *this = convolution((*this), f); }
-    constexpr fps &operator/=(const mint &v) {
-        assert(v != 0);
-        mint iv = modinv(v);
-        for (int i = 0; i < (int)this->size(); ++i) (*this)[i] *= iv;
-        return *this;
-    }
-    constexpr fps &operator<<=(int x) {
-        fps res(x, 0);
-        res.insert(res.end(), begin(*this), end(*this));
-        return *this = res;
-    }
-    constexpr fps &operator>>=(int x) {
-        fps res;
-        res.insert(res.end(), begin(*this) + x, end(*this));
-        return *this = res;
-    }
+template <class mint, internal::is_static_modint_t<mint> * = nullptr>
+std::vector<mint> inv(const std::vector<mint> &h) {
+    return inv(h, h.size());
+}
 
-    constexpr fps operator+(const mint &v) const { return fps(*this) += v; }
-    constexpr fps operator+(const fps &f) const { return fps(*this) += f; }
-    constexpr fps operator-(const mint &v) const { return fps(*this) -= v; }
-    constexpr fps operator-(const fps &f) const { return fps(*this) -= f; }
-    constexpr fps operator*(const mint &v) const { return fps(*this) *= v; }
-    constexpr fps operator*(const fps &f) const { return fps(*this) *= f; }
-    constexpr fps operator/(const mint &v) const { return fps(*this) /= v; }
-    constexpr fps operator<<(int x) const { return fps(*this) <<= x; }
-    constexpr fps operator>>(int x) const { return fps(*this) >>= x; }
+template <class mint, internal::is_static_modint_t<mint> * = nullptr>
+std::vector<mint> log(const std::vector<mint> &h, int deg) {
+    assert(!h.empty() && h[0] == 1);
+    std::vector<mint> f(h.begin() + 1, h.end());
+    for (int i = 0; i < (int)f.size(); ++i) f[i] *= i + 1;
+    f = convolution(f, inv(h));
+    f.emplace(f.begin(), 0);
+    f.resize(deg);
+    for (int i = 1; i < deg; ++i) f[i] /= i;
+    return f;
+}
 
-    constexpr fps operator+() const { return fps(*this); }
-    constexpr fps operator-() const {
-        fps res = *this;
-        for (int i = 0; i < (int)res.size(); ++i) res[i] = -res[i];
+template <class mint, internal::is_static_modint_t<mint> * = nullptr>
+std::vector<mint> log(const std::vector<mint> &h) {
+    return log(h, h.size());
+}
+
+template <class mint, internal::is_static_modint_t<mint> * = nullptr>
+std::vector<mint> exp(const std::vector<mint> &h, int deg) {
+    std::vector<mint> f(deg), g(deg);
+    f[0] = 1;
+    g[0] = 1;
+    for (int d = 1; d < deg; d <<= 1) {
+        std::vector<mint> dt(d);
+        for (int i = 0; i < d; ++i) dt[i] = h[i] * i;
+        std::vector<mint> tf(2 * d), tg(2 * d), sf(d);
+        for (int i = 0; i < d; ++i) sf[i] = f[i];
+        for (int i = 0; i < d; ++i) tf[i] = f[i];
+        for (int i = 0; i < d; ++i) tg[i] = g[i];
+
+        tf = convolution(tf, dt);
+        tf.resize(2 * d);
+        for (int i = 0; i < d; ++i) tf[i] -= f[i] * i;
+        tf = convolution(tf, tg);
+        tf.resize(2 * d);
+        for (int i = 0; i < d; ++i) tf[i] -= dt[i];
+        for (int i = 1; i < 2 * d; ++i) tf[i] = tf[i] / i;
+        tf[0] = 0;
+        for (int i = 0; i < std::min((int)h.size(), 2 * d); ++i) tf[i] += h[i];
+        tf = convolution(sf, tf);
+        for (int i = d; i < std::min(deg, 2 * d); ++i) f[i] = tf[i];
+
+        std::vector<mint> ft(2 * d), gt(2 * d);
+        for (int i = 0; i < 2 * d; ++i) ft[i] = f[i];
+        for (int i = 0; i < d; ++i) gt[i] = g[i];
+        internal::butterfly(ft);
+        internal::butterfly(gt);
+        for (int i = 0; i < 2 * d; ++i) ft[i] *= gt[i];
+        internal::butterfly_inv(ft);
+        mint id = mint(2 * d).inv();
+        for (int i = 0; i < 2 * d; ++i) ft[i] *= id;
+        for (int i = 0; i < d; ++i) ft[i] = 0;
+        internal::butterfly(ft);
+        for (int i = 0; i < 2 * d; ++i) ft[i] *= gt[i];
+        internal::butterfly_inv(ft);
+        for (int i = 0; i < 2 * d; ++i) ft[i] *= id;
+        for (int i = d; i < std::min(deg, 2 * d); ++i) g[i] = -ft[i];
+    }
+    return f;
+}
+
+template <class mint, internal::is_static_modint_t<mint> * = nullptr>
+std::vector<mint> exp(const std::vector<mint> &h) {
+    return exp(h, h.size());
+}
+
+template <class mint, internal::is_static_modint_t<mint> * = nullptr>
+std::vector<mint> pow(const std::vector<mint> &h, std::int64_t m, int deg) {
+    if (m == 0) {
+        std::vector<mint> res(deg, 0);
+        res[0] = 1;
         return res;
     }
+    if (m == 1) return h;
+    if (m < 0) return inv(pow(h, -m, deg));
 
-    constexpr fps pre(int sz) const {
-        return fps(std::begin(*this), std::begin(*this) + std::min((int)this->size(), sz));
-    }
-    constexpr fps inv(int deg) {
-        assert((*this)[0] != 0);
-        if (deg < 0) deg = (int)v.size();
-        fps res({mint(1) / (*this)[0]});
-        for (int i = 1; i < deg; i <<= 1) {
-            res = (res + res - res * res * this->pre(i << 1)).pre(i << 1);
-        }
-        res.resize(deg);
-        return res;
-    }
-    constexpr fps inv() { return inv(v.size()); }
-};
+    int n = h.size();
+    int k = 0;
+    while (k < n && h[k] == 0) ++k;
+    if (k >= (deg + m - 1) / m) return std::vector<mint>(deg);
+    mint c = h[k];
+    mint ic = c.inv();
+    mint pc = c.pow(m);
+    std::vector<mint> res = h;
+    res.erase(res.begin(), res.begin() + k);
+    for (int i = 0; i < n - k; ++i) res[i] *= ic;
+    res = log(res, deg - k * m);
+    for (int i = 0; i < deg; ++i) res[i] *= m;
+    res = exp(res, deg - k * m);
+    for (int i = 0; i < deg - k * m; ++i) res[i] *= pc;
+    res.insert(res.begin(), k * m, mint());
+    return res;
+}
+
+template <class mint, internal::is_static_modint_t<mint> * = nullptr>
+std::vector<mint> pow(const std::vector<mint> &h, std::int64_t m) {
+    return pow(h, m, h.size());
+}
+
+}  // namespace fps
