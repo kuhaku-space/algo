3b60c95e74c7f5a9be57b53fff2c8652ec8f74b8
kuhaku-space <te4958yu6924@gmail.com>
2024-05-20 21:35:13 +0900

    Replace newline code

--- lib/graph/warshall_floyd.hpp
+++ lib/graph/warshall_floyd.hpp
@@ -8,9 +8,7 @@ void warshall_floyd(matrix_graph<T> &g) {
     for (int i = 0; i < n; ++i) g[i][i] = T();
     for (int k = 0; k < n; ++k) {
         for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                chmin(g[i][j], g[i][k] + g[k][j]);
-            }
+            for (int j = 0; j < n; ++j) { chmin(g[i][j], g[i][k] + g[k][j]); }
         }
     }
 }
