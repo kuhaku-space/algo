113ea9d7923de2717bf9647c3026fa489670f229
kuhaku-space <te4958yu6924@gmail.com>
2024-07-14 17:20:00 +0900

    Update test

--- test/yukicoder/1036.test.cpp
+++ test/yukicoder/1036.test.cpp
@@ -1,7 +1,18 @@
 // competitive-verifier: PROBLEM https://yukicoder.me/problems/no/1036
+#include <cstdint>
 #include <iostream>
+#include <numeric>
 #include <vector>
 #include "data_structure/swag.hpp"
+
+template <class T>
+struct Gcd {
+    using value_type = T;
+    static constexpr T id = std::numeric_limits<T>::max();
+    static constexpr T op(const T &lhs, const T &rhs) {
+        return lhs == Gcd::id ? rhs : (rhs == Gcd::id ? lhs : std::gcd(lhs, rhs));
+    }
+};
 
 int main(void) {
     int n;
