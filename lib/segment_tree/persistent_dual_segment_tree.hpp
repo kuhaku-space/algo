8392b2fa8431904dc85916408581045bd1afd9fa
kuhaku-space <te4958yu6924@gmail.com>
2024-08-05 21:22:51 +0900

    Rename `_node` class

--- lib/segment_tree/persistent_dual_segment_tree.hpp
+++ lib/segment_tree/persistent_dual_segment_tree.hpp
@@ -8,18 +8,18 @@ struct persistent_dual_segment_tree {
   private:
     using T = typename M::value_type;
 
-    struct Node {
-        using pointer = Node *;
+    struct _node {
+        using pointer = _node *;
         T val;
         pointer left, right;
 
-        constexpr Node(T _val) : val(_val), left(), right() {}
-        constexpr Node(T _val, pointer _left, pointer _right)
+        constexpr _node(T _val) : val(_val), left(), right() {}
+        constexpr _node(T _val, pointer _left, pointer _right)
             : val(_val), left(_left), right(_right) {}
     };
 
   public:
-    using node_pointer = typename Node::pointer;
+    using node_pointer = typename _node::pointer;
 
     constexpr persistent_dual_segment_tree() : _size(), root() {}
     constexpr persistent_dual_segment_tree(int n, node_pointer _root) : _size(n), root(_root) {}
@@ -44,17 +44,17 @@ struct persistent_dual_segment_tree {
     node_pointer root;
 
     static node_pointer merge(node_pointer left, node_pointer right) {
-        return new Node(M::op(left->val, right->val), left, right);
+        return new _node(M::op(left->val, right->val), left, right);
     }
 
     node_pointer build(int l, int r, T val) const {
-        if (l + 1 == r) return new Node(val);
+        if (l + 1 == r) return new _node(val);
         int m = (l + r) >> 1;
         return this->merge(this->build(l, m, val), this->build(m, r, val));
     }
     template <class U>
     node_pointer build(int l, int r, const std::vector<U> &v) const {
-        if (l + 1 == r) return new Node(v[l]);
+        if (l + 1 == r) return new _node(v[l]);
         int m = (l + r) >> 1;
         return this->merge(this->build(l, m, v), this->build(m, r, v));
     }
@@ -68,10 +68,10 @@ struct persistent_dual_segment_tree {
 
     node_pointer apply(int l, int r, int a, int b, T val, T prop, node_pointer node) const {
         if (a <= l && r <= b)
-            return new Node(M::op(val, M::op(prop, node->val)), node->left, node->right);
-        if (b <= l || r <= a) return new Node(M::op(prop, node->val), node->left, node->right);
+            return new _node(M::op(val, M::op(prop, node->val)), node->left, node->right);
+        if (b <= l || r <= a) return new _node(M::op(prop, node->val), node->left, node->right);
         int m = (l + r) >> 1;
-        return new Node(M::id(), this->apply(l, m, a, b, val, M::op(prop, node->val), node->left),
-                        this->apply(m, r, a, b, val, M::op(prop, node->val), node->right));
+        return new _node(M::id(), this->apply(l, m, a, b, val, M::op(prop, node->val), node->left),
+                         this->apply(m, r, a, b, val, M::op(prop, node->val), node->right));
     }
 };
