471c5535ee612d2b86ab1c7730b72974412426af
kuhaku-space <te4958yu6924@gmail.com>
2024-08-05 21:24:52 +0900

    Remove this pointer

--- lib/segment_tree/persistent_dual_segment_tree.hpp
+++ lib/segment_tree/persistent_dual_segment_tree.hpp
@@ -23,20 +23,19 @@ struct persistent_dual_segment_tree {
 
     constexpr persistent_dual_segment_tree() : _size(), root() {}
     constexpr persistent_dual_segment_tree(int n, node_ptr _root) : _size(n), root(_root) {}
-    persistent_dual_segment_tree(int n, T e = M::id()) : _size(n), root(this->build(0, n, e)) {}
+    persistent_dual_segment_tree(int n, T e = M::id()) : _size(n), root(build(0, n, e)) {}
     template <class U>
     persistent_dual_segment_tree(const std::vector<U> &v)
-        : _size(v.size()), root(this->build(0, v.size(), v)) {}
+        : _size(v.size()), root(build(0, v.size(), v)) {}
 
-    T operator[](int i) const { return this->get(0, this->_size, i, this->root); }
-    T at(int k) const { return this->operator[](k); }
-    T get(int k) const { return this->operator[](k); }
+    T operator[](int i) const { return get(0, _size, i, root); }
+    T at(int k) const { return operator[](k); }
+    T get(int k) const { return operator[](k); }
 
-    persistent_dual_segment_tree apply(int a, T val) const { return this->apply(a, a + 1, val); }
+    persistent_dual_segment_tree apply(int a, T val) const { return apply(a, a + 1, val); }
     persistent_dual_segment_tree apply(int a, int b, T val) const {
-        assert(0 <= a && b <= this->_size);
-        return persistent_dual_segment_tree(
-            this->_size, this->apply(0, this->_size, a, b, val, M::id(), this->root));
+        assert(0 <= a && b <= _size);
+        return persistent_dual_segment_tree(_size, apply(0, _size, a, b, val, M::id(), root));
     }
 
   private:
@@ -50,20 +49,20 @@ struct persistent_dual_segment_tree {
     node_ptr build(int l, int r, T val) const {
         if (l + 1 == r) return new _node(val);
         int m = (l + r) >> 1;
-        return this->merge(this->build(l, m, val), this->build(m, r, val));
+        return merge(build(l, m, val), build(m, r, val));
     }
     template <class U>
     node_ptr build(int l, int r, const std::vector<U> &v) const {
         if (l + 1 == r) return new _node(v[l]);
         int m = (l + r) >> 1;
-        return this->merge(this->build(l, m, v), this->build(m, r, v));
+        return merge(build(l, m, v), build(m, r, v));
     }
 
     T get(int l, int r, int k, node_ptr node) const {
         if (l + 1 == r) return node->val;
         int m = (l + r) >> 1;
-        if (k < m) return M::op(node->val, this->get(l, m, k, node->left));
-        else return M::op(node->val, this->get(m, r, k, node->right));
+        if (k < m) return M::op(node->val, get(l, m, k, node->left));
+        else return M::op(node->val, get(m, r, k, node->right));
     }
 
     node_ptr apply(int l, int r, int a, int b, T val, T prop, node_ptr node) const {
@@ -71,7 +70,7 @@ struct persistent_dual_segment_tree {
             return new _node(M::op(val, M::op(prop, node->val)), node->left, node->right);
         if (b <= l || r <= a) return new _node(M::op(prop, node->val), node->left, node->right);
         int m = (l + r) >> 1;
-        return new _node(M::id(), this->apply(l, m, a, b, val, M::op(prop, node->val), node->left),
-                         this->apply(m, r, a, b, val, M::op(prop, node->val), node->right));
+        return new _node(M::id(), apply(l, m, a, b, val, M::op(prop, node->val), node->left),
+                         apply(m, r, a, b, val, M::op(prop, node->val), node->right));
     }
 };
