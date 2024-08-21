feccecfe70e727bc9c7b0f8b180225d5dbaedd66
kuhaku-space <kuhakuspace2000@gmail.com>
2024-08-22 04:26:11 +0900

    Add implementation about `graph_csr` class

--- lib/graph/scc.hpp
+++ lib/graph/scc.hpp
@@ -1,6 +1,8 @@
+#pragma once
 #include <algorithm>
 #include <vector>
 #include "graph/graph.hpp"
+#include "internal/internal_csr.hpp"
 
 /**
  * @brief 強連結成分分解
@@ -12,13 +14,14 @@
 template <class T>
 std::vector<int> scc(const Graph<T> &g) {
     int n = g.size();
-    Graph<void> rg(n);
     std::vector<int> comp(n, -1), order;
     std::vector<bool> used(n);
 
+    internal::graph_csr rg(n);
     for (auto &es : g) {
         for (auto &e : es) rg.add_edge(e.to(), e.from());
     }
+    rg.build();
 
     auto dfs = [&](auto self, int index) {
         if (used[index]) return;
@@ -29,7 +32,40 @@ std::vector<int> scc(const Graph<T> &g) {
     auto rdfs = [&](auto self, int index, int count) {
         if (~comp[index]) return;
         comp[index] = count;
-        for (auto &e : rg[index]) self(self, e.to(), count);
+        for (int u : rg[index]) self(self, u, count);
+    };
+
+    for (int i = 0; i < n; ++i) dfs(dfs, i);
+    std::reverse(order.begin(), order.end());
+    int ptr = 0;
+    for (auto i : order) {
+        if (comp[i] == -1) rdfs(rdfs, i, ptr++);
+    }
+
+    return comp;
+};
+
+std::vector<int> scc(const internal::graph_csr &g) {
+    int n = g.size();
+    std::vector<int> comp(n, -1), order;
+    std::vector<bool> used(n);
+
+    internal::graph_csr rg(n);
+    for (int i = 0; i < n; ++i) {
+        for (int u : g[i]) rg.add_edge(u, i);
+    }
+    rg.build();
+
+    auto dfs = [&](auto self, int index) {
+        if (used[index]) return;
+        used[index] = true;
+        for (int u : g[index]) self(self, u);
+        order.emplace_back(index);
+    };
+    auto rdfs = [&](auto self, int index, int count) {
+        if (~comp[index]) return;
+        comp[index] = count;
+        for (int u : rg[index]) self(self, u, count);
     };
 
     for (int i = 0; i < n; ++i) dfs(dfs, i);
@@ -68,6 +104,18 @@ Graph<void> make_directed_acyclic_graph(const Graph<void> &g, const std::vector<
     for (auto &es : g) {
         for (auto &e : es) {
             int x = v[e.from()], y = v[e.to()];
+            if (x != y) res.add_edge(x, y);
+        }
+    }
+    return res;
+}
+
+internal::graph_csr make_directed_acyclic_graph(internal::graph_csr &g, const std::vector<int> &v) {
+    int n = *std::max_element(v.begin(), v.end()) + 1;
+    internal::graph_csr res(n);
+    for (int i = 0; i < n; ++i) {
+        for (int u : g[i]) {
+            int x = v[i], y = v[u];
             if (x != y) res.add_edge(x, y);
         }
     }
