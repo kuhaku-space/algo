34f4d392f321614fc00fc918d144b6888c88e815
kuhaku-space <kuhakuspace2000@gmail.com>
2024-11-29 04:03:01 +0900

    Update docs

--- lib/binary_tree/fenwick_tree_raq.hpp
+++ lib/binary_tree/fenwick_tree_raq.hpp
@@ -1,10 +1,6 @@
 #include "binary_tree/fenwick_tree.hpp"
 
-/**
- * @brief フェニック木（区間加算、区間総和）
- *
- * @tparam T
- */
+/// @brief フェニック木（区間加算、区間総和）
 template <class T>
 struct fenwick_tree_raq {
     fenwick_tree_raq() = default;
@@ -18,28 +14,12 @@ struct fenwick_tree_raq {
         for (int i = 0, n = v.size(); i < n; ++i) add(i, v[i]);
     }
 
-    /**
-     * @brief v[k] = val
-     *
-     * @param k index of array
-     * @param val new value
-     */
+    /// @brief v[k] = val
     void update(int k, T val) { add(k, k + 1, val - at(k)); }
 
-    /**
-     * @brief v[k] += val
-     *
-     * @param k index of array
-     * @param val
-     */
+    /// @brief v[k] += val
     void add(int k, T val) { add(k, k + 1, val); }
-    /**
-     * @brief v[a ... b-1] += val
-     *
-     * @param a first index of array
-     * @param b last index of array
-     * @param val
-     */
+    /// @brief v[a ... b-1] += val
     void add(int a, int b, T val) {
         assert(0 <= a && a <= b && b <= n);
         p.add(a, -val * a);
@@ -48,23 +28,12 @@ struct fenwick_tree_raq {
         q.add(b, -val);
     }
 
-    /**
-     * @brief v[0] + ... v[k - 1]
-     *
-     * @param k index of array
-     * @return T
-     */
+    /// @brief v[0] + ... v[k - 1]
     T sum(int k) const {
         assert(0 <= k && k <= n);
         return p.sum(k) + q.sum(k) * k;
     }
-    /**
-     * @brief v[a] + ... + v[b - 1]
-     *
-     * @param a first index of array
-     * @param b last index of array
-     * @return T
-     */
+    /// @brief v[a] + ... + v[b - 1]
     T sum(int a, int b) const { return sum(b) - sum(a); }
 
   private:
