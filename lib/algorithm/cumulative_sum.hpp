1c20dff9c676b0eb0bc58c17c51514267cdab1d0
kuhaku-space <kuhakuspace2000@gmail.com>
2025-01-15 14:46:53 +0900

    Add `cumulative_sum_2d` class

---
+++ lib/algorithm/cumulative_sum.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <cassert>
+#include <vector>
+
+template <class T>
+struct cumulative_sum_2d {
+    cumulative_sum_2d(const std::vector<std::vector<T>> &_v) : v(_v) { build(); }
+
+    void build() {
+        n = v.size();
+        assert(n > 0);
+        m = v[0].size();
+        assert(m > 0);
+        v.resize(n + 1);
+        for (int i = 0; i <= n; ++i) v[i].resize(m + 1);
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = m - 1; j >= 0; --j) v[i][j] += v[i][j + 1];
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = m - 1; j >= 0; --j) v[i][j] += v[i + 1][j];
+        }
+    }
+
+    T get(int x1, int y1, int x2, int y2) {
+        assert(0 <= x1 && x1 <= x2 && x2 <= n && 0 <= y1 && y1 <= y2 && y2 <= m);
+        return v[x1][y1] - v[x1][y2] - v[x2][y1] + v[x2][y2];
+    }
+
+  private:
+    std::vector<std::vector<T>> v;
+    int n, m;
+};
