0c42f947106d478053caf1da3a019c511dc6d087
kuhaku-space <kuhakuspace2000@gmail.com>
2025-02-23 03:39:17 +0900

    Add `linear_sieve` class

---
+++ lib/math/linear_sieve.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <array>
+#include <cassert>
+#include <vector>
+
+/// @brief 線形篩
+template <int N>
+struct linear_sieve {
+    linear_sieve() : lpf{}, pr() { build(); }
+
+    bool is_prime(int x) {
+        assert(1 <= x && x <= N);
+        return lpf[x] == x;
+    }
+
+    std::vector<int> prime_factorization(int x) {
+        assert(1 <= x && x <= N);
+        std::vector<int> res;
+        while (x > 1) {
+            res.emplace_back(lpf[x]);
+            x /= lpf[x];
+        }
+        return res;
+    }
+
+  private:
+    std::array<int, N + 1> lpf;
+    std::vector<int> pr;
+
+    void build() {
+        for (int i = 2; i <= N; ++i) {
+            if (lpf[i] == 0) {
+                lpf[i] = i;
+                pr.emplace_back(i);
+            }
+            for (int j = 0; i * pr[j] <= N; ++j) {
+                lpf[i * pr[j]] = pr[j];
+                if (pr[j] == lpf[i]) break;
+            }
+        }
+    }
+};
