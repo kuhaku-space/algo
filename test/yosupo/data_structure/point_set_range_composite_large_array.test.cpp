c645569c80c6c5d98f9399981a720b31f005cde2
kuhaku-space <kuhakuspace2000@gmail.com>
2025-03-14 00:09:01 +0900

    Add test for `dynamic_segment_tree` class

---
+++ test/yosupo/data_structure/point_set_range_composite_large_array.test.cpp
@@ -0,0 +1,28 @@
+// competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/point_set_range_composite_large_array
+#include <iostream>
+#include "math/modint.hpp"
+#include "segment_tree/dynamic_segment_tree.hpp"
+
+using Mint = modint998;
+
+int main(void) {
+    int n, q;
+    std::cin >> n >> q;
+    dynamic_segment_tree<Rev<Affine<Mint>>> dst(n);
+    while (q--) {
+        int t;
+        std::cin >> t;
+        if (t == 0) {
+            int p, c, d;
+            std::cin >> p >> c >> d;
+            dst.set(p, {c, d});
+        } else {
+            int l, r, x;
+            std::cin >> l >> r >> x;
+            auto [c, d] = dst.prod(l, r);
+            std::cout << c * x + d << '\n';
+        }
+    }
+
+    return 0;
+}
