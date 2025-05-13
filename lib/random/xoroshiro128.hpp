8817a46c20e53cf94f56a7e90890ed3bdb767682
kuhaku-space <kuhakuspace2000@gmail.com>
2025-05-13 11:20:41 +0900

    Improve pseudorandom generator

--- lib/random/xoroshiro128.hpp
+++ lib/random/xoroshiro128.hpp
@@ -5,17 +5,13 @@
 #include <utility>
 #include "random/split_mix_64.hpp"
 
-/**
- * @brief 疑似乱数生成器 xoroshiro128++
- * @details 周期：$2^128-1$
- */
+/// @brief 疑似乱数生成器 xoroshiro128++
 struct xoroshiro128 {
     using state_type = std::array<std::uint64_t, 2>;
     using result_type = std::uint64_t;
 
     constexpr xoroshiro128() noexcept : state(split_mix_64{}.generate_seed_sequence<2>()) {}
-    constexpr xoroshiro128(std::int64_t seed) noexcept
-        : state(split_mix_64{seed}.generate_seed_sequence<2>()) {}
+    constexpr xoroshiro128(std::uint64_t seed) noexcept : state(split_mix_64{seed}.generate_seed_sequence<2>()) {}
 
     static constexpr result_type min() noexcept { return std::numeric_limits<result_type>::min(); }
     static constexpr result_type max() noexcept { return std::numeric_limits<result_type>::max(); }
@@ -34,20 +30,17 @@ struct xoroshiro128 {
     constexpr void deserialize(const state_type &data) noexcept { state = data; }
     constexpr void deserialize(state_type &&data) noexcept { state = std::move(data); }
 
-    /**
-     * @brief a以上b以下の整数を生成
-     *
-     * @param a
-     * @param b
-     * @return int [a, b]
-     */
-    int rand_range(int a, int b) { return a + operator()() % (b - a + 1); }
+    /// @brief a以上b以下の整数を生成
+    /// @param a
+    /// @param b
+    /// @return int [a, b]
+    int rand_range(std::uint64_t a, std::uint64_t b) {
+        if (a == xoroshiro128::min() && b == xoroshiro128::max()) return operator()();
+        return a + operator()() % (b - a + 1);
+    }
 
-    /**
-     * @brief 0.0以上1.0未満の浮動小数点数を生成
-     *
-     * @return double [0, 1)
-     */
+    /// @brief 0.0以上1.0未満の浮動小数点数を生成
+    /// @return double [0, 1)
     double random() noexcept { return (double)operator()() / max(); }
 
   private:
