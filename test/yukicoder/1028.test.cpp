5b0f5ecd8aab82fd7fc16a45a5015452fc586176
kuhaku-space <kuhakuspace2000@gmail.com>
2025-05-30 00:11:25 +0900

    Add test for `slope_trick` class

---
+++ test/yukicoder/1028.test.cpp
@@ -0,0 +1,27 @@
+// competitive-verifier: PROBLEM https://yukicoder.me/problems/no/1028
+#include <iostream>
+#include <vector>
+
+#include "math/slope_trick.hpp"
+
+int main(void) {
+    int n;
+    std::cin >> n;
+    std::vector a(n, std::vector(n, 0));
+    for (auto &v : a) {
+        for (auto &e : v) std::cin >> e;
+    }
+    std::vector<slope_trick<int>> v(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            v[a[i][j] - 1].add(j);
+            v[a[i][j] - 1].add_f(i + j);
+            v[a[i][j] - 1].add_g(i - j);
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < n; ++i) ans += v[i].get();
+    std::cout << ans << '\n';
+
+    return 0;
+}
