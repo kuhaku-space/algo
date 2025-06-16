4cd75866beb8f08d164a1c1f878d18d1211e02b1
kuhaku-space <kuhakuspace2000@gmail.com>
2025-06-16 11:47:39 +0900

    Remove template.hpp

--- test/aoj/grl/bellman_ford.test.cpp
+++ test/aoj/grl/bellman_ford.test.cpp
@@ -1,7 +1,6 @@
 // competitive-verifier: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/GRL_1_B
 #include "graph/bellman_ford.hpp"
 #include <iostream>
-#include "template/template.hpp"
 
 int main(void) {
     int n, m, r;
@@ -9,15 +8,15 @@ int main(void) {
     Graph<int> g(n);
     g.input_edge(m, 0);
 
-    auto dist = bellman_ford(g, r, Inf);
+    auto dist = bellman_ford(g, r);
     for (int i = 0; i < n; ++i) {
-        if (dist[i] == -Inf) {
+        if (dist[i] == std::numeric_limits<int>::lowest()) {
             std::cout << "NEGATIVE CYCLE\n";
             return 0;
         }
     }
     for (int i = 0; i < n; ++i) {
-        if (dist[i] != Inf) std::cout << dist[i] << '\n';
+        if (dist[i] != std::numeric_limits<int>::max()) std::cout << dist[i] << '\n';
         else std::cout << "INF\n";
     }
 
--- test/yukicoder/0273.test.cpp
+++ test/yukicoder/0273.test.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string>
 #include "algorithm/palindrome.hpp"
-#include "template/template.hpp"
 
 int main(void) {
     std::string s;
@@ -11,7 +10,7 @@ int main(void) {
     int ans = 0;
     for (int l = 1; l < n; ++l) {
         for (int i = 0; i <= n - l; ++i) {
-            if (is_palindrome(s.substr(i, l))) chmax(ans, l);
+            if (is_palindrome(s.substr(i, l))) ans = std::max(ans, l);
         }
     }
     std::cout << ans << '\n';
