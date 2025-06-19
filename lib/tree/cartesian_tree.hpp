324d2c4be1d83f67204330a169fda6c62f066aa2
kuhaku-space <kuhakuspace2000@gmail.com>
2025-06-19 17:07:52 +0900

    Add `cartesian_tree_fully` function

--- lib/tree/cartesian_tree.hpp
+++ lib/tree/cartesian_tree.hpp
@@ -1,13 +1,8 @@
+#pragma once
 #include <stack>
 #include <vector>
 
-/**
- * @brief Cartesian Tree
- *
- * @tparam T 配列の型
- * @param v 配列
- * @return std::vector<int>
- */
+/// @brief Cartesian Tree
 template <typename T>
 std::vector<int> cartesian_tree(const std::vector<T> &v) {
     int n = v.size();
@@ -21,6 +16,32 @@ std::vector<int> cartesian_tree(const std::vector<T> &v) {
         }
         if (p != -1) res[p] = i;
         if (!st.empty()) res[i] = st.top();
+        st.emplace(i);
+    }
+    return res;
+}
+
+namespace internal {
+
+struct node_t {
+    int parent, left, right;
+
+    constexpr node_t() : parent(-1), left(-1), right(-1) {}
+};
+
+}  // namespace internal
+
+/// @brief Cartesian Tree
+template <typename T>
+std::vector<internal::node_t> cartesian_tree_fully(const std::vector<T> &v) {
+    int n = v.size();
+    std::vector<internal::node_t> res(n);
+    std::stack<T> st;
+    for (int i = 0; i < n; ++i) {
+        int p = -1;
+        while (!st.empty() && v[i] < v[st.top()]) p = st.top(), st.pop();
+        if (p != -1) res[p].parent = i, res[i].left = p;
+        if (!st.empty()) res[i].parent = st.top(), res[st.top()].right = i;
         st.emplace(i);
     }
     return res;
