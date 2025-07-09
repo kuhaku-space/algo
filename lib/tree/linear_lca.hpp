47e097e9edd0161d74d9e48e9c571c95af7de514
kuhaku-space <kuhakuspace2000@gmail.com>
2025-07-09 13:18:43 +0900

    Improve `linear_lca` class

--- lib/tree/linear_lca.hpp
+++ lib/tree/linear_lca.hpp
@@ -6,14 +6,15 @@
 #include <vector>
 #include "data_structure/linear_sparse_table.hpp"
 #include "graph/graph.hpp"
+#include "internal/internal_csr.hpp"
 
 struct linear_lca {
   private:
     struct S {
         int depth, index;
 
-        bool operator<(const S &rhs) const { return depth < rhs.depth; }
-        bool operator==(const S &rhs) const = default;
+        constexpr bool operator<(const S &rhs) const { return depth < rhs.depth; }
+        constexpr bool operator==(const S &rhs) const = default;
     };
 
     struct M {
@@ -41,6 +42,27 @@ struct linear_lca {
                 if (ord[e.to()] != -1) continue;
                 st.emplace(~x, d);
                 st.emplace(e.to(), d + 1);
+            }
+        }
+        lst = linear_sparse_table<M>(v);
+    }
+    linear_lca(const internal::graph_csr &g, int r = 0) : ord(g.size(), -1), lst() {
+        std::vector<S> v;
+        std::stack<std::pair<int, int>> st;
+        st.emplace(r, 0);
+        while (!st.empty()) {
+            auto [x, d] = st.top();
+            st.pop();
+            if (x < 0) {
+                v.emplace_back(d, ~x);
+                continue;
+            }
+            ord[x] = v.size();
+            v.emplace_back(d, x);
+            for (int y : g[x]) {
+                if (ord[y] != -1) continue;
+                st.emplace(~x, d);
+                st.emplace(y, d + 1);
             }
         }
         lst = linear_sparse_table<M>(v);
