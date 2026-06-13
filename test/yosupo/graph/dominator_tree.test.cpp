076127207dd7b8078aaa0a9034b92a6493ae8164
kuhaku-space <kuhakuspace2000@gmail.com>
2026-06-13 16:59:20 +0900

    Fix test

--- test/yosupo/graph/dominator_tree.test.cpp
+++ test/yosupo/graph/dominator_tree.test.cpp
@@ -1,17 +1,16 @@
 // competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/dominatortree
-#include <iostream>
 #include "graph/dominator_tree.hpp"
+#include <iostream>
+#include "graph/edge_input.hpp"
 
 int main() {
     int n, m, s;
     std::cin >> n >> m >> s;
-    Graph<void> g(n);
-    g.input_edge(m, 0);
+    edge_input<void> ei(m, 0);
+    auto g = ei.to_directed_graph(n);
 
     dominator_tree dt(g, s);
-    for (int i = 0; i < n; ++i) {
-        std::cout << (dt.idom(i) == -1 ? i : dt.idom(i)) << " \n"[i + 1 == n];
-    }
+    for (int i = 0; i < n; ++i) std::cout << dt.idom(i) << " \n"[i + 1 == n];
 
     return 0;
 }
